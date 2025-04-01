5cead3852f7809cdf2bcb2a09d969ff03ce8ed7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:03:47 +0000

    Add package auto-round-lib/0.4.7/auto_round_lib-0.4.7-py3-none-any.whl

---
+++ packages/auto-round-lib/auto_round_lib-0.4.7-py3-none-any.whl/auto_round/special_model_handler.py
@@ -0,0 +1,129 @@
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+from collections import UserDict
+shareable_keywords = ("position_ids", "cache_position", "position_embeddings")
+mllms_with_limited_bs = ("llava", "qwen2_vl", "phi3_v", "mllama") # Limitations on batch_size
+skippable_cache_keys = ("past_key_value",)
+
+SUPPORT_ONLY_TEXT_MODELS = [
+    "phi3_v",
+    "cogvlm2",
+    "llava",
+    "qwen2_vl",
+    "deepseek_vl_v2",
+    "chatglm",
+    "idefics3"
+]
+
+def _get_deepseek_vl2_multimodal_block(model, quant_vision=False):
+    model.forward = model.language.forward
+    block_names = []
+    if quant_vision:
+        block_names.append([f"vision.blocks.{i}" for i in range(len(model.vision.blocks))])
+        block_names.append([f"projector.layers.{i}" for i in range(len(model.projector.layers))])
+    block_names.append([f"language.model.layers.{i}" for i in range(len(model.language.model.layers))])
+    return block_names
+
+SPECIAL_MULTIMODAL_BLOCK = {
+    "deepseek_vl_v2": _get_deepseek_vl2_multimodal_block
+}
+
+
+def to_device(input, device=torch.device("cpu")):
+    """Moves input data to the specified device.
+
+    Args:
+    input: The input data to be moved.
+    device: The target device.
+
+    Returns:
+    The input data on the specified device.
+    """
+    if input is None:
+        return None
+    if isinstance(input, torch.Tensor):
+        return input.to(device)
+    if isinstance(input, dict) or isinstance(input, UserDict):
+        for inp in input.keys():
+            input[inp] = to_device(input[inp], device)
+
+    elif isinstance(input, list) or isinstance(input, tuple):
+        if len(input) == 0:
+            return input
+        input_res = []
+        for inp in input:
+            input_res.append(to_device(inp, device))
+        if isinstance(input, tuple):
+            input_res = tuple(input_res)
+        input = input_res
+
+    return input
+
+
+def init_cache_for_special_model(model, positional_inputs, inputs):
+    """
+    Initializes special model inputs by adding positional inputs if missing.
+
+    Args:
+        model: The model instance being initialized.
+        positional_inputs (list): List of positional inputs to add to inputs.
+        inputs (dict): Dictionary of model inputs.
+    
+    Modifies:
+        inputs (dict): Adds "positional_inputs" key if not present.
+    """
+    if "positional_inputs" not in inputs: # for chatglm Series
+        inputs["positional_inputs"] = []
+    for idx, item in enumerate(positional_inputs):
+        inputs["positional_inputs"] = to_device(positional_inputs)
+
+
+def reset_params(inputs):
+    """
+    Resets specific input parameters to avoid saving the key-value cache during fine-tuning.
+    
+    Args:
+        inputs (dict): Dictionary of model inputs.
+    
+    Modifies:
+        inputs (dict): Sets "use_cache" to False if the key is present.
+    """
+    if "use_cache" in inputs.keys(): # Not storing kv cache
+        inputs['use_cache'] = False
+        
+
+def check_skippable_keywords(key):
+    """
+    Prints a reminder if a key is not stored during quantization fine-tuning.
+    """
+    for cache_key in skippable_cache_keys:
+        if cache_key not in key:
+            return True
+    return False
+            
+
+def check_mllm_model_batch(model, batch_size, gradient_accumulate_steps=1):
+    """
+    Checks model configuration to determine if it's necessary to limit bs to avoid potential input shape mismatches.
+    """
+    for key in mllms_with_limited_bs:
+        if hasattr(model, "config") and key in model.config.model_type and batch_size != 1:
+            accumulate_steps = batch_size * gradient_accumulate_steps
+            print("To avoid the tensor concat mismatch problem, modified parameters to " \
+                    f"batch_size=1. As an alternative, set the gradient_accumulate_steps={accumulate_steps}")
+            return 1, accumulate_steps
+    return batch_size, gradient_accumulate_steps
+
