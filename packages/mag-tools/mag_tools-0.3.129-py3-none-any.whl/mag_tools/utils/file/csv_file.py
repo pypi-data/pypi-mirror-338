41d16b9da9e84b577b0b73d5f17a885ad68b1df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:03:57 +0000

    Add package mag-tools/0.3.129/mag_tools-0.3.129-py3-none-any.whl

---
+++ packages/mag-tools/mag_tools-0.3.129-py3-none-any.whl/mag_tools/utils/file/csv_file.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+
+@dataclass
+class CsvFile:
+    @classmethod
+    def save(cls, file_path: str, content: bytes):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+
+    @classmethod
+    def process(cls, file_path: str, target_columns: int= 1) -> tuple[np.ndarray, np.ndarray]:
+        """
+        :param file_path: 文件路径
+        :param target_columns: 目标值的列数
+        """
+        df = pd.read_csv(file_path)
+
+        inputs = df.iloc[:, :-target_columns].values  # 选择所有列，排除最后N列
+        targets = df.iloc[:, -target_columns].values  # 选择最后N列作为目标值
+
+        inputs = np.array(inputs)
+        targets = np.array(targets)
+
+        return inputs, targets
