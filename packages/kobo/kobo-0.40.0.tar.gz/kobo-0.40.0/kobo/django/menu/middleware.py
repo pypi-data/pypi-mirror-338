06942c0b528b7934309423ee0a6323d1c15c541e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:04:04 +0000

    Add package kobo/0.40.0/kobo-0.40.0.tar.gz

---
+++ packages/kobo/kobo-0.40.0.tar.gz/kobo-0.40.0/kobo/django/menu/middleware.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.deprecation import MiddlewareMixin
+
+from kobo.django.menu import menu
+
+
+__all__ = (
+    "MenuMiddleware",
+)
+
+
+class LazyMenu(object):
+    """
+    @summary: Cached menu object
+    """
+    def __get__(self, request, obj_type=None):
+        if not hasattr(request, "_cached_menu"):
+            request._cached_menu = menu.setup(request)
+        return request._cached_menu
+
+
+class MenuMiddleware(MiddlewareMixin):
+    """
+    @summary: Middleware for menu object.
+    """
+    def process_request(self, request):
+        """
+        @summary: Adds menu to request object
+        @param request: http request object
+        @type request: django.http.HttpRequest
+        """
+        request.__class__.menu = LazyMenu()
