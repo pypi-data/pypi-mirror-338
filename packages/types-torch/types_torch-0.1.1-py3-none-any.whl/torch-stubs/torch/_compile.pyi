3eb124f54579b7b8237cac8a1a20f182f1c0a059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:04:05 +0000

    Add package types-torch/0.1.1/types_torch-0.1.1-py3-none-any.whl

---
+++ packages/types-torch/types_torch-0.1.1-py3-none-any.whl/torch-stubs/torch/_compile.pyi
@@ -0,0 +1,12 @@
+from _typeshed import Incomplete
+
+def _disable_dynamo(fn: Incomplete | None = None, recursive: bool = True):
+    """
+    This API should be only used inside torch, external users should still use
+    torch._dynamo.disable. The main goal of this API is to avoid circular
+    imports issues that is common while using _dynamo.disable inside torch
+    itself.
+
+    This API avoids it by lazily importing torch._dynamo from the import time to
+    the invocation of the decorated function.
+    """
