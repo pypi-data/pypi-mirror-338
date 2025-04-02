d904ffaa83120f738aef48030fda986fef4287fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:54 +0000

    Add package geopop/0.0.2/geopop-0.0.2.tar.gz

---
+++ packages/geopop/geopop-0.0.2.tar.gz/geopop-0.0.2/README.md
@@ -0,0 +1,17 @@
+# geopop
+
+
+[![image](https://img.shields.io/pypi/v/geopop.svg)](https://pypi.python.org/pypi/geopop)
+[![image](https://img.shields.io/conda/vn/conda-forge/geopop.svg)](https://anaconda.org/conda-forge/geopop)
+
+
+**Python Package**
+
+
+-   Free software: MIT License
+-   Documentation: https://jriedl1.github.io/geopop
+
+
+## Features
+
+-   TODO
---
+++ packages/geopop/geopop-0.0.2.tar.gz/geopop-0.0.2/tests/test_geopop.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geopop` package."""
+
+
+import unittest
+
+from geopop import geopop
+
+
+class TestGeopop(unittest.TestCase):
+    """Tests for `geopop` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
