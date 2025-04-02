c35722f7d02c45d1be6b718417a53a57e9b8512c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:53 +0000

    Add package py-tools-wd/0.2.1/py_tools_wd-0.2.1.tar.gz

---
+++ packages/py-tools-wd/py_tools_wd-0.2.1.tar.gz/py_tools_wd-0.2.1/src/py_tools_wd/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "py-tools-wd"
+__description__ = "wd py tools"
+__url__ = "https://github.com/pgcomb/wd_tools_py"
+__version__ = "0.1.11"
+# __build__ = 0x023202
+__author__ = ["Wang Dongxu", "Ma xiaohui"]
+__author_email__ = ["wangdongxudyx@163.com", ""]
+__license__ = "Apache-2.0"
+# __copyright__ = "Copyright Kenneth Reitz"
+# __cake__ = "\u2728 \U0001f370 \u2728"
