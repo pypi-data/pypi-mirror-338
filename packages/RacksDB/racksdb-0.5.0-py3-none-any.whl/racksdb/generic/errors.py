b2f9738198d605cc2ced9b3afc361eee1d59b7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:18 +0000

    Add package RacksDB/0.5.0/racksdb-0.5.0-py3-none-any.whl

---
+++ packages/RacksDB/racksdb-0.5.0-py3-none-any.whl/racksdb/generic/errors.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022-2023 Rackslab
+#
+# This file is part of RacksDB.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+class DBSchemaError(Exception):
+    pass
+
+
+class DBFormatError(Exception):
+    pass
+
+
+class DBDumperError(Exception):
+    pass
+
+
+class DBViewError(Exception):
+    pass
+
+
+class DBOpenAPIGeneratorError(Exception):
+    pass
