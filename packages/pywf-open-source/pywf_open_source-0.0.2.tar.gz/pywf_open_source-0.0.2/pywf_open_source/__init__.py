e5501b404404f7bae6939415573bb4cff60492a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:04:55 +0000

    Add package pywf-open-source/0.0.2/pywf_open_source-0.0.2.tar.gz

---
+++ packages/pywf-open-source/pywf_open_source-0.0.2.tar.gz/pywf_open_source-0.0.2/pywf_open_source/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python project Ops automation.
+"""
+
+
+from ._version import __version__
+
+from ._version import __version__
+from ._version import __short_description__
+from ._version import __license__
+from ._version import __author__
+from ._version import __author_email__
+from ._version import __maintainer__
+from ._version import __maintainer_email__
