a340a79fbc6a29cc676d10efbf7b91ea1560c18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 15:04:27 +0000

    Add package sleepydatapeek/1.5.1/sleepydatapeek-1.5.1.tar.gz

---
+++ packages/sleepydatapeek/sleepydatapeek-1.5.1.tar.gz/sleepydatapeek-1.5.1/sleepydatapeek_toolchain/params.py
@@ -0,0 +1,15 @@
+supported_formats = {
+  'csv':'data',
+  'json': 'data',
+  'parquet': 'data',
+  'pkl': 'data',
+  'xlsx': 'data',
+  'pdf': 'metadata',
+}
+
+# these could later be a config file in a user's home directory
+default_sample_output_limit = 5
+sample_data_table_type = 'simple'
+metadata_table_type = 'rounded_grid'
+groupby_counts_table_type = 'rounded_grid'
+default_max_terminal_width = 80
