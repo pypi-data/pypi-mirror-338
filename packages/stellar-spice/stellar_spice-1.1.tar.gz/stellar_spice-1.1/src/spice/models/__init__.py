9563ad157ad7e2832d954cd8a51e1bbfd6e60da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:31 +0000

    Add package stellar-spice/1.1/stellar_spice-1.1.tar.gz

---
+++ packages/stellar-spice/stellar_spice-1.1.tar.gz/stellar_spice-1.1/src/spice/models/__init__.py
@@ -0,0 +1,6 @@
+from .model import Model
+from .mesh_generation import icosphere
+from .mesh_model import IcosphereModel, MeshModel
+from .spots import add_spherical_harmonic_spot, add_spherical_harmonic_spots
+from .binary import Binary
+from .phoebe_model import PhoebeModel
