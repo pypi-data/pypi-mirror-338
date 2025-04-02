99a54cff8a784fea569a294dfc4b655593ec250e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:59 +0000

    Add package keelson/0.3.9rc21/keelson-0.3.9rc21-py3-none-any.whl

---
+++ packages/keelson/keelson-0.3.9rc21-py3-none-any.whl/keelson/payloads/GeoJSON_pb2.pyi
@@ -0,0 +1,14 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GeoJSON(_message.Message):
+    __slots__ = ("timestamp", "geojson")
+    TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
+    GEOJSON_FIELD_NUMBER: _ClassVar[int]
+    timestamp: _timestamp_pb2.Timestamp
+    geojson: str
+    def __init__(self, timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., geojson: _Optional[str] = ...) -> None: ...
