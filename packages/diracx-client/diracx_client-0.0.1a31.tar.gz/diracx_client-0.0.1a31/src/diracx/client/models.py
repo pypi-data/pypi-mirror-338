8c796f2c10b223246173bb07f6828f24544d8bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:05:07 +0000

    Add package diracx-client/0.0.1a31/diracx_client-0.0.1a31.tar.gz

---
+++ packages/diracx-client/diracx_client-0.0.1a31.tar.gz/diracx_client-0.0.1a31/src/diracx/client/models.py
@@ -0,0 +1,7 @@
+from .generated.models import *  # pylint: disable=unused-wildcard-import
+
+
+# TODO: replace with postprocess
+from .generated.models import DeviceFlowErrorResponse
+
+__all__ = ("DeviceFlowErrorResponse",)
