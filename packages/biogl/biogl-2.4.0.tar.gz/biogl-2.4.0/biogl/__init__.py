11479024229cb44f66bd02fa5b8cd5cc8fd83492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 07:04:25 +0000

    Add package biogl/2.4.0/biogl-2.4.0.tar.gz

---
+++ packages/biogl/biogl-2.4.0.tar.gz/biogl-2.4.0/biogl/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "2.4.0"
+
+from biogl.arg_info_header import arg_info_header
+from biogl.byte_convert import byte_convert
+from biogl.coord_overlap import coord_overlap
+from biogl.fasta_parse import fasta_parse
+from biogl.file_hash import file_hash
+from biogl.flex_open import flex_open
+from biogl.get_runtime import get_runtime
+from biogl.GxfParse import GxfParse
+from biogl.is_fasta import is_fasta
+from biogl.label_info import label_info
+from biogl.random_seq import random_seq
+from biogl.recursive_items import recursive_items
+from biogl.rev_comp import rev_comp
+from biogl.translate import translate
+from biogl.window import window
