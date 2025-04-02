2d3b60305790b79b5ba91417bfbfd467ab2ffd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:07 +0000

    Add package python-to-cpp/0.1.8/python_to_cpp-0.1.8.tar.gz

---
+++ packages/python-to-cpp/python_to_cpp-0.1.8.tar.gz/python_to_cpp-0.1.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_to_cpp", 
+    version="0.1.8",
+    packages=find_packages(where='src'),
+    package_dir={"": "src"}, 
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "python_to_cpp=python_to_cpp.transpiler:main",
+        ],
+    },
+    author="Singhom931",
+    description="Convert Python To C++",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Singhom931/python_to_cpp",
+    # python_requires=">=3.6",
+)
