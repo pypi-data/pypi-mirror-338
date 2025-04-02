7508f2a1802a4e063fb84883c8f3724ac4842b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 04:04:23 +0000

    Add package ditidy/2.0.0/ditidy-2.0.0-py3-none-any.whl

---
+++ packages/ditidy/ditidy-2.0.0-py3-none-any.whl/ditidy/checks/extern_c.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+from ditidy.error import on_error
+
+
+def includes_extern(file: str, code: str):
+    match = re.match(r'.*#\s*ifdef\s+__cplusplus\s+extern\s+"C"\s*{\s*#\s*endif', code, re.DOTALL)
+    if match is None:
+        return False
+    return True
+
+
+def check(root_dir: str, files: list):
+    if len(files) == 0:
+        on_error("extern-c: no file(s) specified")
+
+    fails = []
+    for f in files:
+        with open(os.path.join(root_dir, f)) as codef:
+            code = codef.read()
+            if not includes_extern(f, code):
+                fails.append(f)
+    if len(fails) > 0:
+        return fails
+    return None
