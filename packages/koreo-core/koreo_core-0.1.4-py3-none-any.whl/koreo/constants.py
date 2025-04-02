134f57e4c5cad9d0de398a0416f8740f864605ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:55 +0000

    Add package koreo-core/0.1.4/koreo_core-0.1.4-py3-none-any.whl

---
+++ packages/koreo-core/koreo_core-0.1.4-py3-none-any.whl/koreo/constants.py
@@ -0,0 +1,17 @@
+API_GROUP = "koreo.dev"
+
+PREFIX = API_GROUP
+
+DEFAULT_API_VERSION = "v1beta1"
+
+ACTIVE_LABEL = f"{PREFIX}/active"
+
+LAST_APPLIED_ANNOTATION = f"{PREFIX}/last-applied-configuration"
+
+DEFAULT_LOAD_RETRY_DELAY = 30
+
+KOREO_DIRECTIVE_KEYS: set[str] = {
+    "x-koreo-compare-as-set",
+    "x-koreo-compare-as-map",
+    "x-koreo-compare-last-applied",
+}
