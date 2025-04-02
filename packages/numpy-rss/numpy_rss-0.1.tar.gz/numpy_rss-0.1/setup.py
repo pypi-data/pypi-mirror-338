718a3d136d759aadcfc6145a05b7af79a6654d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:05:09 +0000

    Add package numpy-rss/0.1/numpy_rss-0.1.tar.gz

---
+++ packages/numpy-rss/numpy_rss-0.1.tar.gz/numpy_rss-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="numpy_rss",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A custom library with E1 to E9 functions",
+    author="Jordan Blake",
+    author_email="jordanblake.tech@gmail.com",
+)
