c6a704145419879935d9846ea442c480a8d3b716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:51 +0000

    Add package ModImpNetTest/0.0.2/modimpnettest-0.0.2.tar.gz

---
+++ packages/ModImpNetTest/modimpnettest-0.0.2.tar.gz/modimpnettest-0.0.2/src/ModImpNetTest/modis_download_all.py
@@ -0,0 +1,36 @@
+"""This module makes the MODIS download automatic"""
+
+from datetime import datetime
+import os
+import toml
+
+from ModImpNetTest.modis_requests import create_log, write_product_metadata
+
+
+##################################################################################
+##################################################################################
+
+def download_modis(config_toml_path):
+    """Downloads the MODIS results in csv form, saves them to a directory, and writes a text log.
+
+    Parameters
+    ----------
+    config_toml_path: str
+        path to the TOML configuration file
+
+    Returns
+    -------
+    .csv files with MODIS data
+    .txt log file
+    """
+
+    with open(config_toml_path, 'r', encoding="utf-8") as f:
+        config = toml.load(f)
+
+    list_product_id = ['MOD10A1.061', 'MOD11A1.061']
+
+    dest_dir = config['directories']['dest_dir']
+    os.makedirs(dest_dir, exist_ok=True)
+    logfilepath = create_log(dest_dir)
+    for product_id in list_product_id:
+        write_product_metadata(logfilepath, product_id)
