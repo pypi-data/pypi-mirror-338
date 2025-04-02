f7eb27a0cf9b667e856b6626a95ab4cb35ec1fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:05:08 +0000

    Add package FFGo/1.12.9/ffgo-1.12.9.tar.gz

---
+++ packages/FFGo/ffgo-1.12.9.tar.gz/ffgo-1.12.9/ffgo-launcher.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ffgo-launcher.py --- Script to launch FFGo from its unpacked source
+# Copyright (c) 2015-2025, Florent Rougon
+#
+# This file is distributed under the terms of the DO WHAT THE FUCK YOU WANT TO
+# PUBLIC LICENSE version 2, dated December 2004, by Sam Hocevar. You should
+# have received a copy of this license along with this file. You can also find
+# it at <https://www.wtfpl.net/>.
+
+import os
+import sys
+
+srcPath = os.path.join(os.path.dirname(__file__), "src")
+sys.path.insert(0, srcPath)
+
+import ffgo.main
+ffgo.main.main()
