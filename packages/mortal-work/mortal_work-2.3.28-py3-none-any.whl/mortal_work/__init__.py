4c1124eeb4f63591cce671c0219b518c49276b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:19 +0000

    Add package mortal-work/2.3.28/mortal_work-2.3.28-py3-none-any.whl

---
+++ packages/mortal-work/mortal_work-2.3.28-py3-none-any.whl/mortal_work/__init__.py
@@ -0,0 +1,15 @@
+# cython: language_level=3
+# #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: MaJian
+@Time: 2024/1/17 9:28
+@SoftWare: PyCharm
+@Project: mortal
+@File: __init__.py
+"""
+__all__ = ["bonade"]
+from .bonade_main import BONADE
+
+
+bonade = BONADE()
