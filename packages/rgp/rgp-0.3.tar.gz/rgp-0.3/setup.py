90adc388e1bbced2ea6a97894125417f534485d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:04:54 +0000

    Add package rgp/0.3/rgp-0.3.tar.gz

---
+++ packages/rgp/rgp-0.3.tar.gz/rgp-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rgp',
+    version='0.3',
+    packages=find_packages(),
+    author='Joker',
+    author_email='m.mubarak1243@gmail.com',
+    description='Simple',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
