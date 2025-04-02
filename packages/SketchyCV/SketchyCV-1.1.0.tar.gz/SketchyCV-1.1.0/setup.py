842d146cbb2215e5814d7e3e736bec66b72121e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:04:38 +0000

    Add package SketchyCV/1.1.0/SketchyCV-1.1.0.tar.gz

---
+++ packages/SketchyCV/SketchyCV-1.1.0.tar.gz/SketchyCV-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name = 'SketchyCV',
+    version= '1.1.0',
+    packages= find_packages(),
+    install_requires = [],
+
+    long_description=description,
+    long_description_content_type= "text/markdown",
+)
