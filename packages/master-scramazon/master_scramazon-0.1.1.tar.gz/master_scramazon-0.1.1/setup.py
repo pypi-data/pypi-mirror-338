33ccbc3c44b0a63c78b7a7f6f04ba6505beb72bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:56 +0000

    Add package master-scramazon/0.1.1/master_scramazon-0.1.1.tar.gz

---
+++ packages/master-scramazon/master_scramazon-0.1.1.tar.gz/master_scramazon-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="master_scramazon",
+    version="0.1.1",
+    author="Mehran Safarzadeh",
+    url="https://github.com/mehran-sfz/Amazon-Scraper",
+    install_requires=[
+        "beautifulsoup4>=4.13.3",
+        "requests>=2.32.3",
+        "pillow>=11.2.0",
+        "selenium>=4.30.0",
+    ],
+    packages=find_packages(),
+    
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+    
