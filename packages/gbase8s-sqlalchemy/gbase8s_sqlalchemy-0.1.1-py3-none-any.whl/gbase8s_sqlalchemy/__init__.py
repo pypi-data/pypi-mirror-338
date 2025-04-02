95e953923838053b759dc971934b35d9a4e03edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 15:04:23 +0000

    Add package gbase8s-sqlalchemy/0.1.1/gbase8s_sqlalchemy-0.1.1-py3-none-any.whl

---
+++ packages/gbase8s-sqlalchemy/gbase8s_sqlalchemy-0.1.1-py3-none-any.whl/gbase8s_sqlalchemy/__init__.py
@@ -0,0 +1,5 @@
+from . import gbase8sdb
+
+__version__ = "0.1.1"
+
+dialect = gbase8sdb.GBase8sDialect
---
+++ packages/gbase8s-sqlalchemy/gbase8s_sqlalchemy-0.1.1-py3-none-any.whl/sqlalchemy_gbase8s/__init__.py
@@ -0,0 +1,5 @@
+from . import gbase8sdb
+
+__version__ = "0.0.1"
+
+dialect = gbase8sdb.GBase8sDialect
