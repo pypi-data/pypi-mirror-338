5feb5572ad9eda5cdb6f14e805a91aa36288b294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:35 +0000

    Add package whiskerrag/0.0.27/whiskerrag-0.0.27.tar.gz

---
+++ packages/whiskerrag/whiskerrag-0.0.27.tar.gz/whiskerrag-0.0.27/src/whiskerrag_types/model/tenant.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone
+from typing import Any, Optional
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field, field_validator, model_validator
+
+from whiskerrag_types.model.utils import parse_datetime
+
+
+class Tenant(BaseModel):
+    tenant_id: str = Field(
+        default_factory=lambda: str(uuid4()), description="tenant id"
+    )
+    tenant_name: str = Field("", description="tenant name")
+    email: str = Field(..., description="email")
+    secret_key: str = Field("", description="secret_key")
+    is_active: bool = Field(True, description="is active")
+    created_at: Optional[datetime] = Field(
+        default=None,
+        description="tenant created time",
+        alias="gmt_create",
+    )
+    updated_at: Optional[datetime] = Field(
+        default=None,
+        description="tenant updated time",
+        alias="gmt_modified",
+    )
+
+    def update(self, **kwargs: Any) -> "Tenant":
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        self.updated_at = datetime.now()
+        return self
+
+    @field_validator("is_active", mode="before")
+    @classmethod
+    def convert_tinyint_to_bool(cls, v: Any) -> bool:
+        return bool(v)
+
+    @model_validator(mode="before")
+    def pre_process_data(cls, data: dict) -> dict:
+        for field, value in data.items():
+            if isinstance(value, UUID):
+                data[field] = str(value)
+        field_mappings = {"created_at": "gmt_create", "updated_at": "gmt_modified"}
+        for field, alias_name in field_mappings.items():
+            val = data.get(field) or data.get(alias_name)
+            if val is None:
+                continue
+
+            if isinstance(val, str):
+                dt = parse_datetime(val)
+                data[field] = dt
+                data[alias_name] = dt
+            else:
+                if val and val.tzinfo is None:
+                    dt = val.replace(tzinfo=timezone.utc)
+                    data[field] = dt
+                    data[alias_name] = dt
+
+        return data
+
+    @model_validator(mode="after")
+    def set_defaults(self) -> "Tenant":
+        now = datetime.now(timezone.utc)
+        if self.created_at is None:
+            self.created_at = now
+        if self.updated_at is None:
+            self.updated_at = now
+        return self
+
+    class Config:
+        allow_population_by_field_name = True
