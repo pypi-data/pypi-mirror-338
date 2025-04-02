f9bf84e7dc799e487b0e6f9da7b62be43203c221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:04:45 +0000

    Add package fixout/0.1.25/fixout-0.1.25.tar.gz

---
+++ packages/fixout/fixout-0.1.25.tar.gz/fixout-0.1.25/fixout/__init__.py
@@ -0,0 +1,15 @@
+from .artifact import FixOutArtifact
+from .helper import ReverseFairness, UnfairModel, clazzes
+from .runner import FixOutRunner
+
+
+from .fairness import (
+    equal_opportunity,
+    demographic_parity,
+    conditional_accuracy_equality,
+    predictive_equality,
+    predictive_parity,
+    equalized_odds,
+    )
+
+__all__ = ['artifact', 'fairness', 'helper', 'runner', 'utils']
