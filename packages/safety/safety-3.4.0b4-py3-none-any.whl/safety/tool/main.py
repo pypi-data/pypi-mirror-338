0866517862f71e71704e72f811f65c8fc2a462da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:23 +0000

    Add package safety/3.4.0b4/safety-3.4.0b4-py3-none-any.whl

---
+++ packages/safety/safety-3.4.0b4-py3-none-any.whl/safety/tool/main.py
@@ -0,0 +1,61 @@
+import os.path
+from pathlib import Path
+from typing import Optional
+
+from safety.console import main_console as console
+from safety.tool.utils import (
+    PipConfigurator,
+    PipRequirementsConfigurator,
+    PoetryPyprojectConfigurator,
+    is_os_supported,
+)
+
+from .interceptors import create_interceptor
+
+
+def has_local_tool_files(directory: Path) -> bool:
+    configurators = [PipRequirementsConfigurator(), PoetryPyprojectConfigurator()]
+
+    for file_name in os.listdir(directory):
+        if os.path.isfile(file_name):
+            file = Path(file_name)
+            for configurator in configurators:
+                if configurator.is_supported(file):
+                    return True
+
+    return False
+
+
+def configure_system(org_slug: Optional[str]):
+    configurators = [PipConfigurator()]
+
+    for configurator in configurators:
+        configurator.configure(org_slug)
+
+
+def reset_system():
+    configurators = [PipConfigurator()]
+
+    for configurator in configurators:
+        configurator.reset()
+
+
+def configure_alias():
+    if not is_os_supported():
+        return
+
+    interceptor = create_interceptor()
+    interceptor.install_interceptors()
+
+    console.print("Configured PIP alias")
+
+
+def configure_local_directory(directory: Path, org_slug: Optional[str]):
+    configurators = [PipRequirementsConfigurator(), PoetryPyprojectConfigurator()]
+
+    for file_name in os.listdir(directory):
+        if os.path.isfile(file_name):
+            file = Path(file_name)
+            for configurator in configurators:
+                if configurator.is_supported(file):
+                    configurator.configure(file, org_slug)
