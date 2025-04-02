3f581544e87fd42fc4abb7d868ea93eca1eebfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:06:01 +0000

    Add package wwwpy/0.1.84/wwwpy-0.1.84.tar.gz

---
+++ packages/wwwpy/wwwpy-0.1.84.tar.gz/wwwpy-0.1.84/src/wwwpy/remote/designer/dev_mode.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+
+import js
+
+logger = logging.getLogger(__name__)
+
+
+async def activate():
+    from wwwpy.remote import micropip_install
+    from wwwpy.common import designer
+
+    for package in designer.pypi_packages:
+        await micropip_install(package)
+
+    from wwwpy.remote.designer import helpers
+    js.window.onerror = helpers._on_error
+    js.window.onunhandledrejection = helpers._on_unhandledrejection
+    from wwwpy.remote.designer import log_redirect
+    log_redirect.redirect_logging()
+
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(_global_exception_handler)
+
+
+def is_active() -> bool:
+    try:
+        return js.window._wwwpy_dev_mode
+    except AttributeError:
+        return False
+
+
+def _global_exception_handler(loop, context):
+    # The context parameter contains details about the exception
+    if 'message' in context:
+        logger.info(f"Global handler caught: {context['message']}")
+    if 'exception' not in context:
+        logger.error(f"Global handler caught: {context}")
+        return
+    exception = context.get('exception')
+    if exception:
+        logger.info(f"Exception type: {type(exception)}, Args: {exception.args}")
+        logger.exception(exception)
+
+
+def set_active(dev_mode: bool):
+    js.window._wwwpy_dev_mode = dev_mode
---
+++ packages/wwwpy/wwwpy-0.1.84.tar.gz/wwwpy-0.1.84/src/wwwpy/server/pytestlib/remote_conftest.py
@@ -0,0 +1,32 @@
+import asyncio
+
+import pytest
+from js import window
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    # this prevents pytest-asyncio to closing the pyodide event loop (webloop)
+    yield asyncio.get_running_loop()
+
+
+def pytest_sessionstart(session):
+    print(f'invocation_dir={session.config.invocation_dir}')
+    print(f'rootpath={session.config.rootpath}')
+
+
+def pytest_xvirt_send_event(event_json):
+    async def callback():
+        path = '#xvirt_notify_path_marker#'
+        await async_fetch_str(path, method='POST', data=event_json)
+
+    asyncio.create_task(callback())
+
+
+async def async_fetch_str(url: str, method: str = 'GET', data: str = '') -> str:
+    print(f'url={url}')
+    print(f'method={method}')
+    print(f'data=`{data[:100]}`')
+    response = await window.fetch(url, method=method, body=data)
+    text = await response.text()
+    return text
