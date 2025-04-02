7731766138da4d85af070461345c01745289c89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:05:05 +0000

    Add package pybwa/1.6.0/pybwa-1.6.0-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/pybwa/pybwa-1.6.0-cp311-cp311-manylinux_2_28_x86_64.whl/pybwa/libbwa.py
@@ -0,0 +1,29 @@
+import enum
+
+from pybwa.libbwaaln import _set_bwa_aln_verbosity
+from pybwa.libbwaindex import _set_bwa_idx_verbosity
+from pybwa.libbwamem import _set_bwa_mem_verbosity
+
+__all__ = [
+    "BwaVerbosity",
+    "set_bwa_verbosity",
+]
+
+
+# class syntax
+@enum.unique
+class BwaVerbosity(enum.IntEnum):
+    """The verbosity level for the BWA C-API"""
+
+    QUIET = 0
+    ERROR = 1
+    WARNING = 2
+    INFO = 3
+    DEBUG = 4
+
+
+def set_bwa_verbosity(level: BwaVerbosity) -> bool:
+    changed = _set_bwa_idx_verbosity(level)
+    changed |= _set_bwa_mem_verbosity(level)
+    changed |= _set_bwa_aln_verbosity(level)
+    return changed
