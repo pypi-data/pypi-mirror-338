1a810d99d53e1b3c49b450816d35e1aff204700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:27 +0000

    Add package ModImpNetTest/0.0.4/modimpnettest-0.0.4-py3-none-any.whl

---
+++ packages/ModImpNetTest/modimpnettest-0.0.4-py3-none-any.whl/ModImpNetTest/modis_requests.py
@@ -0,0 +1,29 @@
+"""This module has functions that allow for authentication, task submission, download"""
+
+from datetime import datetime
+import os
+
+##################################################################################
+##################################################################################
+
+def create_log(dest_dir):
+    """Creates a .txt log for MODIS task request submission and download.
+
+    Parameters
+    ----------
+    dest_dir: str
+        A directory that where the log file will be created.
+
+    Returns
+    -------
+    logfilepath: str
+        path to the log file
+    """
+    datetime_now = datetime.now().strftime('%Y%m%d%H%M%S')
+    logfile = f'MODIS_log_{datetime_now}.txt'
+    logfilepath = os.path.join(dest_dir, logfile)
+
+    with open(logfilepath,'a+', encoding="utf-8") as _:
+        pass
+
+    return logfilepath
