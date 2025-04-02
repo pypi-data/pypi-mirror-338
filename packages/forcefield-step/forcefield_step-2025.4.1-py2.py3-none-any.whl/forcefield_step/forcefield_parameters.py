6a822e65e945ab107be392d2574bc4c8206cc431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 07:04:48 +0000

    Add package forcefield-step/2025.4.1/forcefield_step-2025.4.1-py2.py3-none-any.whl

---
+++ packages/forcefield-step/forcefield_step-2025.4.1-py2.py3-none-any.whl/forcefield_step/forcefield_parameters.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""Control parameters for using forcefields"""
+
+import importlib
+import logging
+import os
+from pathlib import Path
+import pkg_resources
+import seamm
+
+logger = logging.getLogger(__name__)
+
+
+# Get the list of available forcefields
+path = pkg_resources.resource_filename(__name__, "data/")
+
+# Check if we can use OpenKIM
+if importlib.util.find_spec("kim_query") is None:
+    forcefields = []
+else:
+    forcefields = ["OpenKIM"]
+
+logger.debug("Looking for forcefields at " + path)
+with os.scandir(path) as it:
+    for entry in it:
+        if not entry.name.startswith(".") and entry.is_file():
+            logger.debug("   " + entry.name)
+            ffname, ext = os.path.splitext(entry.name)
+            if ext == ".frc":
+                forcefields.append(entry.name)
+
+# and local forcefields in the ~/SEAMM/data/Forcefields
+local = Path.home() / "SEAMM" / "data" / "Forcefields"
+logger.debug("Looking for forcefields at " + str(local))
+if local.exists():
+    for path in local.glob("**/*.frc"):
+        logger.debug("   " + str(path))
+        forcefields.append("local:" + str(path.relative_to(local)))
+
+# and local forcefields in the ~/.seamm.d/data/Forcefields
+local = Path.home() / ".seamm.d" / "data" / "Forcefields"
+logger.debug("Looking for forcefields at " + str(local))
+if local.exists():
+    for path in local.glob("**/*.frc"):
+        logger.debug("   " + str(path))
+        forcefields.append("local:" + str(path.relative_to(local)))
+
+forcefields = sorted(forcefields)
+
+
+class ForcefieldParameters(seamm.Parameters):
+    """The control parameters for forcefields"""
+
+    parameters = {
+        "task": {
+            "default": "setup forcefield",
+            "kind": "enumeration",
+            "default_units": "",
+            "enumeration": (
+                "setup forcefield",
+                "assign forcefield to structure",
+            ),
+            "format_string": "s",
+            "description": "What to do:",
+            "help_text": "What to do with the forcefield.",
+        },
+        "forcefield_file": {
+            "default": "oplsaa.frc",
+            "kind": "enumeration",
+            "default_units": "",
+            "enumeration": tuple(forcefields),
+            "format_string": "s",
+            "description": "Forcefield Repository:",
+            "help_text": "The forcefield repository or file to use.",
+        },
+        "forcefield": {
+            "default": "default",
+            "kind": "enumeration",
+            "default_units": "",
+            "enumeration": ("default",),
+            "format_string": "s",
+            "description": "Forcefield:",
+            "help_text": "The forcefield with the file.",
+        },
+        "elements": {
+            "default": "",
+            "kind": "periodic table",
+            "default_units": None,
+            "enumeration": None,
+            "format_string": "",
+            "description": "Elements:",
+            "help_text": "The elements to include.",
+        },
+        "potentials": {
+            "default": "",
+            "kind": "enumeration",
+            "default_units": "",
+            "enumeration": ("will be replaced",),
+            "format_string": "s",
+            "description": "Interatomic Potentials:",
+            "help_text": "The interatomic potentials to use.",
+        },
+    }
+
+    def __init__(self, defaults={}, data=None):
+        """Initialize the instance, by default from the default
+        parameters given in the class"""
+
+        super().__init__(
+            defaults={**ForcefieldParameters.parameters, **defaults}, data=data
+        )
