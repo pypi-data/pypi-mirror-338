bd007a82b578b0698cf8340f8b201726978ed35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:06:01 +0000

    Add package tuoni/0.9.1/tuoni-0.9.1.tar.gz

---
+++ packages/tuoni/tuoni-0.9.1.tar.gz/tuoni-0.9.1/tuoni/__init__.py
@@ -0,0 +1,84 @@
+"""
+tuoni: A Python library for using tuoni.io
+
+This package provides tools to automate usage of Tuoni server, providing same functionality as GUI
+
+
+"""
+import inspect
+
+from .TuoniAgent import TuoniAgent
+from .TuoniAlias import TuoniAlias
+from .TuoniC2 import TuoniC2
+from .TuoniCommand import TuoniCommand
+from .TuoniDefaultCommands import *
+from .TuoniExceptions import *
+from .TuoniListener import TuoniListener
+from .TuoniListenerPlugin import TuoniListenerPlugin
+from .TuoniPayloadPlugin import TuoniPayloadPlugin
+from .TuoniResult import TuoniResult
+from .TuoniResultPart import TuoniResultPart
+from .TuoniUser import TuoniUser
+from .TuoniFile import TuoniFile
+from .TuoniDataHost import TuoniDataHost
+from .TuoniDataService import TuoniDataService
+from .TuoniDataCredential import TuoniDataCredential
+
+
+__all__ = [
+    'TuoniAgent',
+    'TuoniAlias',
+    'TuoniC2',
+    'TuoniCommand',
+    'TuoniListener',
+    'TuoniListenerPlugin',
+    'TuoniPayloadPlugin',
+    'TuoniResult',
+    'TuoniResultPart',
+    'TuoniUser',
+    'TuoniFile',
+    "TuoniDataHost",
+    "TuoniDataService",
+    "TuoniDataCredential",
+    
+    'ExceptionTuoniAuthentication',
+    'ExceptionTuoniRequestFailed',
+    'ExceptionTuoniDeleted',
+    
+    'ExecutionNew',
+    'ExecutionExisting',
+    'TuoniCommandBof',
+    'TuoniCommandCd',
+    'TuoniCommandDie',
+    'TuoniCommandLs',
+    'TuoniCommandCmd',
+    'TuoniCommandJobs',
+    'TuoniCommandProclist',
+    'TuoniCommandRun',
+    'TuoniCommandPowershell',
+    'TuoniCommandSleep',
+    'TuoniCommandTokenSteal',
+    'TuoniCommandTokenDeleteAll',
+    'TuoniCommandTokenDelete',
+    'TuoniCommandTokenList',
+    'TuoniCommandTokenMake',
+    'TuoniCommandTokenUse',
+    'TuoniCommandRm',
+    'TuoniCommandDownload',
+    'TuoniCommandUpload',
+    'TuoniCommandCp',
+    'TuoniCommandMv',
+    'TuoniCommandMkdir',
+    'TuoniCommandSocks5',
+    'TuoniCommandConnectTcp',
+    'TuoniCommandexecuteAssembly',
+    'TuoniCommandInject',
+    'TuoniCommandProcinfo',
+    'TuoniCommandScreenshot',
+    'TuoniCommandSpawn',
+    'TuoniCommandJumpService',
+    'TuoniCommandJumpWinrm',
+    'TuoniCommandJumpWmi',
+    'TuoniCommandJumpSsh',
+    'TuoniCommandReverseShellCommunication'
+]
