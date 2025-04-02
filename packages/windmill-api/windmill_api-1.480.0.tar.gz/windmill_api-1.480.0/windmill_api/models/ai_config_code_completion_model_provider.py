0660f425f9775b7847799b73cfa98b2d6df51e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:04:57 +0000

    Add package windmill-api/1.480.0/windmill_api-1.480.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.480.0.tar.gz/windmill_api-1.480.0/windmill_api/models/ai_config_code_completion_model_provider.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class AIConfigCodeCompletionModelProvider(str, Enum):
+    ANTHROPIC = "anthropic"
+    CUSTOMAI = "customai"
+    DEEPSEEK = "deepseek"
+    GOOGLEAI = "googleai"
+    GROQ = "groq"
+    MISTRAL = "mistral"
+    OPENAI = "openai"
+    OPENROUTER = "openrouter"
+    TOGETHERAI = "togetherai"
+
+    def __str__(self) -> str:
+        return str(self.value)
