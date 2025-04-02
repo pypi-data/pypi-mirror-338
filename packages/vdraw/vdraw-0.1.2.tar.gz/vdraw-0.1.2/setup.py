796c2af646bf049216dfba87e5b2c9830be266a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 07:04:45 +0000

    Add package vdraw/0.1.2/vdraw-0.1.2.tar.gz

---
+++ packages/vdraw/vdraw-0.1.2.tar.gz/vdraw-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vdraw",
+    version="0.1.2",
+    description="A library for working with segmentation masks",
+    author="workerlv",
+    packages=find_packages(),
+    install_requires=["numpy", "opencv-python"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
