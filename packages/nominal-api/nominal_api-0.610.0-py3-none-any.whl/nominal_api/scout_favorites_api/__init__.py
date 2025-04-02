992dca1d9dc398ff02a8ff1016399dd7e811c6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 15:04:55 +0000

    Add package nominal-api/0.610.0/nominal_api-0.610.0-py3-none-any.whl

---
+++ packages/nominal-api/nominal_api-0.610.0-py3-none-any.whl/nominal_api/scout_favorites_api/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from .._impl import (
+    scout_favorites_api_FavoriteResource as FavoriteResource,
+    scout_favorites_api_FavoriteResourceVisitor as FavoriteResourceVisitor,
+    scout_favorites_api_ListFavoritesRequest as ListFavoritesRequest,
+    scout_favorites_api_ListFavoritesResponse as ListFavoritesResponse,
+    scout_favorites_api_ResourceType as ResourceType,
+    scout_favorites_api_UpdateFavoritesRequest as UpdateFavoritesRequest,
+    scout_favorites_api_UpdateFavoritesResponse as UpdateFavoritesResponse,
+)
+
