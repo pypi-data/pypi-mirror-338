085c1075f28f32bc36d61eff7a014d0ab3ffed4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:06:20 +0000

    Add package vallog/0.2.2/vallog-0.2.2-py3-none-any.whl

---
+++ packages/vallog/vallog-0.2.2-py3-none-any.whl/vallog/__init__.py
@@ -0,0 +1,14 @@
+"""
+importing Logger module
+make variables accessible to the user
+"""
+
+from vallog.logger import Logger
+
+
+default = Logger.default
+info = Logger.info
+warning = Logger.warning
+error = Logger.error
+debug = Logger.debug
+header = Logger.header
