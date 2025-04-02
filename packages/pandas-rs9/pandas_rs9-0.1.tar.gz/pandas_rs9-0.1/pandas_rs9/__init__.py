c0ef5cabfa8a8401b65f97d571155ea5f553c528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:05:15 +0000

    Add package pandas-rs9/0.1/pandas_rs9-0.1.tar.gz

---
+++ packages/pandas-rs9/pandas_rs9-0.1.tar.gz/pandas_rs9-0.1/pandas_rs9/__init__.py
@@ -0,0 +1,3 @@
+from .module import Experiments
+
+__all__ = ["Experiments"]
---
+++ packages/pandas-rs9/pandas_rs9-0.1.tar.gz/pandas_rs9-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pandas_rs9",
+    version="0.1",
+    packages=find_packages(),
+    author="JKBlake",
+    description="A library for running different recommendation system experiments",
+    python_requires='>=3.6',
+)
