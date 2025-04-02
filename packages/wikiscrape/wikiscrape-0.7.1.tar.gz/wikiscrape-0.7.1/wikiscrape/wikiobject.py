2272df0bcdcd6fd4f3b4fc37407d494a61295ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:04:28 +0000

    Add package wikiscrape/0.7.1/wikiscrape-0.7.1.tar.gz

---
+++ packages/wikiscrape/wikiscrape-0.7.1.tar.gz/wikiscrape-0.7.1/wikiscrape/wikiobject.py
@@ -0,0 +1,26 @@
+from typing import ClassVar
+
+from bs4 import BeautifulSoup
+
+
+class Wikiobject:
+    _html_tag: str | None = None
+    _identifier: ClassVar[dict[str, str]] = {}
+
+    def __init__(self, value):
+        self.value = value
+
+    def __new__(cls, *args, **kwargs):
+        if cls == Wikiobject:
+            raise NotImplementedError("Wikiobject should not be created directly")
+
+        return super().__new__(cls)
+
+    @classmethod
+    def from_html(cls, html: str):
+        soup = BeautifulSoup(html, "html.parser")
+        return cls.from_soup(soup)
+
+    @classmethod
+    def from_soup(cls, soup: BeautifulSoup):
+        return cls(soup.find(cls._html_tag, cls._identifier))
