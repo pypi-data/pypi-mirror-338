5d096dbf5b2296dcad5fe68ce08456e9a0171c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:04:53 +0000

    Add package LCNE-patchseq-analysis/0.6.0/lcne_patchseq_analysis-0.6.0.tar.gz

---
+++ packages/LCNE-patchseq-analysis/lcne_patchseq_analysis-0.6.0.tar.gz/lcne_patchseq_analysis-0.6.0/src/LCNE_patchseq_analysis/__init__.py
@@ -0,0 +1,17 @@
+"""Init package"""
+
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+__version__ = "0.6.0"
+
+# Get the path of this file
+PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+RAW_DIRECTORY = os.path.join(PACKAGE_DIRECTORY, "../../data/LCNE-patchseq-ephys-raw")
+
+# Check if Raw data directory exists
+if not os.path.exists(RAW_DIRECTORY):
+    logger.warning(f"Raw data directory does not exist: {RAW_DIRECTORY}")
