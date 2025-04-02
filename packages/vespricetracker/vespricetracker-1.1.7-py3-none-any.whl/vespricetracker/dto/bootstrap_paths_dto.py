eda4f6a232469a04473bde43c84234f144f56196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:27 +0000

    Add package vespricetracker/1.1.7/vespricetracker-1.1.7-py3-none-any.whl

---
+++ packages/vespricetracker/vespricetracker-1.1.7-py3-none-any.whl/vespricetracker/dto/bootstrap_paths_dto.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+# Frozen=True creates an implicit hash method, eq is created by default
+@dataclass(frozen=True)
+class BootstrapPathsDTO:
+    log_dir: Path
