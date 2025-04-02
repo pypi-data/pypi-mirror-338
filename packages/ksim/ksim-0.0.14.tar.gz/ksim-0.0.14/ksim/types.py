6a6d42fc4cd8bb1a95f0770c9ffc51f5b32bbef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:05:00 +0000

    Add package ksim/0.0.14/ksim-0.0.14.tar.gz

---
+++ packages/ksim/ksim-0.0.14.tar.gz/ksim-0.0.14/ksim/types.py
@@ -0,0 +1,86 @@
+"""Base Types for Environments."""
+
+__all__ = [
+    "PhysicsData",
+    "PhysicsModel",
+    "PhysicsState",
+    "Trajectory",
+    "Rewards",
+    "Histogram",
+    "Metrics",
+]
+
+from dataclasses import dataclass
+from typing import Mapping, TypeAlias
+
+import jax
+import mujoco
+import xax
+from jaxtyping import Array, PyTree
+from mujoco import mjx
+
+PhysicsData: TypeAlias = mjx.Data | mujoco.MjData
+PhysicsModel: TypeAlias = mjx.Model | mujoco.MjModel
+
+
+@jax.tree_util.register_dataclass
+@dataclass(frozen=True)
+class PhysicsState:
+    """Everything you need for the engine to take an action and step physics."""
+
+    most_recent_action: Array
+    data: PhysicsData
+    event_states: xax.FrozenDict[str, PyTree]
+
+
+@jax.tree_util.register_dataclass
+@dataclass(frozen=True)
+class Trajectory:
+    qpos: Array
+    qvel: Array
+    xpos: Array
+    xquat: Array
+    obs: xax.FrozenDict[str, PyTree]
+    command: xax.FrozenDict[str, PyTree]
+    event_state: xax.FrozenDict[str, Array]
+    action: Array
+    done: Array
+    timestep: Array
+    termination_components: xax.FrozenDict[str, Array]
+    aux_outputs: PyTree
+
+
+@jax.tree_util.register_dataclass
+@dataclass(frozen=True)
+class Rewards:
+    total: Array
+    components: xax.FrozenDict[str, Array]
+
+
+@jax.tree_util.register_dataclass
+@dataclass(frozen=True)
+class Histogram:
+    counts: Array
+    limits: Array
+    min: Array
+    max: Array
+    sum: Array
+    sum_squares: Array
+    mean: Array
+
+
+@jax.tree_util.register_dataclass
+@dataclass(frozen=True)
+class Metrics:
+    train: Mapping[str, Array | Histogram]
+    reward: Mapping[str, Array | Histogram]
+    termination: Mapping[str, Array | Histogram]
+    curriculum_level: Array
+
+
+@jax.tree_util.register_dataclass
+@dataclass(frozen=True)
+class SingleTrajectory:
+    trajectory: Trajectory
+    rewards: Rewards
+    metrics: xax.FrozenDict[str, Array]
