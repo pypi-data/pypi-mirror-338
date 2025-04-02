ec4af80717a3ae2a397befb075077d2bcbe59d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:05:00 +0000

    Add package NepTrainKit/2.0.2/neptrainkit-2.0.2.tar.gz

---
+++ packages/NepTrainKit/neptrainkit-2.0.2.tar.gz/neptrainkit-2.0.2/docs/source/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+project = 'NepTrainKit'
+copyright = '2024, NepTrain Team'
+author="NepTrain Team"
+release = '1.4.9'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+html_show_sourcelink = False
+extensions = [
+    # 'sphinx.ext.autodoc',
+    # 'sphinx.ext.napoleon',
+    # 'sphinx.ext.mathjax',
+
+"sphinx_design",
+    "myst_parser"
+]
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    # "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+templates_path = ['_templates']
+# locale_dirs = ['docs/locales']  # 语言文件目录
+language = 'en'
+
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_context = {
+    "author_name": author,
+}
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
---
+++ packages/NepTrainKit/neptrainkit-2.0.2.tar.gz/neptrainkit-2.0.2/docs/source/index.rst
@@ -0,0 +1,15 @@
+.. NepTrainKit documentation master file, created by
+   sphinx-quickstart on Wed Dec  4 19:50:15 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+NepTrainKit documentation
+=========================
+NepTrainKit is a toolkit focused on the operation and visualization of neuroevolution potential (NEP) training datasets. It is mainly used to simplify and optimize the NEP model training process, providing an intuitive graphical interface and analysis tools to help users adjust train dataset.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Documentation:
+
+   module    <module/index>
+   example    <example/index>
