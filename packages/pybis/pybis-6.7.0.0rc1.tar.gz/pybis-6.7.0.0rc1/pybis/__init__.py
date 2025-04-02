df8015d83ff4f2b2f7b8dc357fdfc3bd6d13c71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 03:15:45 +0000

    Add package pybis/6.7.0.0rc1/pybis-6.7.0.0rc1.tar.gz

---
+++ packages/pybis/pybis-6.7.0.0rc1.tar.gz/pybis-6.7.0.0rc1/pybis/__init__.py
@@ -0,0 +1,25 @@
+#   Copyright ETH 2018 - 2023 Zürich, Scientific IT Services
+# 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+#   
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+name = "pybis"
+__author__ = "ID SIS • ETH Zürich"
+__email__ = "openbis-support@id.ethz.ch"
+__version__ = "6.7.0.0-rc1"
+
+from . import pybis
+from .pybis import DataSet
+from .pybis import Openbis
+from .pybis import ImagingControl
+from .pybis import Spreadsheet
+from .imaging import *
