790201af7f4dffa6efceb79f74678ccec8160fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:27 +0000

    Add package vikdatashift/1.0.1/vikdatashift-1.0.1.tar.gz

---
+++ packages/vikdatashift/vikdatashift-1.0.1.tar.gz/vikdatashift-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vikdatashift',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'psycopg2>=2.8.0',
+        'cx_Oracle>=7.0.0'
+    ],
+    long_description= open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+)
