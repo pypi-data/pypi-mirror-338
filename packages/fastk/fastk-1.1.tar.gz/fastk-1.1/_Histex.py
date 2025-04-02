e0938ce8e86ed86e981610d8cd600a2bca505957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:36 +0000

    Add package fastk/1.1/fastk-1.1.tar.gz

---
+++ packages/fastk/fastk-1.1.tar.gz/fastk-1.1/_Histex.py
@@ -0,0 +1,34 @@
+from ctypes import CDLL, POINTER, Structure, c_char_p, c_int, c_longlong, cast
+from os.path import dirname, isfile, join
+
+import bits.util as bu
+
+lib = CDLL(join(dirname(__file__), "Histex.so"))
+lib.load_hist.restype = POINTER(c_longlong)
+
+
+def histex(
+    fastk_prefix: str, min_count: int = 1, max_count: int = 100, unique: bool = False
+) -> bu.RelCounter:
+    """Run Histex and return a histogram of k-mer count frequencies.
+
+    positional arguments:
+      @ fastk_prefix : Prefix of the output files of FastK.
+
+    optional arguments:
+      @ [min|max]_count : Specify the range of the k-mer count.
+      @ unique          : If True, return counts of unique k-mers.
+    """
+    assert isfile(f"{fastk_prefix}.hist"), "No .hist file"
+    cgram = lib.load_hist(
+        c_char_p(fastk_prefix.encode("utf-8")),
+        c_int(min_count),
+        c_int(max_count),
+        c_int(1 if unique else 0),
+    )
+    x = cast(cgram, POINTER(c_longlong * (max_count - min_count + 1)))[0]
+    hist = bu.RelCounter(
+        {j: x[i] for i, j in enumerate(range(min_count, max_count + 1))}
+    )
+    lib.free_hist(cgram)
+    return hist
---
+++ packages/fastk/fastk-1.1.tar.gz/fastk-1.1/_Profex.py
@@ -0,0 +1,66 @@
+from ctypes import CDLL, POINTER, Structure, c_char_p, c_int, c_longlong, c_ushort, cast
+from os.path import dirname, isfile, join
+from typing import Generator, List, Sequence, Tuple, Union
+
+
+class Profile(Structure):
+    _fields_ = [("profile", POINTER(c_ushort)), ("length", c_int), ("K", c_int)]
+
+
+lib = CDLL(join(dirname(__file__), "Profex.so"))
+lib.load_profile.restype = POINTER(Profile)
+
+
+def profex(
+    fastk_prefix: str, read_id: int, zero_padding: bool = False, return_k: bool = False
+) -> Union[List[int], Tuple[List[int], int]]:
+    """Run Profex and return the k-mer count profile of a single read.
+
+    positional arguments:
+      @ fastk_prefix : Prefix of the output files of FastK.
+      @ read_id      : Read ID (1, 2, ...)
+      @ zero_padding : If True, add (K - 1) zero counts to the prefix.
+      @ return_k     : If True, return the value of K as well.
+    """
+    assert isfile(f"{fastk_prefix}.prof"), "No .prof file"
+
+    lib.open_profile(c_char_p(fastk_prefix.encode("utf-8")))
+
+    ret = lib.load_profile(c_longlong(read_id))
+    x = cast(ret.contents.profile, POINTER(c_ushort * ret.contents.length))[0]
+    counts = [x[i] for i in range(len(x))]
+    K = ret.contents.K
+    if zero_padding:
+        counts = [0] * (K - 1) + counts
+    lib.free_profile(ret)
+    return counts if not return_k else (counts, K)
+
+
+def profex_multi(
+    fastk_prefix: str,
+    read_ids: Sequence[int],
+    zero_padding: bool = False,
+    return_k: bool = False,
+) -> Generator[Union[List[int], Tuple[List[int], int]]]:
+    """Run Profex and return the k-mer count profile of a single read.
+
+    positional arguments:
+      @ fastk_prefix : Prefix of the output files of FastK.
+      @ read_id      : Read ID (1, 2, ...)
+      @ zero_padding : If True, add (K - 1) zero counts to the prefix.
+      @ return_k     : If True, return the value of K as well.
+    """
+    assert isfile(f"{fastk_prefix}.prof"), "No .prof file"
+
+    lib.open_profile(c_char_p(fastk_prefix.encode("utf-8")))
+
+    for read_id in read_ids:
+        ret = lib.load_profile(c_longlong(read_id))
+        x = cast(ret.contents.profile, POINTER(c_ushort * ret.contents.length))[0]
+        counts = [x[i] for i in range(len(x))]
+        K = ret.contents.K
+        if zero_padding:
+            counts = [0] * (K - 1) + counts
+        yield counts if not return_k else (counts, K)
+
+    lib.free_profile(ret)
