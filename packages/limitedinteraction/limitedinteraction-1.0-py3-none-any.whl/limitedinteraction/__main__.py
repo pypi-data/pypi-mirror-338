b9968c44221e90fd2dc6e8d6b090de79968ed5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 04:04:41 +0000

    Add package limitedinteraction/1.0/limitedinteraction-1.0-py3-none-any.whl

---
+++ packages/limitedinteraction/limitedinteraction-1.0-py3-none-any.whl/limitedinteraction/__main__.py
@@ -0,0 +1,9 @@
+import limitedinteraction as li
+
+if __name__ == "__main__":
+    li.button_dialog(
+        "Simple dialog windows for scripters and scientists.",
+        choices=['OK'],
+        title='Limited Interaction',
+        icon='light',
+    )
