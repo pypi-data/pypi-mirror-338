2a18cdeeb6ff8e6e4e460ec277d8a9270f51a71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:04:25 +0000

    Add package inertia-flask/1.0.0/inertia_flask-1.0.0.tar.gz

---
+++ packages/inertia-flask/inertia_flask-1.0.0.tar.gz/inertia_flask-1.0.0/inertia_flask/settings.py
@@ -0,0 +1,32 @@
+from .utils import InertiaJsonEncoder
+
+
+class Settings:
+    """Default Inertia Flask settings.
+    These settings can be overridden by setting them in your Flask app's config.
+    """
+
+    INERTIA_JSON_ENCODER = InertiaJsonEncoder
+    INERTIA_ENCRYPT_HISTORY = False
+    INERTIA_SSR_ENABLED = False
+    INERTIA_SSR_URL = "http://localhost:13714"
+    INERTIA_ROOT = "app"
+    INERTIA_VITE_ORIGIN = "http://localhost:5173"
+    INERTIA_VITE_CLIENT = "client"
+    INERTIA_VITE_SERVER = "server"
+    INERTIA_VITE_MANIFEST = "manifest.json"
+    INERTIA_VITE_SSR_MANIFEST = "manifest.json"
+    INERTIA_VITE_STATIC = "static"
+    INERTIA_VITE_DIR = "inertia"
+
+
+def init_settings(app):
+    """Initialize Inertia settings while preserving existing app configurations."""
+    defaults = {
+        key: value for key, value in vars(Settings).items() if not key.startswith("_")
+    }
+
+    # Only set values that aren't already in app.config
+    for key, value in defaults.items():
+        if key not in app.config:
+            app.config[key] = value
