e8b747cccbc78e6d81192069d9bbe100f840127d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 03:15:48 +0000

    Add package odoo-addon-stock-picking-auto-create-lot/16.0.3.1.0/odoo_addon_stock_picking_auto_create_lot-16.0.3.1.0-py3-none-any.whl

---
+++ packages/odoo-addon-stock-picking-auto-create-lot/odoo_addon_stock_picking_auto_create_lot-16.0.3.1.0-py3-none-any.whl/odoo/addons/stock_picking_auto_create_lot/models/__init__.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import product
+from . import product_category
+from . import stock_picking
+from . import stock_picking_type
+from . import stock_move
+from . import stock_move_line
