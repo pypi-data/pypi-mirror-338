465a3d97da88f996cd80eb876c523f0bdbb7f61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:46 +0000

    Add package eric-ide/25.4/eric_ide-25.4.tar.gz

---
+++ packages/eric-ide/eric_ide-25.4.tar.gz/eric_ide-25.4/src/eric7/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 - 2025 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module containing some eric-ide version information.
+"""
+
+Version = "25.4 (rev. 9a1cb597911f)"
+VersionOnly = "25.4"
