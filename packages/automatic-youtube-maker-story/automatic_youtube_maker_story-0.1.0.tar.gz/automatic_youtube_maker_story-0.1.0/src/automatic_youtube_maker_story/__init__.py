b6d046eac2237a260b497a3ed38df17a291a803f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:28 +0000

    Add package automatic-youtube-maker-story/0.1.0/automatic_youtube_maker_story-0.1.0.tar.gz

---
+++ packages/automatic-youtube-maker-story/automatic_youtube_maker_story-0.1.0.tar.gz/automatic_youtube_maker_story-0.1.0/src/automatic_youtube_maker_story/__init__.py
@@ -0,0 +1,4 @@
+from .install_packeges import configPaths, start
+from .maneger import stepComplete, statusProdution, checkVideos
+
+__all__ = ['configPaths','start','stepComplete', 'statusProdution', 'checkVideos']
