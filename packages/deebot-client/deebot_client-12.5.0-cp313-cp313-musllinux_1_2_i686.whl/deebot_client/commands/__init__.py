c043295bd0d0a5873877696e17e253e36fb20467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:06:07 +0000

    Add package deebot-client/12.5.0/deebot_client-12.5.0-cp313-cp313-musllinux_1_2_i686.whl

---
+++ packages/deebot-client/deebot_client-12.5.0-cp313-cp313-musllinux_1_2_i686.whl/deebot_client/commands/__init__.py
@@ -0,0 +1,29 @@
+"""Commands module."""
+
+from __future__ import annotations
+
+from enum import IntEnum, unique
+from typing import TYPE_CHECKING
+
+from deebot_client.const import DataType
+
+from .json import (
+    COMMANDS as JSON_COMMANDS,
+    COMMANDS_WITH_MQTT_P2P_HANDLING as JSON_COMMANDS_WITH_MQTT_P2P_HANDLING,
+)
+
+if TYPE_CHECKING:
+    from deebot_client.command import Command, CommandMqttP2P
+
+COMMANDS: dict[DataType, dict[str, type[Command]]] = {DataType.JSON: JSON_COMMANDS}
+
+COMMANDS_WITH_MQTT_P2P_HANDLING: dict[DataType, dict[str, type[CommandMqttP2P]]] = {
+    DataType.JSON: JSON_COMMANDS_WITH_MQTT_P2P_HANDLING
+}
+
+
+@unique
+class StationAction(IntEnum):
+    """Enum class for all possible station actions."""
+
+    EMPTY_DUSTBIN = 1
