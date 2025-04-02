2a672ee947c7f48e3118b63631cf4e4d59f67ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 04:05:16 +0000

    Add package wbportfolio/1.49.6/wbportfolio-1.49.6-py2.py3-none-any.whl

---
+++ packages/wbportfolio/wbportfolio-1.49.6-py2.py3-none-any.whl/wbportfolio/filters/__init__.py
@@ -0,0 +1,32 @@
+from .assets import (  # ProductAllocationEquityFilter, AssetPositionAPIFilter, AssetPositionDateFilter, ContributionChartFilter, AssetPositionIndexFilter, AssetPositionInstrumentFilter, EquityCashPositionFilter,  AssetPositionEquityFilter
+    AssetPositionEquityGroupFilter,
+    AssetPositionFilter,
+    AssetPositionIndexGroupFilter,
+    AssetPositionInstrumentFilter,
+    AssetPositionPortfolioFilter,
+    AssetPositionProductGroupFilter,
+    AssetPositionUnderlyingInstrumentChartFilter,
+    CashPositionPortfolioFilterSet,
+    CompositionModelPortfolioPandasFilter,
+    ContributionChartFilter,
+    DistributionFilter,
+)
+from .custodians import CustodianFilterSet
+from .performances import (
+    PerformanceComparisonFilter,
+    PerformancePandasFilter,
+    ProductPerformanceNetNewMoneyFilter,
+)
+from .portfolios import PortfolioFilterSet, PortfolioTreeGraphChartFilterSet
+from .positions import AggregatedAssetPositionLiquidityFilter, AssetPositionPandasFilter
+from .roles import PortfolioRoleFilterSet
+from .signals import *
+from .transactions import *
+from .products import (
+    BaseProductFilterSet,
+    ProductFilter,
+    ProductCustomerFilter,
+    ProductFeeFilter,
+)
+from .assets_and_net_new_money_progression import AssetsAndNetNewMoneyProgressionFilterSet
+from .esg import ESGMetricAggregationPortfolioPandasFilterSet
