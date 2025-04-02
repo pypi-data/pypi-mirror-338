45499226d288f71a37b254a69003ad7352ba3bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 03:15:59 +0000

    Add package nonebot-plugin-ottohzys/1.0.0.post1/nonebot_plugin_ottohzys-1.0.0.post1.tar.gz

---
+++ packages/nonebot-plugin-ottohzys/nonebot_plugin_ottohzys-1.0.0.post1.tar.gz/nonebot_plugin_ottohzys-1.0.0.post1/nonebot_plugin_ottohzys/migrate.py
@@ -0,0 +1,31 @@
+import shutil
+from pathlib import Path
+
+from nonebot import logger
+
+from .config import DATA_DIR
+from .pack.pack import (
+    TOKENS_DIRNAME,
+    YSDD_TOKENS_DIRNAME,
+    YSDD_TOKENS_FILENAME,
+)
+
+OLD_DATA_DIR = Path("data/ottohzys")
+
+
+def v0_migrate():
+    if not OLD_DATA_DIR.exists():
+        return
+
+    logger.info("Migrating data from old version")
+
+    pack_dir = DATA_DIR / "otto"
+
+    shutil.copytree(OLD_DATA_DIR / "tokens", pack_dir / TOKENS_DIRNAME)
+    shutil.copytree(OLD_DATA_DIR / "ysddTokens", pack_dir / YSDD_TOKENS_DIRNAME)
+    # shutil.copy(OLD_DATA_DIR / "chinglish.json", pack_dir / CHINGLISH_FILENAME)
+    shutil.copy(OLD_DATA_DIR / "ysdd.json", pack_dir / YSDD_TOKENS_FILENAME)
+
+    shutil.rmtree(OLD_DATA_DIR)
+
+    logger.info("Successfully migrated data from old version")
