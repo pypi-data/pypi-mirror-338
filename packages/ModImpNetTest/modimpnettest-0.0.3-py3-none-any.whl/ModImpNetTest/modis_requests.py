8040c7cbf5ee738561aaf2ff09cda3483d507d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:56 +0000

    Add package ModImpNetTest/0.0.3/modimpnettest-0.0.3-py3-none-any.whl

---
+++ packages/ModImpNetTest/modimpnettest-0.0.3-py3-none-any.whl/ModImpNetTest/modis_requests.py
@@ -0,0 +1,57 @@
+"""This module has functions that allow for authentication, task submission, download"""
+
+import time
+from datetime import datetime
+import os
+import requests
+import pandas as pd
+
+##################################################################################
+##################################################################################
+
+def create_log(dest_dir):
+    """Creates a .txt log for MODIS task request submission and download.
+
+    Parameters
+    ----------
+    dest_dir: str
+        A directory that where the log file will be created.
+
+    Returns
+    -------
+    logfilepath: str
+        path to the log file
+    """
+    datetime_now = datetime.now().strftime('%Y%m%d%H%M%S')
+    logfile = f'MODIS_log_{datetime_now}.txt'
+    logfilepath = os.path.join(dest_dir, logfile)
+
+    with open(logfilepath,'a+', encoding="utf-8") as _:
+        pass
+
+    return logfilepath
+
+def write_product_metadata(logfilepath, product_id):
+    """Writes product (MODIS) metadata to .txt log file.
+
+    Parameters
+    ----------
+    logfilepath: str
+        path to the log file
+    product_id: str
+    """
+
+    params = {'pretty': True}
+    response = requests.get(
+        f'https://appeears.earthdatacloud.nasa.gov/api/product/{product_id}',
+        params=params, timeout=86400)
+    dataset_metadata = response.text
+
+    with open(logfilepath,'a+', encoding="utf-8") as f:
+        f.write('Log file for MODIS download\n \n')
+        f.write('#' * 80 + '\n')
+        f.write('#' * 80 + '\n')
+        f.write('\n \n')
+        f.write(f'Information about the MODIS product with product_id={product_id}: \n \n')
+        f.write(dataset_metadata)
+        f.write('\n \n')
