a92083ced212c1166bfe4d404ed5840ac89f8ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:18 +0000

    Add package algebraic-immunity-utils/1.6.0/algebraic_immunity_utils-1.6.0-cp310-cp310-win_amd64.whl

---
+++ packages/algebraic-immunity-utils/algebraic_immunity_utils-1.6.0-cp310-cp310-win_amd64.whl/algebraic_immunity_utils/__init__.py
@@ -0,0 +1,5 @@
+from .algebraic_immunity_utils import *
+
+__doc__ = algebraic_immunity_utils.__doc__
+if hasattr(algebraic_immunity_utils, "__all__"):
+    __all__ = algebraic_immunity_utils.__all__
