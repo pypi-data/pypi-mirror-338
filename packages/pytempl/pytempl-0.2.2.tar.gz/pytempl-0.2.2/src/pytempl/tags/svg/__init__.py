32398456b045dccece03c0249394e9f64efb9b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:51 +0000

    Add package pytempl/0.2.2/pytempl-0.2.2.tar.gz

---
+++ packages/pytempl/pytempl-0.2.2.tar.gz/pytempl-0.2.2/src/pytempl/tags/svg/__init__.py
@@ -0,0 +1,26 @@
+# https://developer.mozilla.org/en-US/docs/Web/SVG
+
+from ._base import BaseSVGAttributes, BaseSVGElement
+from .circle import Circle, CircleAttributes
+from .g import G, GAttributes
+from .line import Line, LineAttributes
+from .path import Path, PathAttributes
+from .rect import Rect, RectAttributes
+from .svg import Svg, SvgAttributes
+
+__all__ = [
+    "BaseSVGElement",
+    "Circle",
+    "CircleAttributes",
+    "G",
+    "GAttributes",
+    "Line",
+    "LineAttributes",
+    "Path",
+    "PathAttributes",
+    "Rect",
+    "RectAttributes",
+    "Svg",
+    "SvgAttributes",
+    "BaseSVGAttributes",
+]
