f0dd2e82996d162b74f053dfc0d87f7343d14808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 03:16:00 +0000

    Add package str2speech/0.3.2/str2speech-0.3.2.tar.gz

---
+++ packages/str2speech/str2speech-0.3.2.tar.gz/str2speech-0.3.2/src/str2speech/spark_tts.py
@@ -0,0 +1,43 @@
+from huggingface_hub import snapshot_download
+from .utils import get_downloads_path
+import os
+from .base_tts import BaseTTS
+from .cloner import Cloner
+import requests
+import subprocess
+
+
+class SparkTTS(BaseTTS):
+    model_name = "SparkAudio/Spark-TTS-0.5B"
+
+    def __init__(self):
+        super().__init__()
+        self.download_model()
+
+    def download_model(self):
+        self.model_dir = get_downloads_path("SparkTTS")
+        if not os.path.exists(self.model_dir):
+            snapshot_download(self.model_name, local_dir=self.model_dir)
+            os.makedirs(os.path.join(self.model_dir, "voices"))
+            default_voice_asset = "https://github.com/hathibelagal-dev/str2speech/raw/refs/heads/main/assets/voices/generic_female.wav"
+            response = requests.get(default_voice_asset)
+            if response.status_code == 200:
+                with open(
+                    os.path.join(self.model_dir, "voices", "generic_female.wav"), "wb"
+                ) as f:
+                    f.write(response.content)
+                    print("Default voice downloaded")
+        else:
+            print("Model already downloaded")
+        try:
+            from sparktts.models.audio_tokenizer import BiCodecTokenizer
+        except:
+            print("Installing sparktts")
+            Cloner.clone_and_install(
+                "https://github.com/hathibelagal-dev/Spark-TTS.git", False
+            )
+
+    def generate(self, prompt, output_file):
+        command = f"sparktts --text '{prompt}' --save_file '{output_file}' --model_dir '{self.model_dir}'"
+        subprocess.run(command, shell=True)
+        print("Audio saved.")
