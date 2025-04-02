8777454407eb2f7225967c92a5c7350e66359df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:53 +0000

    Add package talos-aclient/1.6.0/talos_aclient-1.6.0.tar.gz

---
+++ packages/talos-aclient/talos_aclient-1.6.0.tar.gz/talos_aclient-1.6.0/talos_aclient/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from talos_aclient.api.credentials_api import CredentialsApi
+from talos_aclient.api.dataset_api import DatasetApi
+from talos_aclient.api.default_api import DefaultApi
+from talos_aclient.api.graph_api import GraphApi
+from talos_aclient.api.model_api import ModelApi
+from talos_aclient.api.ontology_api import OntologyApi
+from talos_aclient.api.resource_api import ResourceApi
+from talos_aclient.api.segment_api import SegmentApi
+from talos_aclient.api.task_api import TaskApi
+from talos_aclient.api.vector_api import VectorApi
+
