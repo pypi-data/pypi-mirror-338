b1b9c9565d03bd704f21fe56670a224d12893293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:05:04 +0000

    Add package uipath-sdk/0.0.108/uipath_sdk-0.0.108.tar.gz

---
+++ packages/uipath-sdk/uipath_sdk-0.0.108.tar.gz/uipath_sdk-0.0.108/uipath_sdk/_models/interrupt_models.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel
+
+from .actions import Action
+from .job import Job
+
+
+class InvokeProcess(BaseModel):
+    name: str
+    input_arguments: Optional[Dict[str, Any]]
+
+
+class WaitJob(BaseModel):
+    job: Job
+
+
+class CreateAction(BaseModel):
+    name: Optional[str] = None
+    key: Optional[str] = None
+    title: str
+    data: Optional[Dict[str, Any]] = None
+    app_version: Optional[int] = None
+
+
+class WaitAction(BaseModel):
+    action: Action
