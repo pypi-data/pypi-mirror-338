b8d0ae48ea40126abcdcc24b060951128fb60b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:37 +0000

    Add package pytempl/0.2.3/pytempl-0.2.3-py3-none-any.whl

---
+++ packages/pytempl/pytempl-0.2.3-py3-none-any.whl/pytempl/tags/svg/polygon.py
@@ -0,0 +1,50 @@
+from typing import Dict, List, Literal, Self
+
+from pydantic import ValidationError as PydanticValidationError
+
+from pytempl.errors import ValidationError
+from pytempl.utils import (
+    ValidatorFunction,
+    format_validation_error_message,
+    validate_dictionary_data,
+)
+
+from ._base import BaseSVGAttributes, BaseSVGElement
+
+try:
+    from typing import Unpack
+except ImportError:
+    from typing_extensions import Unpack
+
+
+class PolygonAttributes(BaseSVGAttributes):
+    points: str
+    pathLength: str | Literal["none"]
+
+    @classmethod
+    def validate(
+        cls,
+        data: Dict,
+        default_values: Dict | None = None,
+        custom_validators: List[ValidatorFunction] | None = None,
+    ) -> Self:
+        return validate_dictionary_data(cls, data, default_values, custom_validators)
+
+    @classmethod
+    def set_defaults(cls) -> Dict:
+        return {"points": "", "pathLength": "none"}
+
+
+class Polygon(BaseSVGElement):
+    tag_name = "polygon"
+    have_children = False
+
+    def __init__(self, **attributes: Unpack[PolygonAttributes]):
+        try:
+            validated_attributes = PolygonAttributes.validate(
+                attributes, PolygonAttributes.set_defaults()
+            )
+        except (ValidationError, PydanticValidationError) as err:
+            raise ValueError(format_validation_error_message(err))
+
+        super().__init__(**validated_attributes)
---
+++ packages/pytempl/pytempl-0.2.3-py3-none-any.whl/pytempl/tags/svg/rect.py
@@ -0,0 +1,63 @@
+from typing import Dict, List, Literal, Self
+
+from pydantic import ValidationError as PydanticValidationError
+
+from pytempl.errors import ValidationError
+from pytempl.utils import (
+    ValidatorFunction,
+    format_validation_error_message,
+    validate_dictionary_data,
+)
+
+from ._base import BaseSVGAttributes, BaseSVGElement
+
+try:
+    from typing import Unpack
+except ImportError:
+    from typing_extensions import Unpack
+
+
+class RectAttributes(BaseSVGAttributes):
+    x: str
+    y: str
+    width: str
+    height: str
+    rx: str
+    ry: str
+    pathLength: str | Literal["none"]
+
+    @classmethod
+    def validate(
+        cls,
+        data: Dict,
+        default_values: Dict | None = None,
+        custom_validators: List[ValidatorFunction] | None = None,
+    ) -> Self:
+        return validate_dictionary_data(cls, data, default_values, custom_validators)
+
+    @classmethod
+    def set_defaults(cls) -> Dict:
+        return {
+            "x": 0,
+            "y": 0,
+            "width": "auto",
+            "height": "auto",
+            "rx": "auto",
+            "ry": "auto",
+            "pathLength": "none",
+        }
+
+
+class Rect(BaseSVGElement):
+    tag_name = "rect"
+    have_children = False
+
+    def __init__(self, **attributes: Unpack[RectAttributes]):
+        try:
+            validated_attributes = RectAttributes.validate(
+                attributes, RectAttributes.set_defaults()
+            )
+        except (ValidationError, PydanticValidationError) as err:
+            raise ValueError(format_validation_error_message(err))
+
+        super().__init__(**validated_attributes)
