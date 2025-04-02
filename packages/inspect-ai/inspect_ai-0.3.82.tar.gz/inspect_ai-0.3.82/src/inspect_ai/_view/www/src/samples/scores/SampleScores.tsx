86d8f92e2d0ab0412e782e8f4eb667c6707e9883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:04:57 +0000

    Add package inspect-ai/0.3.82/inspect_ai-0.3.82.tar.gz

---
+++ packages/inspect-ai/inspect_ai-0.3.82.tar.gz/inspect_ai-0.3.82/src/inspect_ai/_view/www/src/samples/scores/SampleScores.tsx
@@ -0,0 +1,33 @@
+import { FC, Fragment } from "react";
+import { SampleSummary } from "../../api/types";
+
+import { useSampleDescriptor } from "../../state/hooks";
+import styles from "./SampleScores.module.css";
+
+interface SampleScoresProps {
+  sample: SampleSummary;
+  scorer: string;
+}
+
+export const SampleScores: FC<SampleScoresProps> = ({ sample, scorer }) => {
+  const samplesDescriptor = useSampleDescriptor();
+  const scores = scorer
+    ? samplesDescriptor?.evalDescriptor
+        .scorerDescriptor(sample, { scorer, name: scorer })
+        .scores()
+    : samplesDescriptor?.selectedScorerDescriptor(sample)?.scores();
+
+  if (scores?.length === 1) {
+    return scores[0].rendered();
+  } else {
+    const rows = scores?.map((score) => {
+      return (
+        <Fragment>
+          <div style={{ opacity: "0.7" }}>{score.name}</div>
+          <div>{score.rendered()}</div>
+        </Fragment>
+      );
+    });
+    return <div className={styles.grid}>{rows}</div>;
+  }
+};
