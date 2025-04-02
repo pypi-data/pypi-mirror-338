e6d728996b2eead4db5957e283d91e7e4a0d1f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:04:10 +0000

    Add package optimed/1.0.0a1/optimed-1.0.0a1-py3-none-any.whl

---
+++ packages/optimed/optimed-1.0.0a1-py3-none-any.whl/optimed/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "1.0.0a1"
+
+
+def _check_availability(name: str) -> bool:
+    import importlib.util
+
+    return importlib.util.find_spec(name) is not None
+
+
+_cupy_available = _check_availability("cupy")
+_cucim_available = _check_availability("cucim")
