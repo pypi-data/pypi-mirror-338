ac1b60d14a1533baff0e59cdd599a395bddf9fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 06:05:56 +0000

    Add package Practs/1.2.3/practs-1.2.3.tar.gz

---
+++ packages/Practs/practs-1.2.3.tar.gz/practs-1.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Practs",
+    version="1.2.3",
+    author="Akash Sharma",
+    author_email="akashsharma8655@gmail.com",
+    description="A simple greeting function",
+    long_description=open("C:\\Users\\Akash\\PycharmProjects\\run-terr\\readme.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/space-add-me/update",  # Replace with your repo URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
