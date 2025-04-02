ade7004d8281b752b3e64dca4331c172639db1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:06:07 +0000

    Add package automonisaur/0.6.19/automonisaur-0.6.19.tar.gz

---
+++ packages/automonisaur/automonisaur-0.6.19.tar.gz/automonisaur-0.6.19/automon/helpers/__init__.py
@@ -0,0 +1,9 @@
+from .dates import Dates
+from .loggingWrapper import LoggingClient, INFO, DEBUG, ERROR, CRITICAL
+from .markdown import Chat, Format
+from .networking import Networking
+from .sleeper import Sleeper
+from .subprocessWrapper import Run
+from .tempfileWrapper import Tempfile
+
+import automon.helpers.loggingWrapper
---
+++ packages/automonisaur/automonisaur-0.6.19.tar.gz/automonisaur-0.6.19/automon/integrations/ollamaWrapper/__init__.py
@@ -0,0 +1,5 @@
+from .client import (
+    OllamaClient,
+)
+
+from .chat import OllamaChat
