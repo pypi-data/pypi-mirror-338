98ea1541a910f21aad9877718947b6bdee2dd5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 03:15:44 +0000

    Add package celltypemark/0.1.0/celltypemark-0.1.0-py3-none-any.whl

---
+++ packages/celltypemark/celltypemark-0.1.0-py3-none-any.whl/celltypemark/resource.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+import pathlib
+
+def download_resource(resource_url: str='https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=HuBMAP_ASCT_plus_B_augmented_w_RNAseq_Coexpression', save_path: str=pathlib.Path(__file__).parent.joinpath("HuBMAP_ASCT_plus_B_augmented_w_RNAseq_Coexpression.txt")):
+    ## download from given url and save to given path
+    import requests
+    response = requests.get(resource_url)
+    with open(save_path, 'w') as hubmap:
+        hubmap.write(response.text)
+
+def load_resource(resource_path: str=pathlib.Path(__file__).parent.joinpath("HuBMAP_ASCT_plus_B_augmented_w_RNAseq_Coexpression.txt")) -> defaultdict:
+    ## if resource_path is not exist, download it
+    if not resource_path.exists():
+        download_resource()
+    marker_genes = defaultdict(list)
+    with open(resource_path, 'r') as hubmap:
+        for line in hubmap:
+            marker_genes[line.strip().split('\t\t')[0]] = line.strip().split('\t\t')[1].split('\t')
+    return marker_genes
