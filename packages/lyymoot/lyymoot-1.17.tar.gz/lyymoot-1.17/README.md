71199ba9b4cf71a27faa6426cba7c37cebd4709e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:05:00 +0000

    Add package lyymoot/1.17/lyymoot-1.17.tar.gz

---
+++ packages/lyymoot/lyymoot-1.17.tar.gz/lyymoot-1.17/README.md
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='lyylog',
+    version='1.0.0',
+    author='lyy',
+    author_email='',
+    description='lyylog for lyy',
+    packages=packages=find_packages(),
+    install_requires=[
+        'logging',
+
+    ],
+)
---
+++ packages/lyymoot/lyymoot-1.17.tar.gz/lyymoot-1.17/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='lyymoot',
+    version='1.17',
+    author='yy',
+    author_email='',
+    description='lyymoot for lyy',
+    #packages=find_packages(),
+    packages=['lyymoot'], 
+    package_dir={'lyymoot': '.'},
+    py_modules=['lyymoot','utils','quotes','consts'],
+    license="MIT",
+    install_requires=[
+    ],
+)
