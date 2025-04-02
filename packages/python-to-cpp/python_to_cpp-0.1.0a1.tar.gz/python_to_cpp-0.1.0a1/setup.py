d80d7391015798126f68b3a2f3e267efde23b782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:06:10 +0000

    Add package python-to-cpp/0.1.0a1/python_to_cpp-0.1.0a1.tar.gz

---
+++ packages/python-to-cpp/python_to_cpp-0.1.0a1.tar.gz/python_to_cpp-0.1.0a1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_to_cpp", 
+    version="0.1.0a1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Singhom931",
+    description="Convert Python To C++",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Singhom931/python_cpp",
+    # python_requires=">=3.6",
+)
