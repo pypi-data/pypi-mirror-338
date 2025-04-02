d89b8d66904f51c37f1e5675f20c31aeb7a12285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:04:56 +0000

    Add package talos-loader/0.0.1/talos_loader-0.0.1-py3-none-any.whl

---
+++ packages/talos-loader/talos_loader-0.0.1-py3-none-any.whl/talos_loader/src/schema.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+from talos_aclient import CleanConfig, SplitterConfig, LoaderConfigInput, AutoContextConfig
+
+
+class TalosParseConfig(BaseModel):
+    clean_config: CleanConfig = Field(default_factory=lambda: CleanConfig())
+    splitter_config: SplitterConfig = Field(default_factory=lambda: SplitterConfig())
+    loader_config: LoaderConfigInput = Field(default_factory=lambda: LoaderConfigInput())
+    auto_context_config: AutoContextConfig = Field(default_factory=lambda: AutoContextConfig())
+
+
+class ContentType(str, Enum):
+    title_ = 'title'
+    section_header = 'section_header'
+    image = 'image'
+    text = 'text'
+    table = 'table'
+
+
+class Block(BaseModel):
+    type: ContentType
+    rect: tuple[float, float, float, float] | None = None
+    content: str
+    page_num: int = 0
