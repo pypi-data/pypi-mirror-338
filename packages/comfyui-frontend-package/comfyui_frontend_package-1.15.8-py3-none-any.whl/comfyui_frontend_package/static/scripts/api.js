7bcc76f725787b120d338d005ed14f88811f59c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 07:04:31 +0000

    Add package comfyui-frontend-package/1.15.8/comfyui_frontend_package-1.15.8-py3-none-any.whl

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.15.8-py3-none-any.whl/comfyui_frontend_package/static/scripts/api.js
@@ -0,0 +1,4 @@
+// Shim for scripts/api.ts
+export const PromptExecutionError = window.comfyAPI.api.PromptExecutionError;
+export const ComfyApi = window.comfyAPI.api.ComfyApi;
+export const api = window.comfyAPI.api.api;
