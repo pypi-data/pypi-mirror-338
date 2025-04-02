d926ba7645c76c3f3e1be5542173e1182d014e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 16:05:10 +0000

    Add package ecmhongz/0.2.1/ecmhongz-0.2.1.tar.gz

---
+++ packages/ecmhongz/ecmhongz-0.2.1.tar.gz/ecmhongz-0.2.1/README.md
@@ -0,0 +1,18 @@
+# 使用方法
+
+```python
+
+import ecmhongz.monitor as Monitor
+
+#开始监控(task name随便取，用于给自己区分)
+Monitor.start(task_name = "easy_test", sampling_interval = 0.2, output format = "csv")
+
+# 代码
+
+# 结東监控
+Monitor.stop()
+
+画图代码
+Monitor.draw(table path = '/home/ldaphome/hhz/workspace/easy_test_20250331_161129.csy',format ="csv")
+```
+
---
+++ packages/ecmhongz/ecmhongz-0.2.1.tar.gz/ecmhongz-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ecmhongz",  
+    version="0.2.1",  
+    author="HongzhenHuang",
+    author_email="202130500072@mail.scut.edn.cn", 
+    description="ecmhongz is a Python package for Energy Consumption Monitoring created by HongzhenHuang.",  # 简短描述
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SusCom-Lab/ECM-Tool",  # 你的项目主页
+    install_requires=[
+        "psutil",
+        "mysql-connector-python",  
+        "pandas",
+        "plotly",
+        "deprecated",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  # 适用的 Python 版本
+)
