63bca8006a30b441ca978a722703ee58a430e1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 17:04:40 +0000

    Add package maleo-core/0.2.15/maleo_core-0.2.15-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.2.15-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_shared/transfers/results/service/__init__.py
@@ -0,0 +1,8 @@
+# This file serves all MaleoShared General Results
+
+from .service import MaleoSharedServiceServiceResults
+
+class MaleoSharedServiceResults:
+    Service = MaleoSharedServiceServiceResults
+
+__all__ = ["MaleoSharedServiceResults", "MaleoSharedServiceServiceResults"]
