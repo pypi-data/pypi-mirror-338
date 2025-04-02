09e8d6e011648d4c77bcd6ce3a5b96d979344b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 05:04:58 +0000

    Add package twochar/0.0.3/twochar-0.0.3.tar.gz

---
+++ packages/twochar/twochar-0.0.3.tar.gz/twochar-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="twochar",
+    version="0.0.3",
+    author="Tushar Suredia",
+    description="A simple example Python package",
+    packages=find_packages(),
+    install_requires=[],  
+)
