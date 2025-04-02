61e5468b510ab309e203107398ce02ae8d339a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 03:15:48 +0000

    Add package heimdall-tools/1.3.1/heimdall_tools-1.3.1.tar.gz

---
+++ packages/heimdall-tools/heimdall_tools-1.3.1.tar.gz/heimdall_tools-1.3.1/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+packs= find_packages()
+print(packs)
+
+setup(
+    name='heimdall_tools',
+    version='1.3.1',
+    packages=find_packages(),
+    install_requires=[
+        'boto3', #sqs, sns
+        'redis', # redis_client.py
+        'hvac', #vault
+        'pika' # RabbitMQ
+        # Add any other dependencies here
+    ],
+    author='Robin Thomas',
+    author_email='robin@clockhash.com',
+    description='Custom package including all modules required for my application',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/your_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+)
