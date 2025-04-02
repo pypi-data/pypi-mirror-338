35031675e0eb3ccdac3d9f42554c9da813ffbc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 15:04:32 +0000

    Add package meu-investimento-fiap-val/0.1/meu_investimento_fiap_val-0.1.tar.gz

---
+++ packages/meu-investimento-fiap-val/meu_investimento_fiap_val-0.1.tar.gz/meu_investimento_fiap_val-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_fiap_val',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='Thiago S Adriano',
+   author_email='tadriano.dev@gmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/valdrei/meu_investimento_fiap',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
+
+from setuptools import setup, find_packages
