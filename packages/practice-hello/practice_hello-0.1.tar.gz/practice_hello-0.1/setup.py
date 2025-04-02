a8c64e14686906dd73a7689983704f1b275bad88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 07:04:31 +0000

    Add package practice-hello/0.1/practice_hello-0.1.tar.gz

---
+++ packages/practice-hello/practice_hello-0.1.tar.gz/practice_hello-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='practice_hello',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+        #ex 'numpy>=1.11.1'
+    ],
+)
