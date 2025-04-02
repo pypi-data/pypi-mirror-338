a8839fcf348df10ae41ba3134c377a2c59a448f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-02 14:04:51 +0000

    Add package poetry-setuptools-scm-support/0.5.4/poetry_setuptools_scm_support-0.5.4.tar.gz

---
+++ packages/poetry-setuptools-scm-support/poetry_setuptools_scm_support-0.5.4.tar.gz/poetry_setuptools_scm_support-0.5.4/poetry_setuptools_scm_support/__init__.py
@@ -0,0 +1,79 @@
+from cleo.commands.command import Command
+from cleo.io.inputs.argument import Argument
+from poetry.plugins.application_plugin import ApplicationPlugin
+from poetry.factory import Factory
+
+from setuptools_scm import Configuration, get_version
+from setuptools_scm.version import calver_by_date, release_branch_semver_version
+
+import warnings
+
+def fxn():
+    warnings.warn("deprecated", UserWarning)
+
+
+class CalculateVersion(Command):
+    """
+    Calculates the version of the package.
+
+    command:name {version-calculate : Controls output format}
+    """
+
+    name = "version-calculate"
+    description = "Calculates the version of the project relying on setuptools_scm"
+
+    args_description = """
+        scm: formats according to setuptools_scm <info>get_version</info> default behavior. e.g. <info>0.1.dev1+g1e0ede4</info>.
+        date: formats current date and distance, e.g. <info>2025.4.1.1.dev1+g9d4edec</info> . Scheme used is <info>calver_by_date</info> function
+        branch: Use branch based versioning of library. Scheme used is <info>release_branch_semver_version</info> function
+    """
+    arguments = [
+        Argument(name="format", description=args_description, default="scm", required=False),
+    ]
+
+
+    def __do_scm(self, c: Configuration) -> str:
+        return get_version(root=c.root, relative_to=c.relative_to)
+
+    def __do_date_and_dirty(self, c: Configuration) -> str:
+        return get_version(root=c.root, relative_to=c.relative_to, version_scheme=calver_by_date)
+
+    def __do_semver_branch(self, c: Configuration) -> str:
+        return get_version(root=c.root, relative_to=c.relative_to, version_scheme=release_branch_semver_version)
+
+    def handle(self) -> int:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            poetry = Factory().create_poetry()
+            c = Configuration.from_file(str(poetry.file))
+
+            format_to_use = self.argument("format")
+            if format_to_use == "scm":
+                v = self.__do_scm(c)
+            elif format_to_use == "date":
+                v = self.__do_date_and_dirty(c)
+            elif format_to_use == "branch":
+                v = self.__do_semver_branch(c)
+            else:
+                self.line_error(f"Unknown format: {format_to_use}")
+                return 0
+
+            confirm = self.ask(f'Bumping version "{v}" [Y/n]', 'Y')
+            if confirm == "Y":
+                poetry.pyproject.data.item('project').update(version=v)
+                poetry.pyproject.file.write(poetry.pyproject.data)
+                self.line(f"Version changed to {v}")
+
+            return 0
+
+
+def factory():
+    return CalculateVersion()
+
+class ScmPlugin(ApplicationPlugin):
+    @property
+    def commands(self) -> list[type[Command]]:
+        return [CalculateVersion]
+
+    def activate(self, application):
+        application.command_loader.register_factory("version-calculate", factory)
