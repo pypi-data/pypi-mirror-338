320f539d4e3371229e079e2216e8d857d7024b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:26 +0000

    Add package sonucv/1.2/sonucv-1.2-py3-none-any.whl

---
+++ packages/sonucv/sonucv-1.2-py3-none-any.whl/sonucv/__init__.py
@@ -0,0 +1,12 @@
+ 
+"""
+SonuCV - Interactive Resume Viewer
+
+Installation:
+    pip install sonucv
+
+Usage:
+    - View full resume: resumeviewer
+    - Interactive mode: resumeviewer --interactive
+    - Help: resumeviewer --help
+"""
