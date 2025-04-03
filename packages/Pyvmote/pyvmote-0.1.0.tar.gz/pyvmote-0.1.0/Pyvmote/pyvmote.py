07a6bc9479ca034216ec1e96dbfbe168d062ca56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:07:20 +0000

    Add package Pyvmote/0.1.0/pyvmote-0.1.0.tar.gz

---
+++ packages/Pyvmote/pyvmote-0.1.0.tar.gz/pyvmote-0.1.0/Pyvmote/pyvmote.py
@@ -0,0 +1,58 @@
+from .Graph_generator import Graph
+from .Service import Server
+
+class Pyvmote:
+    def __init__(self):
+        self.gr = Graph()
+        self.sv = Server()
+
+    def start_server(self, puerto):
+        self.sv.start_server(puerto)
+
+    def stop_server(self):
+        self.sv.stop_server()
+        self.gr.clear_history()
+
+    def line_plot(self, x, y, xname="X", yname="Y", title="Line Graph", interactive=False):
+        plot_file = self.gr.line_plot(x, y, xname, yname, title, interactive)
+        if self.sv.start:
+            self.sv.notify_update()
+        return plot_file
+
+    def scatter_plot(self, x, y, xname="X", yname="Y", title="Scatter Plot", interactive=False):
+        plot_file = self.gr.scatter_plot(x, y, xname, yname, title, interactive)
+        if self.sv.start:
+            self.sv.notify_update()
+        return plot_file
+
+    def bar_plot(self, x, y, xname="X", yname="Y", title="Bar Plot", interactive=False):
+        plot_file = self.gr.bar_plot(x, y, xname, yname, title, interactive)
+        if self.sv.start:
+            self.sv.notify_update()
+        return plot_file
+
+    def hist_plot(self, x, xname="Value", yname="Frequency", title="Histogram", bins=20, interactive=False):
+        plot_file = self.gr.hist_plot(x, xname, yname, title, bins, interactive)
+        if self.sv.start:
+            self.sv.notify_update()
+        return plot_file
+
+    def box_plot(self, x, xname="", yname="Value", title="Box Plot", interactive=False):
+        plot_file = self.gr.box_plot(x, xname, yname, title, interactive)
+        if self.sv.start:
+            self.sv.notify_update()
+        return plot_file
+
+    def density_plot(self, x, xname="X", yname="Density", title="Density Plot", interactive=False):
+        plot_file = self.gr.density_plot(x, xname, yname, title, interactive)
+        if self.sv.start:
+            self.sv.notify_update()
+        return plot_file
+    
+    def export_graph(self, title, extension="jpg", target_folder="exports"):
+        ruta = self.gr.save_as_format(title, extension, target_folder)
+        print(ruta)
+
+
+
+
