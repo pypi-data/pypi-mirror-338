5a847d0914b92fa4f3e550b4ff293a2c64283165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 15:06:35 +0000

    Add package kumo-api/0.6.1/kumo_api-0.6.1-py3-none-any.whl

---
+++ packages/kumo-api/kumo_api-0.6.1-py3-none-any.whl/kumoapi/explain/__init__.py
@@ -0,0 +1,18 @@
+from .gradient import GraphGradientScore, TableGradientScore
+from .subgraph import (
+    Feature,
+    SubgraphNode,
+    AggregatedNodes,
+    SubgraphStore,
+    SubgraphFeatureStore,
+)
+
+__all__ = [
+    'TableGradientScore',
+    'GraphGradientScore',
+    'Feature',
+    'SubgraphNode',
+    'AggregatedNodes',
+    'SubgraphStore',
+    'SubgraphFeatureStore',
+]
