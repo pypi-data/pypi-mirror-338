ae5c69e661f7115bd961c578adddc687a216f261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:11 +0000

    Add package animl/1.4.8/animl-1.4.8.tar.gz

---
+++ packages/animl/animl-1.4.8.tar.gz/animl-1.4.8/src/animl/__init__.py
@@ -0,0 +1,174 @@
+from animl import api
+from animl import classification
+from animl import custom_detector
+from animl import custom_yolo_run
+from animl import detect
+from animl import file_management
+from animl import generator
+from animl import link
+from animl import megadetector
+from animl import models
+from animl import multi_species
+from animl import pipeline
+from animl import plot_boxes
+from animl import reid
+from animl import species
+from animl import split
+from animl import test
+from animl import train
+from animl import utils
+from animl import video_processing
+
+from animl.api import (animl_results_to_md_results, animl_to_md, classify_mp,
+                       csv_converter, detect_mp, detection_category_id_to_name,
+                       main, matchypatchy, miew_embedding, process_videos,
+                       timelapse, viewpoint_estimator,)
+from animl.classification import (classify_with_config, load_model,
+                                  predict_species, save_model,
+                                  sequence_classification,
+                                  single_classification, softmax,
+                                  tensor_to_onnx,)
+from animl.custom_detector import (CustomYOLO,)
+from animl.custom_yolo_run import (main_config, main_paths,)
+from animl.detect import (absolute2relative, detect_MD_batch, get_image_size,
+                          parse_MD, parse_YOLO, process_image,)
+from animl.file_management import (IMAGE_EXTENSIONS, VALID_EXTENSIONS,
+                                   WorkingDirectory, active_times,
+                                   build_file_manifest, check_file, load_data,
+                                   save_data,)
+from animl.generator import (ImageGenerator, ResizeWithPadding, TrainGenerator,
+                             manifest_dataloader, train_dataloader,)
+from animl.link import (remove_link, sort_MD, sort_species, update_labels,)
+from animl.megadetector import (CONF_DIGITS, COORD_DIGITS, MegaDetector,
+                                convert_yolo_to_xywh, truncate_float,
+                                truncate_float_array,)
+from animl.models import (AutoShape, Bottleneck, BottleneckCSP, C3, C3Ghost,
+                          C3SPP, C3TR, C3x, Classify, Concat, Contract, Conv,
+                          CrossConv, DWConv, DWConvTranspose2d, Detect,
+                          DetectMultiBackend, Detections, Expand, FILE, Focus,
+                          GhostBottleneck, GhostConv, Model, ROOT, SPP, SPPF,
+                          TransformerBlock, TransformerLayer, autopad,
+                          check_anchor_order, common, parse_model, yolo,)
+from animl.multi_species import (multi_species_detection,)
+from animl.pipeline import (from_config, from_paths,)
+from animl.plot_boxes import (demo_boxes, draw_bounding_boxes, main,
+                              plot_all_bounding_boxes,)
+from animl.reid import (ArcFaceLossAdaptiveMargin, ArcFaceSubCenterDynamic,
+                        ArcMarginProduct, ArcMarginProduct_subcenter,
+                        ElasticArcFace, GeM, IMAGE_HEIGHT, IMAGE_WIDTH,
+                        MiewIdNet, ViewpointModel, heads, l2_norm, load,
+                        miewid, viewpoint, weights_init_classifier,
+                        weights_init_kaiming,)
+from animl.species import (ConvNeXtBase, EfficientNet,)
+from animl.split import (get_animals, get_animals_custom, get_empty,
+                         get_empty_custom, train_val_test,)
+from animl.test import (main, test_func,)
+from animl.train import (init_seed, main, train_func, validate,)
+from animl.utils import (AUTOINSTALL, Albumentations, BAR_FORMAT, CONFIG_DIR,
+                         DATASETS_DIR, EarlyStopping, FILE, FONT, HELP_URL,
+                         IMG_FORMATS, InfiniteDataLoader, LOCAL_RANK, LOGGER,
+                         LoadImages, LoadImagesAndLabels, LoadStreams,
+                         LoadWebcam, ModelEMA, NCOLS, NUM_THREADS, Profile,
+                         RANK, ROOT, Timeout, VERBOSE, VID_FORMATS,
+                         WorkingDirectory, apply_classifier, augment_hsv,
+                         augmentations, autosplit, bbox_iou, box_area,
+                         box_candidates, box_iou, check_amp, check_dataset,
+                         check_file, check_font, check_git_status,
+                         check_img_size, check_imshow, check_online,
+                         check_python, check_requirements, check_suffix,
+                         check_version, check_yaml, clean_str, clip_coords,
+                         coco80_to_coco91_class, colorstr, copy_attr,
+                         copy_paste, create_dataloader, create_folder, cutout,
+                         dataloaders, dataset_stats, de_parallel, device_count,
+                         download, emojis, exif_size, exif_transpose,
+                         extract_boxes, file_age, file_date, file_size,
+                         find_modules, fitness, flatten_recursive,
+                         fuse_conv_and_bn, general, get_device, get_hash,
+                         get_latest_run, git_describe, gsutil_getsize,
+                         hist_equalize, img2label_paths, imread, imshow,
+                         imshow_, imwrite, increment_path, init_seeds,
+                         initialize_weights, intersect_dicts, is_ascii,
+                         is_chinese, is_colab, is_docker, is_kaggle,
+                         is_parallel, is_pip, is_writeable,
+                         labels_to_class_weights, labels_to_image_weights,
+                         letterbox, make_divisible, methods, mixup, model_info,
+                         non_max_suppression, one_cycle, print_args,
+                         print_mutation, profile, prune, random_perspective,
+                         replicate, resample_segments, scale_coords, scale_img,
+                         segment2box, segments2boxes, select_device,
+                         set_logging, sparsity, strip_optimizer, threaded,
+                         time_sync, torch_distributed_zero_first, torch_utils,
+                         try_except, url2file, user_config_dir,
+                         verify_image_label, xyn2xy, xywh2xyxy, xywhn2xyxy,
+                         xyxy2xywh, xyxy2xywhn,)
+from animl.video_processing import (extract_frame_single, extract_frames,)
+
+__all__ = ['AUTOINSTALL', 'Albumentations', 'ArcFaceLossAdaptiveMargin',
+           'ArcFaceSubCenterDynamic', 'ArcMarginProduct',
+           'ArcMarginProduct_subcenter', 'AutoShape', 'BAR_FORMAT',
+           'Bottleneck', 'BottleneckCSP', 'C3', 'C3Ghost', 'C3SPP', 'C3TR',
+           'C3x', 'CONFIG_DIR', 'CONF_DIGITS', 'COORD_DIGITS', 'Classify',
+           'Concat', 'Contract', 'Conv', 'ConvNeXtBase', 'CrossConv',
+           'CustomYOLO', 'DATASETS_DIR', 'DWConv', 'DWConvTranspose2d',
+           'Detect', 'DetectMultiBackend', 'Detections', 'EarlyStopping',
+           'EfficientNet', 'ElasticArcFace', 'Expand', 'FILE', 'FONT', 'Focus',
+           'GeM', 'GhostBottleneck', 'GhostConv', 'HELP_URL',
+           'IMAGE_EXTENSIONS', 'IMAGE_HEIGHT', 'IMAGE_WIDTH', 'IMG_FORMATS',
+           'ImageGenerator', 'InfiniteDataLoader', 'LOCAL_RANK', 'LOGGER',
+           'LoadImages', 'LoadImagesAndLabels', 'LoadStreams', 'LoadWebcam',
+           'MegaDetector', 'MiewIdNet', 'Model', 'ModelEMA', 'NCOLS',
+           'NUM_THREADS', 'Profile', 'RANK', 'ROOT', 'ResizeWithPadding',
+           'SPP', 'SPPF', 'Timeout', 'TrainGenerator', 'TransformerBlock',
+           'TransformerLayer', 'VALID_EXTENSIONS', 'VERBOSE', 'VID_FORMATS',
+           'ViewpointModel', 'WorkingDirectory', 'absolute2relative',
+           'active_times', 'animl_results_to_md_results', 'animl_to_md', 'api',
+           'apply_classifier', 'augment_hsv', 'augmentations', 'autopad',
+           'autosplit', 'bbox_iou', 'box_area', 'box_candidates', 'box_iou',
+           'build_file_manifest', 'check_amp', 'check_anchor_order',
+           'check_dataset', 'check_file', 'check_font', 'check_git_status',
+           'check_img_size', 'check_imshow', 'check_online', 'check_python',
+           'check_requirements', 'check_suffix', 'check_version', 'check_yaml',
+           'classification', 'classify_mp', 'classify_with_config',
+           'clean_str', 'clip_coords', 'coco80_to_coco91_class', 'colorstr',
+           'common', 'convert_yolo_to_xywh', 'copy_attr', 'copy_paste',
+           'create_dataloader', 'create_folder', 'csv_converter',
+           'custom_detector', 'custom_yolo_run', 'cutout', 'dataloaders',
+           'dataset_stats', 'de_parallel', 'demo_boxes', 'detect',
+           'detect_MD_batch', 'detect_mp', 'detection_category_id_to_name',
+           'device_count', 'download', 'draw_bounding_boxes', 'emojis',
+           'exif_size', 'exif_transpose', 'extract_boxes',
+           'extract_frame_single', 'extract_frames', 'file_age', 'file_date',
+           'file_management', 'file_size', 'find_modules', 'fitness',
+           'flatten_recursive', 'from_config', 'from_paths',
+           'fuse_conv_and_bn', 'general', 'generator', 'get_animals',
+           'get_animals_custom', 'get_device', 'get_empty', 'get_empty_custom',
+           'get_hash', 'get_image_size', 'get_latest_run', 'git_describe',
+           'gsutil_getsize', 'heads', 'hist_equalize', 'img2label_paths',
+           'imread', 'imshow', 'imshow_', 'imwrite', 'increment_path',
+           'init_seed', 'init_seeds', 'initialize_weights', 'intersect_dicts',
+           'is_ascii', 'is_chinese', 'is_colab', 'is_docker', 'is_kaggle',
+           'is_parallel', 'is_pip', 'is_writeable', 'l2_norm',
+           'labels_to_class_weights', 'labels_to_image_weights', 'letterbox',
+           'link', 'load', 'load_data', 'load_model', 'main', 'main_config',
+           'main_paths', 'make_divisible', 'manifest_dataloader',
+           'matchypatchy', 'megadetector', 'methods', 'miew_embedding',
+           'miewid', 'mixup', 'model_info', 'models', 'multi_species',
+           'multi_species_detection', 'non_max_suppression', 'one_cycle',
+           'parse_MD', 'parse_YOLO', 'parse_model', 'pipeline',
+           'plot_all_bounding_boxes', 'plot_boxes', 'predict_species',
+           'print_args', 'print_mutation', 'process_image', 'process_videos',
+           'profile', 'prune', 'random_perspective', 'reid',
+           'remove_link', 'replicate', 'resample_segments', 'save_data',
+           'save_model', 'scale_coords', 'scale_img', 'segment2box',
+           'segments2boxes', 'select_device', 'sequence_classification',
+           'set_logging', 'single_classification', 'softmax', 'sort_MD',
+           'sort_species', 'sparsity', 'species', 'split', 'strip_optimizer',
+           'tensor_to_onnx', 'test', 'test_func', 'threaded', 'time_sync',
+           'timelapse', 'torch_distributed_zero_first', 'torch_utils', 'train',
+           'train_dataloader', 'train_func', 'train_val_test',
+           'truncate_float', 'truncate_float_array', 'try_except',
+           'update_labels', 'url2file', 'user_config_dir', 'utils', 'validate',
+           'verify_image_label', 'video_processing', 'viewpoint',
+           'viewpoint_estimator', 'weights_init_classifier',
+           'weights_init_kaiming', 'xyn2xy', 'xywh2xyxy', 'xywhn2xyxy',
+           'xyxy2xywh', 'xyxy2xywhn', 'yolo']
---
+++ packages/animl/animl-1.4.8.tar.gz/animl-1.4.8/src/animl/train.py
@@ -0,0 +1,303 @@
+'''
+    Training script. Here, we load the training and validation datasets (and
+    data loaders) and the model and train and validate the model accordingly.
+
+    Original script from
+    2022 Benjamin Kellenberger
+
+    Modiefied by Peter van Lunteren 2024
+'''
+import argparse
+import yaml
+from tqdm import trange
+import pandas as pd
+import random
+import torch.nn as nn
+import torch
+from torch.backends import cudnn
+from torch.optim import SGD
+from sklearn.metrics import precision_score, recall_score
+from torch.optim.lr_scheduler import ReduceLROnPlateau, LambdaLR
+
+from animl.generator import train_dataloader
+from animl.classification import save_model, load_model
+
+# # log values using comet ml (comet.com)
+# from comet_ml import Experiment
+# from comet_ml.integration.pytorch import log_model
+# experiment = Experiment()
+
+
+def init_seed(seed):
+    '''
+        Initalizes the seed for all random number generators used. This is
+        important to be able to reproduce results and experiment with different
+        random setups of the same code and experiments.
+
+        Args:
+            - seed (int): seed for RNG
+    '''
+    if seed is not None:
+        random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        cudnn.benchmark = True
+        cudnn.deterministic = True
+
+
+def train_func(data_loader, model, optimizer, device='cpu'):
+    '''
+        Training Function
+
+        Args:
+            - data_loader: dataloader object
+            - model: loaded model object
+            - optimizer: optimizer object
+            - device (str): device to load model and data to
+
+        Returns:
+            - loss_total: loss for epoch
+            - oa_total: overall accuracy for epoch
+    '''
+    model.to(device)
+    model.train()  # put the model into training mode
+
+    # loss function
+    criterion = nn.CrossEntropyLoss()
+
+    # log the loss and overall accuracy (OA)
+    loss_total, oa_total = 0.0, 0.0
+
+    progressBar = trange(len(data_loader))
+    for idx, batch in enumerate(data_loader):
+        # put data and labels on device
+        data = batch[0]
+        labels = batch[1]
+        data, labels = data.to(device), labels.to(device)
+        # forward pass
+        prediction = model(data)
+        # reset gradients to zero
+        optimizer.zero_grad()
+
+        loss = criterion(prediction, labels)
+        # calculate gradients of current batch
+        loss.backward()
+        # apply gradients to model parameters
+        optimizer.step()
+
+        loss_total += loss.item()
+
+        pred_label = torch.argmax(prediction, dim=1)
+
+        oa = torch.mean((pred_label == labels).float())
+        oa_total += oa.item()
+
+        progressBar.set_description(
+            '[Train] Loss: {:.2f}; OA: {:.2f}%'.format(
+                loss_total/(idx+1),
+                100*oa_total/(idx+1)
+            )
+        )
+        progressBar.update(1)
+
+    # end of epoch
+    progressBar.close()
+    loss_total /= len(data_loader)
+    oa_total /= len(data_loader)
+
+    return loss_total, oa_total
+
+
+def validate(data_loader, model, device="cpu"):
+    '''
+        Validation function. Note that this looks almost the same as the training
+        function, except that we don't use any optimizer or gradient steps.
+
+    Args:
+        - data_loader: dataloader object
+        - model: loaded model object
+        - device (str): device to load model and data to
+
+    Returns:
+        - loss_total: loss for validation set
+        - oa_total: accuracy for validation set
+        - precision: precision for validation set
+        - recall: recall for validation set
+    '''
+    model.to(device)
+    model.eval()  # put the model into evaluation mode
+
+    criterion = nn.CrossEntropyLoss()
+
+    # log the loss and overall accuracy (OA)
+    loss_total, oa_total = 0.0, 0.0
+
+    # create empty lists for true and predicted labels
+    true_labels = []
+    pred_labels = []
+
+    progressBar = trange(len(data_loader))
+    with torch.no_grad():  # gradients not necessary for validation
+        for idx, batch in enumerate(data_loader):
+            data = batch[0]
+            labels = batch[1]
+            data, labels = data.to(device), labels.to(device)
+
+            # add true labels to the true labels list
+            labels_np = labels.cpu().detach().numpy()
+            true_labels.extend(labels_np)
+
+            # forward pass
+            prediction = model(data)
+
+            # loss
+            loss = criterion(prediction, labels)
+
+            # log statistics
+            loss_total += loss.item()
+
+            pred_label = torch.argmax(prediction, dim=1)
+            oa = torch.mean((pred_label == labels).float())
+            oa_total += oa.item()
+
+            # add predicted labels to the predicted labels list
+            pred_label_np = pred_label.cpu().detach().numpy()
+            pred_labels.extend(pred_label_np)
+
+            progressBar.set_description(
+                '[Val  ] Loss: {:.2f}; OA: {:.2f}%'.format(
+                    loss_total/(idx+1),
+                    100*oa_total/(idx+1)
+                )
+            )
+            progressBar.update(1)
+
+    # end of epoch; finalize
+    progressBar.close()
+    loss_total /= len(data_loader)
+    oa_total /= len(data_loader)
+
+    # calculate precision and recall
+    precision = precision_score(true_labels, pred_labels, average="weighted")
+    recall = recall_score(true_labels, pred_labels, average="weighted")
+
+    return loss_total, oa_total, precision, recall
+
+
+def main():
+    '''
+    Command line function
+
+    Example usage :
+    > python train.py --config configs/exp_resnet18.yaml
+    '''
+    parser = argparse.ArgumentParser(description='Train deep learning model.')
+    parser.add_argument('--config', help='Path to config file', default='exp_resnet18.yaml')
+    args = parser.parse_args()
+
+    # load config
+    print(f'Using config "{args.config}"')
+    cfg = yaml.safe_load(open(args.config, 'r'))
+
+    # init random number generator seed (set at the start)
+    init_seed(cfg.get('seed', None))
+    crop = cfg.get('crop', False)
+    file_col = cfg.get('file_col', 'FilePath') 
+    label_col = cfg.get('label_col', 'species') 
+
+    # check if GPU is available
+    device = cfg.get('device', 'cpu')
+    if device != 'cpu' and not torch.cuda.is_available():
+        print(f'WARNING: device set to "{device}" but CUDA not available; falling back to CPU...')
+        device = 'cpu'
+
+    # initialize model and get class list
+    model, classes, current_epoch = load_model(cfg['experiment_folder'], cfg['class_file'], device=device, architecture=cfg['architecture'])
+
+    categories = dict([[x[cfg.get('class_list_label', 'class')], x[ cfg.get('class_list_index', 'id')]] for _, x in classes.iterrows()])
+
+    # load datasets
+    train_dataset = pd.read_csv(cfg['training_set']).reset_index(drop=True)
+    validate_dataset = pd.read_csv(cfg['validate_set']).reset_index(drop=True)
+
+    # initialize data loaders for training and validation set
+    dl_train = train_dataloader(train_dataset, categories, batch_size=cfg['batch_size'], workers=cfg['num_workers'],
+                                file_col=file_col, label_col=label_col, crop=crop, augment=cfg.get('augment', True))
+    dl_val = train_dataloader(validate_dataset, categories, batch_size=cfg['batch_size'], workers=cfg['num_workers'],
+                              file_col=file_col, label_col=label_col, crop=crop, augment=False)
+
+    # set up model optimizer
+    optim = SGD(model.parameters(), lr=cfg['learning_rate'], weight_decay=cfg['weight_decay'])
+
+    # initialize scheduler
+    if cfg.get("scheduler", True):
+        scheduler = ReduceLROnPlateau(optim, mode='min', factor=0.5, patience=3)
+    else:  # do nothing scheduler
+        scheduler = LambdaLR(optim, lr_lambda=lambda epoch: 1)
+
+    # initialize training arguments
+    numEpochs = cfg['num_epochs']
+    if 'patience' in cfg:
+        patience = cfg['patience']
+        early_stopping = True
+        best_val_loss = float('inf')
+        epochs_no_improve = 0
+        print(f"Early stopping enabled with a patience of {patience} epochs")
+    else:
+        early_stopping = False
+
+    # training loop
+    while current_epoch < numEpochs:
+        current_epoch += 1
+        print(f'Epoch {current_epoch}/{numEpochs}')
+        print(f"Using learning rate : {scheduler.get_last_lr()[0]}")
+
+        loss_train, oa_train = train_func(dl_train, model, optim, device)
+        loss_val, oa_val, precision, recall = validate(dl_val, model, device)
+
+        # combine stats and save
+        stats = {
+            'num_classes': len(classes),
+            'loss_train': loss_train,
+            'loss_val': loss_val,
+            'oa_train': oa_train,
+            'oa_val': oa_val,
+            'precision': precision,
+            'recall': recall
+        }
+
+        # <current_epoch>.pt checkpoint saving every *checkpoint_frequency* epochs
+        checkpoint = cfg.get('checkpoint_frequency', 10)
+        # experiment.log_metrics(stats, step=current_epoch)
+        if current_epoch % checkpoint == 0:
+            save_model(cfg['experiment_folder'], current_epoch, model, stats)
+
+        # if user specified early stopping
+        if early_stopping:
+            # best.pt saving
+            if loss_val < best_val_loss:
+                best_val_loss = loss_val
+                epochs_no_improve = 0
+                save_model(cfg['experiment_folder'], 'best', model, stats)
+                print(f"Current best model saved at epoch {current_epoch} with ...")
+                print(f"     val loss : {best_val_loss:.5f}")
+                print(f"       val OA : {oa_val:.5f}")
+                print(f"val precision : {precision:.5f}")
+                print(f"   val recall : {recall:.5f}\n")
+            else:
+                epochs_no_improve += 1
+
+            # last.pt saving
+            save_model(cfg['experiment_folder'], 'last', model, stats)
+
+            # check patience
+            if epochs_no_improve >= patience:
+                print(f"Early stopping triggered after {patience} epochs without improvement.")
+                break
+
+        # step the scheduler with the validation loss
+        scheduler.step(loss_val)
+
+
+if __name__ == '__main__':
+    main()
