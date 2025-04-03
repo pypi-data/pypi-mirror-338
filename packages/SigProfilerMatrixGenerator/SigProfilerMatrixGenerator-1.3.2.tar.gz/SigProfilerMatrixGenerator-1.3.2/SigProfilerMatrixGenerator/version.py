b2e641887eb98d73bf9c74afdc93a16bf446e700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:26 +0000

    Add package SigProfilerMatrixGenerator/1.3.2/SigProfilerMatrixGenerator-1.3.2.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.3.2.tar.gz/SigProfilerMatrixGenerator-1.3.2/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.3.2'
+version = '1.3.2'
+Update = 'v1.3.2: Add mm39 and rn7 context distribution files and update the respective script.'
+
+	
