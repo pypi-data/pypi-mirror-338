9f0276027d10253189f61071946846f372e8e140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 15:06:16 +0000

    Add package rkmath/0.1.2/rkmath-0.1.2.tar.gz

---
+++ packages/rkmath/rkmath-0.1.2.tar.gz/rkmath-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rkmath',
+    packages=find_packages(include=['rmath']),
+    version='0.1.2',
+    description='Math library for easy math operations',
+    long_description_content_type="text/markdown",
+    author='Swifterhtmler',
+    install_requires=["numpy==2.20.0"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
+
