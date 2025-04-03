b7075cb3b5875f5dd87b12a0009410210444e548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:20 +0000

    Add package torch-transform-image/0.0.2/torch_transform_image-0.0.2.tar.gz

---
+++ packages/torch-transform-image/torch_transform_image-0.0.2.tar.gz/torch_transform_image-0.0.2/src/torch_transform_image/transforms_2d.py
@@ -0,0 +1,36 @@
+from typing import Literal
+
+import einops
+import torch
+from torch_affine_utils import homogenise_coordinates
+from torch_grid_utils import coordinate_grid
+from torch_image_interpolation import sample_image_2d
+
+
+def affine_transform_image_2d(
+        image: torch.Tensor,
+        matrices: torch.Tensor,
+        interpolation: Literal['nearest', 'bilinear', 'bicubic'],
+        yx_matrices: bool = False,
+) -> torch.Tensor:
+    # grab image dimensions
+    h, w = image.shape[-2:]
+
+    if not yx_matrices:
+        matrices[..., :2, :2] = (
+            torch.flip(matrices[..., :2, :2], dims=(-2, -1))
+        )
+        matrices[..., :2, 2] = torch.flip(matrices[..., :2, 2], dims=(-1,))
+
+    # generate grid of pixel coordinates
+    grid = coordinate_grid(image_shape=(h, w), device=image.device)
+
+    # apply matrix to coordinates
+    grid = homogenise_coordinates(grid)  # (h, w, yxw)
+    grid = einops.rearrange(grid, 'h w yxw -> h w yxw 1')
+    grid = matrices @ grid
+    grid = grid[..., :2, 0]  # dehomogenise coordinates: (..., h, w, yxw, 1) -> (..., h, w, yx)
+
+    # sample image at transformed positions
+    result = sample_image_2d(image, coordinates=grid, interpolation=interpolation)
+    return result
