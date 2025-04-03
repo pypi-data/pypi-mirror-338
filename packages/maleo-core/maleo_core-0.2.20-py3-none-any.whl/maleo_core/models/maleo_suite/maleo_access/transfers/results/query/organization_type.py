2dc8b2bc1a3a22268980610cdb369c87a3324f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:00 +0000

    Add package maleo-core/0.2.20/maleo_core-0.2.20-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.2.20-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/results/query/organization_type.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from pydantic import Field
+from typing import Optional
+from maleo_core.models.base.transfers.results.services.query import BaseServiceQueryResults
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.organization_type import MaleoAccessOrganizationTypeGeneralTransfers
+
+class MaleoAccessOrganizationTypeQueryResults:
+    class Get(MaleoAccessOrganizationTypeGeneralTransfers.Base, BaseServiceQueryResults.Get): pass
+
+    Fail = BaseServiceQueryResults.Fail
+
+    class SingleData(BaseServiceQueryResults.SingleData):
+        data:Optional[MaleoAccessOrganizationTypeQueryResults.Get]
+
+    class MultipleData(BaseServiceQueryResults.MultipleData):
+        data:list[MaleoAccessOrganizationTypeQueryResults.Get]
