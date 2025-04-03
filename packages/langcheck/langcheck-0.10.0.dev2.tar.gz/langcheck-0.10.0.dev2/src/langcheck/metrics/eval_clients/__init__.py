3998b7d7d5ca2c720154c839205bd701b2eede52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:07:02 +0000

    Add package langcheck/0.10.0.dev2/langcheck-0.10.0.dev2.tar.gz

---
+++ packages/langcheck/langcheck-0.10.0.dev2.tar.gz/langcheck-0.10.0.dev2/src/langcheck/metrics/eval_clients/__init__.py
@@ -0,0 +1,44 @@
+from langcheck.metrics.eval_clients._base import EvalClient
+from langcheck.metrics.eval_clients._openai import (
+    AzureOpenAIEvalClient,
+    OpenAIEvalClient,
+)
+from langcheck.metrics.eval_clients._openrouter import OpenRouterEvalClient
+
+__all__ = [
+    "AzureOpenAIEvalClient",
+    "EvalClient",
+    "OpenAIEvalClient",
+    "OpenRouterEvalClient",
+]
+
+try:
+    from langcheck.metrics.eval_clients._anthropic import (
+        AnthropicEvalClient,  # NOQA: F401
+    )
+except ModuleNotFoundError:
+    pass
+else:
+    __all__.append("AnthropicEvalClient")
+
+try:
+    from langcheck.metrics.eval_clients._gemini import (
+        GeminiEvalClient,  # NOQA: F401
+    )
+except ModuleNotFoundError:
+    pass
+else:
+    __all__.append("GeminiEvalClient")
+
+try:
+    from langcheck.metrics.eval_clients._llama import (
+        LlamaEvalClient,  # NOQA: F401
+    )
+    from langcheck.metrics.eval_clients._prometheus import (
+        PrometheusEvalClient,  # NOQA: F401
+    )
+except ModuleNotFoundError:
+    pass
+else:
+    __all__.append("PrometheusEvalClient")
+    __all__.append("LlamaEvalClient")
