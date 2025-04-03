6cc4903688bc0712878677f76ae5cba354432077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:05:14 +0000

    Add package blogtuner/0.3.1/blogtuner-0.3.1.tar.gz

---
+++ packages/blogtuner/blogtuner-0.3.1.tar.gz/blogtuner-0.3.1/src/blogtuner/constants.py
@@ -0,0 +1,14 @@
+from typing import Any, Dict
+
+
+# Define default post metadata
+DEFAULT_POST_METADATA: Dict[str, Any] = {}
+DEFAULT_BLOG_METADATA: Dict[str, Any] = {
+    "base_url": None,
+    "base_path": "/",
+    "author": "Anonymous",
+    "name": "My Blog",
+    "lang": "en",
+    "tz": "UTC",
+    "footer_text": "Powered by <a href='https://github.com/alltuner/blogtuner'>BlogTuner</a>",
+}
