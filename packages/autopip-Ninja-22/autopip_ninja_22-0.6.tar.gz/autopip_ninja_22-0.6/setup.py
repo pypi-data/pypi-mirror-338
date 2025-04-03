60474abe584d59f6aabecdcbaa4ac8e9a180f9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 04:04:37 +0000

    Add package autopip-Ninja-22/0.6/autopip_ninja_22-0.6.tar.gz

---
+++ packages/autopip-Ninja-22/autopip_ninja_22-0.6.tar.gz/autopip_ninja_22-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="autopip-Ninja-22",
+    version="0.6",
+    packages=find_packages(),
+    description="Python modüllerini otomatik yükleyen kütüphane",
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
+    author="Ninja-22",
+    author_email="sametcevik88@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
