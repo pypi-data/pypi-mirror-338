bc5e0921d01c54dd008bc65b4a2313d3b381e293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:05:11 +0000

    Add package pyspark-flow/0.1.0/pyspark_flow-0.1.0-py3-none-any.whl

---
+++ packages/pyspark-flow/pyspark_flow-0.1.0-py3-none-any.whl/sparkflow/schemas/ConfigSchema.py
@@ -0,0 +1,34 @@
+from typing import List, Dict, Union
+from pydantic import BaseModel, Field, SecretStr
+from sparkflow.schemas.Enum import SourceType
+
+class SparkConf(BaseModel):
+    masterUrl: str = "local"
+    config: Dict[str, Union[int, float, str]] = Field(default_factory=lambda: {"spark.sql.param1": 4})
+
+class JDBCSource(BaseModel):
+    type: SourceType = Field(default=SourceType.JDBC)
+    name: str = "postgres"
+    username: str = "username"
+    password: SecretStr = "password"
+    table: str
+    url: str
+    options: Dict[str, str] = Field(default_factory=dict)
+
+class HiveSource(BaseModel):
+    type: SourceType = Field(default=SourceType.HIVE)
+    name: str = "lake"
+    table: str = "table_name"
+
+Source = Union[JDBCSource, HiveSource]
+
+class ConfigModel(BaseModel):
+    appName: str = "sparkflow app"
+    sparkConf: SparkConf = SparkConf()
+    sources: List[Source] = Field(default_factory=lambda: [
+        JDBCSource(table="public.my_table", url="jdbc:postgresql://localhost:5432/mydb"),
+        HiveSource()
+    ])
+
+    def get_sources_by_type(self, source_type: SourceType) -> List[Source]:
+        return [source for source in self.sources if source.type == source_type]
