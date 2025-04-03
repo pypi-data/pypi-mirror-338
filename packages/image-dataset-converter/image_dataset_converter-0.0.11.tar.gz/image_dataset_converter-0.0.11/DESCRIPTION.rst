9147b44b196d4bc98620bdbb7325316203fd1352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:05:28 +0000

    Add package image-dataset-converter/0.0.11/image_dataset_converter-0.0.11.tar.gz

---
+++ packages/image-dataset-converter/image_dataset_converter-0.0.11.tar.gz/image_dataset_converter-0.0.11/DESCRIPTION.rst
@@ -0,0 +1,10 @@
+The **image-dataset-converter** library allows the conversion between
+various dataset formats for image annotation datasets.
+Filters can be supplied as well, e.g., for cleaning up the data.
+
+Dataset formats:
+
+- image classification: ADAMS (r/w), sub-dir (r/w)
+- object detection: ADAMS (r/w), COCO (r/w), OPEX (r/w), ROI (r/w), VOC (r/w), YOLO (r/w)
+- image segmentation: blue-channel (r/w), grayscale (r/w), indexed PNG (r/w), layer segments (r/w)
+
---
+++ packages/image-dataset-converter/image_dataset_converter-0.0.11.tar.gz/image_dataset_converter-0.0.11/src/idc/api/__init__.py
@@ -0,0 +1,22 @@
+from ._colors import rgb2yiq, text_color
+from ._fonts import DEFAULT_FONT_FAMILY, load_font, text_size
+from ._data import ImageData, make_list, flatten_list, jpeg_quality, array_to_image, empty_image, save_image, FORMATS, FORMAT_JPEG, FORMAT_PNG, FORMAT_BMP, FORMAT_EXTENSIONS
+from ._generator import Generator, SingleVariableGenerator
+from ._imgcls import ImageClassificationData
+from ._imgseg import ImageSegmentationData, ImageSegmentationAnnotations, combine_layers, split_layers
+from ._imgseg import from_indexedpng, from_bluechannel, from_grayscale, to_indexedpng, to_grayscale, to_bluechannel
+from ._objdet import ObjectDetectionData, get_object_label, set_object_label, DEFAULT_LABEL, LABEL_KEY
+from ._utils import locate_file, locate_image, load_image_from_bytes, load_image_from_file, load_function, JPEG_EXTENSIONS
+from ._utils import load_labels, save_labels, save_labels_csv
+from ._utils import crop_image, pad_image
+from ._utils import safe_deepcopy
+from ._data_types import DATATYPE_IMGCLS, DATATYPE_OBJDET, DATATYPE_IMGSEG, DATATYPES, data_type_to_class
+from ._geometry import locatedobjects_to_shapely, shapely_to_locatedobject, locatedobject_polygon_to_shapely, locatedobject_bbox_to_shapely
+from ._geometry import intersect_over_union, COMBINATIONS, INTERSECT, UNION
+from ._geometry import merge_polygons, fit_located_object, fit_layers
+from ._reader import Reader, parse_reader
+from ._filter import parse_filter
+from ._writer import parse_writer
+from ._writer import BatchWriter, SplittableBatchWriter
+from ._writer import StreamWriter, SplittableStreamWriter
+from ._writer import AnnotationsOnlyWriter, add_annotations_only_param
