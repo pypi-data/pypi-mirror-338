1f5dcef3819e7e69cbd36c3e796c2f5467c13f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:00 +0000

    Add package ommlx/0.0.0.dev271/ommlx-0.0.0.dev271-py3-none-any.whl

---
+++ packages/ommlx/ommlx-0.0.0.dev271-py3-none-any.whl/ommlx/minichain/backends/sentencetransformers.py
@@ -0,0 +1,40 @@
+import typing as ta
+
+from omlish import lang
+
+from ..content.images import Image
+from ..vectors.embeddings import EmbeddingModel
+from ..vectors.embeddings import EmbeddingRequest
+from ..vectors.embeddings import EmbeddingResponse
+from ..vectors.vectors import Vector
+
+
+if ta.TYPE_CHECKING:
+    import sentence_transformers as st
+else:
+    st = lang.proxy_import('sentence_transformers')
+
+
+# @omlish-manifest ommlx.minichain.backends.manifests.BackendManifest(
+#     name='sentencetransformers',
+#     aliases=['st'],
+#     type='EmbeddingModel',
+# )
+class SentencetransformersEmbeddingModel(EmbeddingModel):
+    model = 'clip-ViT-B-32'
+
+    def invoke(self, request: EmbeddingRequest) -> EmbeddingResponse:
+        mdl = st.SentenceTransformer(self.model)  # type: ignore[call-overload]
+
+        obj: ta.Any
+        v = request.v
+        if isinstance(v, str):
+            obj = v
+        elif isinstance(v, Image):
+            obj = v.i
+        else:
+            raise TypeError(v)
+
+        response = mdl.encode(obj)
+
+        return EmbeddingResponse(v=Vector(response.tolist()))
