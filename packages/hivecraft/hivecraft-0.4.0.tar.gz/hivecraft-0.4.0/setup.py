86a5ef778cf6d50b94253517c4e1bbeecc851486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:07:05 +0000

    Add package hivecraft/0.4.0/hivecraft-0.4.0.tar.gz

---
+++ packages/hivecraft/hivecraft-0.4.0.tar.gz/hivecraft-0.4.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hivecraft',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='Éric PHILIPPE',
+    author_email='ericphlpp@proton.me',
+    description='Hivecraft is a tool to crate, test and compile a folder with .alghive extension.',
+)
