9130c3cc9c01c3a2444d9cffe6900c0e89ce422a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:46 +0000

    Add package pyrfu/2.4.17/pyrfu-2.4.17.tar.gz

---
+++ packages/pyrfu/pyrfu-2.4.17.tar.gz/pyrfu-2.4.17/pyrfu/models/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Louis Richard
+from .igrf import igrf
+from .magnetopause_normal import magnetopause_normal
+
+__author__ = "Louis Richard"
+__email__ = "louisr@irfu.se"
+__copyright__ = "Copyright 2020-2023"
+__license__ = "MIT"
+__version__ = "2.4.2"
+__status__ = "Prototype"
+
+__all__ = ["igrf", "magnetopause_normal"]
