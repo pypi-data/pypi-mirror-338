e04c27165fb358e1db1f450c96f08445d8d5280f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 15:06:53 +0000

    Add package infrahouse-toolkit/2.51.0/infrahouse_toolkit-2.51.0-py3-none-any.whl

---
+++ packages/infrahouse-toolkit/infrahouse_toolkit-2.51.0-py3-none-any.whl/infrahouse_toolkit/cli/ih_elastic/cmd_cluster/__init__.py
@@ -0,0 +1,32 @@
+"""
+.. topic:: ``ih-elastic cat``
+
+    A ``ih-elastic cat`` subcommand.
+
+    See ``ih-elastic cat --help`` for more details.
+"""
+
+from logging import getLogger
+
+import click
+
+from infrahouse_toolkit.cli.ih_elastic.cmd_cluster.cmd_allocation_explain import (
+    cmd_allocation_explain,
+)
+from infrahouse_toolkit.cli.ih_elastic.cmd_cluster.cmd_decommision_node import (
+    cmd_decommission_node,
+)
+
+LOG = getLogger(__name__)
+
+
+@click.group(name="cluster")
+def cmd_cluster():
+    """
+    Cluster level operations.
+    """
+
+
+for cmd in [cmd_allocation_explain, cmd_decommission_node]:
+    # noinspection PyTypeChecker
+    cmd_cluster.add_command(cmd)
