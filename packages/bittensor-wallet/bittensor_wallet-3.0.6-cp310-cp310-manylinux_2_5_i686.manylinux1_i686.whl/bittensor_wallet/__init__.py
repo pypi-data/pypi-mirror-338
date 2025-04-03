770ba53cb07d5bde280b034354658a5ab0c241e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:47 +0000

    Add package bittensor-wallet/3.0.6/bittensor_wallet-3.0.6-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.whl

---
+++ packages/bittensor-wallet/bittensor_wallet-3.0.6-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.whl/bittensor_wallet/__init__.py
@@ -0,0 +1,28 @@
+from bittensor_wallet.bittensor_wallet import (
+    Config,
+    Keyfile,
+    Keypair,
+    Wallet,
+    config,
+    errors,
+    keyfile,
+    keypair,
+    utils,
+    wallet,
+)
+
+# classes
+Config = Config
+Keyfile = Keyfile
+Keypair = Keypair
+Wallet = Wallet
+
+# modules
+config = config
+errors = errors
+keyfile = keyfile
+keypair = keypair
+utils = utils
+wallet = wallet
+
+__version__ = "3.0.2"
