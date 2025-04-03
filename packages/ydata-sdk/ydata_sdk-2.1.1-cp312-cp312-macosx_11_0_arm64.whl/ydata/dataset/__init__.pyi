ae444ad5fe42ab3e6fe795f5423c3e5f41b8cfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:45 +0000

    Add package ydata-sdk/2.1.1/ydata_sdk-2.1.1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/ydata-sdk/ydata_sdk-2.1.1-cp312-cp312-macosx_11_0_arm64.whl/ydata/dataset/__init__.pyi
@@ -0,0 +1,5 @@
+from ydata.dataset.dataset import Dataset as Dataset
+from ydata.dataset.dataset_type import DatasetType as DatasetType
+from ydata.dataset.multidataset import MultiDataset as MultiDataset
+
+__all__ = ['Dataset', 'MultiDataset', 'DatasetType']
