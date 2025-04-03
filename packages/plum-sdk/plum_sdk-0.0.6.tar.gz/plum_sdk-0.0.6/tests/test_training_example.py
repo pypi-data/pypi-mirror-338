a45ca6ec5947788599b34d94c21fb75ec4f9eb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:33 +0000

    Add package plum-sdk/0.0.6/plum_sdk-0.0.6.tar.gz

---
+++ packages/plum-sdk/plum_sdk-0.0.6.tar.gz/plum_sdk-0.0.6/tests/test_training_example.py
@@ -0,0 +1,8 @@
+import unittest
+from plum_sdk import TrainingExample
+
+class TestTrainingExample(unittest.TestCase):
+    def test_training_example_creation(self):
+        example = TrainingExample(input="test input", output="test output")
+        self.assertEqual(example.input, "test input")
+        self.assertEqual(example.output, "test output")
