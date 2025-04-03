95aefc5f630ea7815edda3b3e6e8e85b492fe7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:04:55 +0000

    Add package pyrregular/0.1.0/pyrregular-0.1.0.tar.gz

---
+++ packages/pyrregular/pyrregular-0.1.0.tar.gz/pyrregular-0.1.0/pyrregular/repository.py
@@ -0,0 +1,34 @@
+import pooch
+import os
+from pyrregular.io_utils import load_from_file
+from pyrregular.data_utils import get_project_root
+
+REPOSITORY = pooch.create(
+    path=pooch.os_cache("pyrregular"),
+    base_url="https://huggingface.co/datasets/splandi/pyrregular/resolve/main/data_final/",
+    registry=None,
+)
+
+REPOSITORY.load_registry(get_project_root() / "registry.txt")
+
+
+def download_dataset_from_huggingface(name, api_token=None):
+    if api_token is None:
+        api_token = os.getenv("HF_TOKEN")
+    if api_token is None:
+        raise ValueError("You need to provide an API token to download the dataset")
+    downloader = pooch.HTTPDownloader(
+        **dict(headers={"Authorization": f"Bearer {api_token}"})
+    )
+    return REPOSITORY.fetch(name, downloader=downloader)
+
+
+def load_dataset_from_huggingface(name, api_token=None):
+    if ".h5" not in name:
+        name += ".h5"
+    file = download_dataset_from_huggingface(name, api_token)
+    return load_from_file(file)
+
+
+if __name__ == "__main__":
+    df = load_dataset_from_huggingface("Garment.h5")
