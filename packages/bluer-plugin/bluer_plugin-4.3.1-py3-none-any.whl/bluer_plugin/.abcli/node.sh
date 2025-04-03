0ab6b2c07351f460a97acfe8d1b233ab309625ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:16 +0000

    Add package bluer-plugin/4.3.1/bluer_plugin-4.3.1-py3-none-any.whl

---
+++ packages/bluer-plugin/bluer_plugin-4.3.1-py3-none-any.whl/bluer_plugin/.abcli/node.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+function bluer_plugin_node() {
+    local task=$1
+
+    local function_name=bluer_plugin_node_$task
+    if [[ $(type -t $function_name) == "function" ]]; then
+        $function_name "${@:2}"
+        return
+    fi
+    abcli_log "bluer-plugin: node: 🪄"
+}
+
+bluer_ai_source_caller_suffix_path /node
