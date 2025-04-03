03f345fa33584f92ee55215ff28d9bfa0e927211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:55 +0000

    Add package flux-ws-module/1.0.2/flux_ws_module-1.0.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flux-ws-module/flux_ws_module-1.0.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flux_ws_module/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from flux_ws_module._flux_ws_module import BaseExchangeConnector
+from flux_ws_module._flux_ws_module import OkxConnector
+from flux_ws_module._flux_ws_module import MexcConnector
+from . import _flux_ws_module
+__all__: list = ['OkxConnector', 'MexcConnector', 'BaseExchangeConnector']
