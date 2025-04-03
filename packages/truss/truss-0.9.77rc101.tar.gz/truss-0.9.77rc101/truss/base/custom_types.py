b62e9b98e6710e8ab2e3cf40d9bdf63f268f4e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:28 +0000

    Add package truss/0.9.77rc101/truss-0.9.77rc101.tar.gz

---
+++ packages/truss/truss-0.9.77rc101.tar.gz/truss-0.9.77rc101/truss/base/custom_types.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from typing import Any
+
+import pydantic
+
+
+@dataclass
+class Example:
+    name: str
+    input: Any
+
+    @staticmethod
+    def from_dict(example_dict):
+        return Example(name=example_dict["name"], input=example_dict["input"])
+
+    def to_dict(self) -> dict:
+        return {"name": self.name, "input": self.input}
+
+
+class SafeModel(pydantic.BaseModel):
+    """Pydantic base model with reasonable config."""
+
+    model_config = pydantic.ConfigDict(
+        arbitrary_types_allowed=False, strict=True, validate_assignment=True
+    )
+
+
+class SafeModelNonSerializable(pydantic.BaseModel):
+    """Pydantic base model with reasonable config - allowing arbitrary types."""
+
+    model_config = pydantic.ConfigDict(
+        arbitrary_types_allowed=True,
+        strict=True,
+        validate_assignment=True,
+        extra="forbid",
+    )
+
+
+class ConfigModel(pydantic.BaseModel):
+    def to_dict(self, verbose: bool = False) -> dict[str, Any]:
+        mode = "json"
+        full_dump = super().model_dump(mode=mode)
+        if verbose:
+            return full_dump
+
+        reduced_dump = super().model_dump(
+            exclude_unset=True,
+            exclude_none=True,
+            exclude_defaults=True,
+            mode=mode,
+            context={"verbose": verbose},
+        )
+        return reduced_dump
