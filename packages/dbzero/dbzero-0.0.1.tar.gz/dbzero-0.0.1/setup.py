3db1a323450b526bea23e4632e52f808d4fe2e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:50 +0000

    Add package dbzero/0.0.1/dbzero-0.0.1.tar.gz

---
+++ packages/dbzero/dbzero-0.0.1.tar.gz/dbzero-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='dbzero',
+    version='0.0.1',
+    description='dbzero package placeholder',
+    packages=['dbzero']
+)
+# This is a placeholder setup.py file for the dbzero package.
