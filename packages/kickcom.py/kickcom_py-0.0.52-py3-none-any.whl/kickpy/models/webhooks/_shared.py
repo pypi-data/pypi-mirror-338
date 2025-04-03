0afab34be8b1362c9fa0abe6f12b7aeefcad6a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:49 +0000

    Add package kickcom.py/0.0.52/kickcom_py-0.0.52-py3-none-any.whl

---
+++ packages/kickcom.py/kickcom_py-0.0.52-py3-none-any.whl/kickpy/models/webhooks/_shared.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    """Represents a user from a webhook."""
+
+    is_anonymous: bool
+    user_id: int
+    username: str
+    is_verified: bool
+    profile_picture: str
+    channel_slug: str
