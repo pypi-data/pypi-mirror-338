6969c44c61b40a8f0ce63bf2832eaec45f234717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:13 +0000

    Add package livekit-plugins-elevenlabs/1.0.0rc5/livekit_plugins_elevenlabs-1.0.0rc5.tar.gz

---
+++ packages/livekit-plugins-elevenlabs/livekit_plugins_elevenlabs-1.0.0rc5.tar.gz/livekit_plugins_elevenlabs-1.0.0rc5/livekit/plugins/elevenlabs/models.py
@@ -0,0 +1,13 @@
+from typing import Literal
+
+TTSModels = Literal[
+    "eleven_monolingual_v1",
+    "eleven_multilingual_v1",
+    "eleven_multilingual_v2",
+    "eleven_turbo_v2",
+    "eleven_turbo_v2_5",
+    "eleven_flash_v2_5",
+    "eleven_flash_v2",
+]
+
+TTSEncoding = Literal["mp3_44100",]
