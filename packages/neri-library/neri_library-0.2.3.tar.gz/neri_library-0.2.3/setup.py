383576ff0f0ddd585e251735ae9c8c637a89f4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:33 +0000

    Add package neri-library/0.2.3/neri_library-0.2.3.tar.gz

---
+++ packages/neri-library/neri_library-0.2.3.tar.gz/neri_library-0.2.3/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neri_library",
+    version="0.2.3",
+    author="Guilherme Neri",
+    author_email="gui.neriaz@gmail.com ",
+    description="Neri Library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NeriAzv/Neri-Library",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            *[f"{cmd}=bcpkgfox.cli:main" for cmd in [
+                "bcpkgfox",
+                "bpckgofx",
+                "bcpkffox",
+                "bcpkhfox",
+                "bcpkfox",
+                "pkgfox",
+                "bcfox",
+                "bcpkg",
+                "bpkg",
+                "pkg",
+                "fox",
+                "bc",
+            ]],
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'setuptools',
+        'pyperclip',
+        'pyinstaller',
+        'selenium'
+    ],
+    extras_require={
+        "full": [
+            'undetected-chromedriver',
+            'webdriver-manager',
+            'opencv-python',
+            'pygetwindow',
+            'pyinstaller',
+            'pyscreeze',
+            'pyautogui',
+            'selenium',
+            'requests',
+            'pymupdf',
+            'Pillow',
+            'psutil'
+        ],
+
+    },
+)
