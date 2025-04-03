18baa519584798c2428724c5e1b35494069c14ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 04:04:41 +0000

    Add package pubmedquery/0.1.10/pubmedquery-0.1.10.tar.gz

---
+++ packages/pubmedquery/pubmedquery-0.1.10.tar.gz/pubmedquery-0.1.10/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubmedquery',
+    version='0.1.10',
+    author='Yeshwant Chillakuru',
+    description='A Python package to download data from PubMed',
+    long_description='Please refer to the GitHub page for more information / documentation',
+    packages=find_packages(),  # Automatically discovers all packages with `__init__.py`
+    install_requires=[
+        'numpy',
+        'pandas',
+        'requests'
+    ],
+    python_requires='>=3.8',
+)
