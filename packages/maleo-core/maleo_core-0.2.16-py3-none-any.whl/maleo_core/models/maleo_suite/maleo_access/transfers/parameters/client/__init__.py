2d36828d813184a95f2a00e38e73d8d1daf02ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:54 +0000

    Add package maleo-core/0.2.16/maleo_core-0.2.16-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.2.16-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/client/__init__.py
@@ -0,0 +1,24 @@
+# This file serves all MaleoAccess's Cient Parameters
+
+from __future__ import annotations
+from .blood_type import MaleoAccessBloodTypeClientParameters
+from .gender import MaleoAccessGenderClientParameters
+from .organization_role import MaleoAccessOrganizationRoleClientParameters
+from .system_role import MaleoAccessSystemRoleClientParameters
+from .user_type import MaleoAccessUserTypeClientParameters
+
+class MaleoAccessClientParameters:
+    BloodType = MaleoAccessBloodTypeClientParameters
+    Gender = MaleoAccessGenderClientParameters
+    OrganizationRole = MaleoAccessOrganizationRoleClientParameters
+    SystemRole = MaleoAccessSystemRoleClientParameters
+    UserType = MaleoAccessUserTypeClientParameters
+
+__all__ = [
+    "MaleoAccessClientParameters",
+    "MaleoAccessBloodTypeClientParameters",
+    "MaleoAccessGenderClientParameters",
+    "MaleoAccessOrganizationRoleClientParameters",
+    "MaleoAccessSystemRoleClientParameters",
+    "MaleoAccessUserTypeClientParameters"
+]
