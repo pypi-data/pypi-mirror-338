cc7baccdb4e222b6cbf3cc98304d4f24aec61c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:04:57 +0000

    Add package pytest-marker-bugzilla/0.9.5.dev3/pytest_marker_bugzilla-0.9.5.dev3.tar.gz

---
+++ packages/pytest-marker-bugzilla/pytest_marker_bugzilla-0.9.5.dev3.tar.gz/pytest_marker_bugzilla-0.9.5.dev3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(
+	name="pytest-marker-bugzilla",
+	version="0.9.5",
+        setup_requires=['pbr'],
+        pbr=True,
+    )
