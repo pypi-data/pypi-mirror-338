b022935d97da285d11f7f2b615b9d7f401e3c02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 19:05:24 +0000

    Add package ai2soft-automation/0.0.4/ai2soft.automation-0.0.4.tar.gz

---
+++ packages/ai2soft-automation/ai2soft.automation-0.0.4.tar.gz/ai2soft.automation-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai2soft.automation',
+    version='0.0.4',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    description='用于完与一些自动化的操作',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    author='ai2soft.cn',
+    author_email='goldli@live.cn',
+    license='MIT',
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ]
+)
