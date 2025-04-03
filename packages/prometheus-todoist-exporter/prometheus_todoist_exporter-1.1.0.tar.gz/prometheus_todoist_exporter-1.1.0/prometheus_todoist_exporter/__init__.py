16d84f0ccb0e27479a250d7131487c400cb856a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:46 +0000

    Add package prometheus-todoist-exporter/1.1.0/prometheus_todoist_exporter-1.1.0.tar.gz

---
+++ packages/prometheus-todoist-exporter/prometheus_todoist_exporter-1.1.0.tar.gz/prometheus_todoist_exporter-1.1.0/prometheus_todoist_exporter/__init__.py
@@ -0,0 +1,3 @@
+"""Prometheus exporter for Todoist."""
+
+__version__ = "1.1.0"
---
+++ packages/prometheus-todoist-exporter/prometheus_todoist_exporter-1.1.0.tar.gz/prometheus_todoist_exporter-1.1.0/prometheus_todoist_exporter/__main__.py
@@ -0,0 +1,6 @@
+"""Main module entry point."""
+
+from prometheus_todoist_exporter.exporter import main
+
+if __name__ == "__main__":
+    main()
