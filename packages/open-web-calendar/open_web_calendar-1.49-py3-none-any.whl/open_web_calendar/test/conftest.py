f388c9662ed2e479d0d10edb45a0e84cae309b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:47 +0000

    Add package open-web-calendar/1.49/open_web_calendar-1.49-py3-none-any.whl

---
+++ packages/open-web-calendar/open_web_calendar-1.49-py3-none-any.whl/open_web_calendar/test/conftest.py
@@ -0,0 +1,153 @@
+# SPDX-FileCopyrightText: 2024 Nicco Kunzmann and Open Web Calendar Contributors <https://open-web-calendar.quelltext.eu/>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING, Callable, Optional
+from unittest.mock import Mock
+
+import icalendar
+import pytest
+import requests
+
+if TYPE_CHECKING:
+    from flask import Flask
+    from flask.testing import FlaskClient
+
+# constants
+HERE = Path(__file__).parent
+CALENDAR_DIRECTORY = HERE / ".." / "features" / "calendars"
+
+# relative imports
+sys.path.append(HERE.absolute() / ".." / "..")
+sys.path.append(HERE.absolute())
+from open_web_calendar.app import DEFAULT_SPECIFICATION, cache_url  # noqa: E402
+from open_web_calendar.encryption import FernetStore  # noqa: E402
+
+DEFAULT_SPECIFICATION["url"] = []
+
+
+@pytest.fixture(autouse=True)
+def _no_requests(monkeypatch):
+    """Prevent requests from sending out requests
+
+    See https://docs.pytest.org/en/latest/monkeypatch.html#example-preventing-requests-from-remote-operations
+    """
+
+    def test_cannot_call_outside(*args, **kw):
+        raise RuntimeError(
+            "Tests are not allowed to make requests to the"
+            " Internet. You can use cache_url() to mock that."
+        )
+
+    monkeypatch.setattr(requests.sessions.Session, "request", test_cannot_call_outside)
+
+
+@pytest.fixture
+def mock():
+    return Mock()
+
+
+@pytest.fixture
+def app(store, monkeypatch) -> Flask:
+    """Create the app.
+
+    See https://flask.palletsprojects.com/en/2.2.x/testing/
+    """
+    from open_web_calendar import app
+
+    monkeypatch.setattr(FernetStore, "from_environment", lambda: store)
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+
+    # other setup can go here
+
+    return app
+
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+
+calendar_files = {}
+for file in CALENDAR_DIRECTORY.iterdir():
+    if file.is_file():
+        with file.open() as f:
+            calendar_files[file] = f.read()
+
+
+@pytest.fixture
+def calendar_urls() -> dict[str, str]:
+    """Mapping the calendar name without .ics to the cached url.
+
+    The files are located in the CALENDAR_FOLDER.
+    """
+    mapping: dict[str, str] = {}
+    for file, content in calendar_files.items():
+        url = "http://test.examples.local/" + file.name
+        cache_url(url, content)
+        mapping[file.name] = url
+        if file.suffix.lower() == ".ics":
+            mapping[file.stem] = url
+    return mapping
+
+
+@pytest.fixture
+def calendar_content():
+    """Mapping the calendar name without .ics to the calendar content.
+
+    The files are located in the CALENDAR_FOLDER.
+    """
+    mapping = {}
+    for file, content in calendar_files.items():
+        if file.suffix.lower() == ".ics":
+            mapping[file.stem] = content
+    return mapping
+
+
+@pytest.fixture
+def merged(
+    client, calendar_urls
+) -> Callable[[Optional[list[str]], Optional[dict[str, str]]], icalendar.Calendar]:
+    """Return a function to get a parsed calendar that is merged according to spec."""
+
+    def _merged_calendars(
+        urls: Optional[list[str]] = None, specification: Optional[dict[str, str]] = None
+    ) -> icalendar.Calendar:
+        """Return the merged ICS calendar."""
+        urls = urls or []
+        specification = specification or {}
+        query = "?"
+        for url in urls:
+            query += f"url={calendar_urls[url]}&"
+        for k, v in specification.items():
+            query += f"{k}={v}&"
+        response = client.get(f"/calendar.ics{query}")
+        assert response.status_code == 200
+        print(response.data.decode("utf-8"))
+        return icalendar.Calendar.from_ical(response.data)
+
+    return _merged_calendars
+
+
+@pytest.fixture
+def store():
+    """A test crypt store for the open web calendar."""
+    return FernetStore(["n77iebivnjNTLDpmFcu6DuNFTHUnlEjCskx8oe0Xh8k="])
+
+
+@pytest.fixture
+def todo():
+    """This test should be implement later."""
+    pytest.skip("This test needs implementing.")
---
+++ packages/open-web-calendar/open_web_calendar-1.49-py3-none-any.whl/open_web_calendar/test/test_url_cache.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: 2024 Nicco Kunzmann and Open Web Calendar Contributors <https://open-web-calendar.quelltext.eu/>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+"""
+Test the caching functionality which will be used by subsequent tests.
+
+"""
+
+from typing import NamedTuple
+
+import pytest
+import requests
+
+from open_web_calendar.app import cache_url, get_text_from_url
+
+
+# use the content property
+# see https://stackoverflow.com/a/18810889/1320237
+class MockRequestResult(NamedTuple):
+    content: str
+
+    def raise_for_status(self):
+        pass
+
+
+def test_requests_are_automatically_cached(monkeypatch, mock):
+    mock.return_value = MockRequestResult("trallala")
+    monkeypatch.setattr(requests, "get", mock)
+    assert get_text_from_url("https://asd.asd") == "trallala"
+    assert get_text_from_url("https://asd.asd") == "trallala"
+    mock.assert_called_once()
+
+
+def test_tests_can_cache_values():
+    cache_url("http://asd.asd", "test123")
+    assert get_text_from_url("http://asd.asd") == "test123"
+
+
+def test_caching_twice_works():
+    cache_url("http://asd.asd", "test123")
+    cache_url("http://asd.asd", "asd")
+    assert get_text_from_url("http://asd.asd") == "asd"
+
+
+def test_requests_are_forbidden():
+    import requests
+
+    with pytest.raises(RuntimeError):
+        requests.get("https://duckduckgo.com", timeout=20)
