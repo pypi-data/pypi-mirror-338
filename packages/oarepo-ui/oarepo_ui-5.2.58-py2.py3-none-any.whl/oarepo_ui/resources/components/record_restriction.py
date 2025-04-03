860c7b09e1c5087ecb34b4e8846ef0364afee46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:08:01 +0000

    Add package oarepo-ui/5.2.58/oarepo_ui-5.2.58-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-5.2.58-py2.py3-none-any.whl/oarepo_ui/resources/components/record_restriction.py
@@ -0,0 +1,14 @@
+from flask import current_app
+from .base import UIResourceComponent
+from datetime import timedelta
+
+
+class RecordRestrictionComponent(UIResourceComponent):
+    def form_config(self, *, form_config, **kwargs):
+        form_config["recordRestrictionGracePeriod"] = current_app.config.get(
+            "RDM_RECORDS_RESTRICTION_GRACE_PERIOD", timedelta(days=30)
+        ).days
+
+        form_config["allowRecordRestriction"] = current_app.config.get(
+            "RDM_RECORDS_ALLOW_RESTRICTION_AFTER_GRACE_PERIOD", False
+        )
