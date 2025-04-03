4a12c5e2c8c8dd99e1390184de348cfe64fdfcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:31 +0000

    Add package cronflow/0.1.0/cronflow-0.1.0-py3-none-any.whl

---
+++ packages/cronflow/cronflow-0.1.0-py3-none-any.whl/cronflow/__init__.py
@@ -0,0 +1,10 @@
+"""
+Cronflow - A Python package for controlling cron job execution
+
+This package provides utilities for enabling/disabling Python scripts
+that are run through cron.
+"""
+
+from .cron_switch import cron_switch
+
+__version__ = '0.1.0'
