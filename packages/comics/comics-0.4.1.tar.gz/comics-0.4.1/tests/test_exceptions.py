3fc0b5e347880c492ae8778b6448028f63507926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:04:57 +0000

    Add package comics/0.4.1/comics-0.4.1.tar.gz

---
+++ packages/comics/comics-0.4.1.tar.gz/comics-0.4.1/tests/test_exceptions.py
@@ -0,0 +1,29 @@
+"""
+tests/test_exceptions
+~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import pytest
+
+import comics
+from comics import InvalidDateError, InvalidEndpointError
+
+
+def test_invalid_endpoint():
+    """Tests proper raise of `InvalidEndpointError` when using an endpoint that does
+    not exist in GoComics."""
+    with pytest.raises(InvalidEndpointError):
+        comics.search("invalid_endpoint").date("2000-01-01")
+
+
+def test_date_before_creation():
+    """Tests proper raise of `InvalidDateError` when using date that is before the
+    comic's creation date."""
+    with pytest.raises(InvalidDateError):
+        comics.search("calvinandhobbes").date("1900-01-01")
+
+
+def test_invalid_future_date():
+    """Tests proper raise of `InvalidDateError` when using date that is in the future."""
+    with pytest.raises(InvalidDateError):
+        comics.search("calvinandhobbes").date("2050-01-01")
