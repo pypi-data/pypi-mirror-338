81f1a309beea94dc9b620ac29d8a1df1aaae57ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:07:39 +0000

    Add package kql/1.0.2/kql-1.0.2.tar.gz

---
+++ packages/kql/kql-1.0.2.tar.gz/kql-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kql",  # ✅ Unique package name
+    version="1.0.2",  # Start with version 0.1.0
+    packages=find_packages(),
+    install_requires=["requests"],  # List dependencies
+    author="STEIN",
+    author_email="devilsteinshorts@gmail.com",
+    description="A Python package for user info, username generation, Instagram checking, and email validation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/steinlurks",  # Change this to your actual GitHub repo
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
