800ad4961ef21bc739db13f0c4ce0cd7a214ee26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:06:56 +0000

    Add package neuropipeline2/0.1.1/neuropipeline2-0.1.1.tar.gz

---
+++ packages/neuropipeline2/neuropipeline2-0.1.1.tar.gz/neuropipeline2-0.1.1/neuropipeline2/eeg.py
@@ -0,0 +1,35 @@
+
+
+
+class EEG():
+    def __init__(self, filepath:str|None=None):
+
+        self.history = []
+        pass
+    def ParseHDF5(self, ):
+
+        pass
+    
+    def ParseEDF(self, ):
+
+        pass
+    
+    def WriteHDF5(self, ):
+
+        pass
+    def WriteEDF(self, ):
+
+        pass
+
+    def Trim(self, before_first_feature=20.0, after_last_feature=10.0):
+        """ Trims the eeg file """
+
+        self.history.append("TRIM")
+        pass
+    def Preprocess(self, ):
+
+        pass
+    
+
+
+eeg = EEG()
