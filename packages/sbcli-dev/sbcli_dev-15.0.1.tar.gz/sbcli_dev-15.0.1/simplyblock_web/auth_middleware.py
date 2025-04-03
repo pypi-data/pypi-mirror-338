c8d354c8a2133b54fcd68f9e77cf667056deb844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 17:05:09 +0000

    Add package sbcli-dev/15.0.1/sbcli_dev-15.0.1.tar.gz

---
+++ packages/sbcli-dev/sbcli_dev-15.0.1.tar.gz/sbcli_dev-15.0.1/simplyblock_web/auth_middleware.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from functools import wraps
+from flask import request
+
+from simplyblock_core.db_controller import DBController
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        cluster_id = None
+        cluster_secret = None
+        if "Authorization" in request.headers:
+            au = request.headers["Authorization"]
+            if len(au.split()) == 2:
+                cluster_id = au.split()[0]
+                cluster_secret = au.split()[1]
+
+        if not cluster_id or not cluster_secret:
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unauthorized"
+            }, 401
+        try:
+            db_controller = DBController()
+            cluster = db_controller.get_cluster_by_id(cluster_id)
+            if not cluster:
+                return {
+                    "message": "Invalid Cluster ID",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
+            if cluster.secret != cluster_secret:
+                return {
+                    "message": "Invalid Cluster secret",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+
+        return f(*args, **kwargs)
+
+    return decorated
