aa2a452f8d80ebda77068c23d661b11e98f465c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 18:05:41 +0000

    Add package zeit.nightwatch/2.3.0/zeit_nightwatch-2.3.0.tar.gz

---
+++ packages/zeit.nightwatch/zeit_nightwatch-2.3.0.tar.gz/zeit_nightwatch-2.3.0/tests/test_basics.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def nightwatch_config():
+    return {"browser": {"baseurl": "https://httpbin.org"}}
+
+
+@pytest.fixture(scope="session")
+def base_url(nightwatch_config):
+    return nightwatch_config.get("browser", {}).get("baseurl")
+
+
+def test_get(http):
+    r = http.get("/get")
+    assert r.status_code == 200
+
+
+def test_playwright_works(page):
+    page.goto("/forms/post")
+    assert page.locator("form").count() == 1
