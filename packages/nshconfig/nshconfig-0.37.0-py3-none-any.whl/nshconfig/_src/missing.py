2faedf9ba0d624b04d4a41d704938766eae23de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:03 +0000

    Add package nshconfig/0.37.0/nshconfig-0.37.0-py3-none-any.whl

---
+++ packages/nshconfig/nshconfig-0.37.0-py3-none-any.whl/nshconfig/_src/missing.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Annotated, Any, cast
+
+from pydantic import BaseModel
+from pydantic_core import PydanticCustomError
+from typing_extensions import TypeAliasType, TypeVar
+
+
+@dataclass
+class AllowMissingAnnotation:
+    pass
+
+
+MISSING = cast(Any, None)
+
+T = TypeVar("T", infer_variance=True)
+if TYPE_CHECKING:
+    AllowMissing = TypeAliasType(
+        "AllowMissing",
+        Annotated[T, AllowMissingAnnotation()],
+        type_params=(T,),
+    )
+else:
+    AllowMissing = TypeAliasType(
+        "AllowMissing",
+        Annotated[T | None, AllowMissingAnnotation()],
+        type_params=(T,),
+    )
+
+
+def validate_no_missing_values(model: BaseModel):
+    for name, field in type(model).__pydantic_fields__.items():
+        # If the field doesn't have the `AllowMissing` annotation, ignore it.
+        #   (i.e., just let Pydantic do its thing).
+        if not any(isinstance(m, AllowMissingAnnotation) for m in field.metadata):
+            continue
+
+        # By this point, the field **should** have some value.
+        if not hasattr(model, name):
+            raise PydanticCustomError(
+                "field_not_set",
+                'Field "{name}" is missing from the model.',
+                {"name": name},
+            )
+
+        # Now, we error out if the field is missing.
+        if getattr(model, name) is None:
+            raise PydanticCustomError(
+                "field_MISSING",
+                'Field "{name}" is still `MISSING`. Please provide a value for it.',
+                {"name": name},
+            )
