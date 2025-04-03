18fb001242f74aac32de803ea954c404cad46265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:55 +0000

    Add package deepcodec/0.0.1/deepcodec-0.0.1.tar.gz

---
+++ packages/deepcodec/deepcodec-0.0.1.tar.gz/deepcodec-0.0.1/tests/test_vfast_load.py
@@ -0,0 +1,29 @@
+from deepcodec.vfast import vfast_load, get_stats
+import numpy as np
+import time
+
+def main(video_path):
+
+    video_path = str(video_path)
+    print(type(video_path))
+    d = get_stats(video_path)
+
+    indices = list(range(0,d["num_frames"], 50))
+
+    for thread in [4,2]:
+        s = time.time()
+        data = vfast_load(video_path, indices=indices, height=360, width=360, num_threads=thread)
+        e = time.time()
+        print(f"Timing was: {e-s}")
+
+    
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    home_dir = Path.home()
+    mp4_files = home_dir.rglob("*.mp4")  # Recursively search for .mp4 files
+    file = next(mp4_files)
+    
+    print(f"Testing {file}")
+    main(file)
