d7b92d62ffd2c0db1a7ed8fcff02a9d816ddd485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:07 +0000

    Add package bitbadgespy-sdk/0.1.2/bitbadgespy_sdk-0.1.2.tar.gz

---
+++ packages/bitbadgespy-sdk/bitbadgespy_sdk-0.1.2.tar.gz/bitbadgespy_sdk-0.1.2/bitbadgespy_sdk/api/__init__.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+
+# import apis into api package
+from bitbadgespy-sdk.api.accounts_api import AccountsApi
+from bitbadgespy-sdk.api.address_lists_api import AddressListsApi
+from bitbadgespy-sdk.api.applications_api import ApplicationsApi
+from bitbadgespy-sdk.api.attestations_api import AttestationsApi
+from bitbadgespy-sdk.api.badges_api import BadgesApi
+from bitbadgespy-sdk.api.claim_alerts_api import ClaimAlertsApi
+from bitbadgespy-sdk.api.claims_api import ClaimsApi
+from bitbadgespy-sdk.api.dynamic_stores_api import DynamicStoresApi
+from bitbadgespy-sdk.api.maps_and_protocols_api import MapsAndProtocolsApi
+from bitbadgespy-sdk.api.miscellanous_api import MiscellanousApi
+from bitbadgespy-sdk.api.plugins_api import PluginsApi
+from bitbadgespy-sdk.api.sign_in_with_bit_badges_api import SignInWithBitBadgesApi
+from bitbadgespy-sdk.api.transactions_api import TransactionsApi
+from bitbadgespy-sdk.api.utility_listings_api import UtilityListingsApi
+
---
+++ packages/bitbadgespy-sdk/bitbadgespy_sdk-0.1.2.tar.gz/bitbadgespy_sdk-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bitbadgespy-sdk",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "urllib3>=1.25.3",
+        "python-dateutil",
+        "requests>=2.0.0"
+    ],
+)
