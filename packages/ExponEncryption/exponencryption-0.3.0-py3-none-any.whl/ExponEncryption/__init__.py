6901a7579b8e45c80ea54bfdbc4159bfec96c60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:52 +0000

    Add package ExponEncryption/0.3.0/exponencryption-0.3.0-py3-none-any.whl

---
+++ packages/ExponEncryption/exponencryption-0.3.0-py3-none-any.whl/ExponEncryption/__init__.py
@@ -0,0 +1,10 @@
+"""
+ExponEncryption - A Python library for secure string encryption
+"""
+
+from .encryption_V2 import ExponEncryption
+
+__version__ = "0.3.0"
+__author__ = "Eason Ma"
+
+__all__ = ["ExponEncryption"]
