05ee44600f48ca6b4ae957fb8211debe92137536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:05:47 +0000

    Add package litecnn/1.0.1/litecnn-1.0.1.tar.gz

---
+++ packages/litecnn/litecnn-1.0.1.tar.gz/litecnn-1.0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="litecnn",
+    version="1.0.1",
+    description="LiteCNN: Intuitive Python library for creating, training and visualizing convolutional neural networks. Features simplified CNN layer definition, automated training workflows, model visualization, and seamless Keras-to-ONNX conversion. Includes 15 pre-configured popular models for immediate use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Documentation": "https://github.com/Gabrli/EasyCNN---docs",
+        "Source Code" :"https://github.com/Gabrli/easyCNN",
+    },
+    keywords="deep-learning, cnn, neural-networks, tensorflow, keras, machine-learning, ai, computer-vision, image-processing, model-training, visualization, easy-to-use, python, convolutional-networks",
+    packages=find_packages(),
+    author="Gabriel Wiśniewski",
+    author_email="gabrys.wisniewski@op.pl",
+    classifiers=[  
+        "Programming Language :: Python :: 3",  
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent", 
+    ],
+     install_requires=[
+        "tensorflow",
+        "numpy",
+        "matplotlib",
+        "opencv-python",
+        "tf2onnx",
+        "onnx"
+    ],
+    entry_points={
+        "console_scripts":[
+            "litecnn-core = litecnn:LiteCNN",
+            "litecnn-preset = litecnn:Preset",
+            "litecnn-visualizer = litecnn:TrainingVisualizer",
+            "litecnn-exporter = litecnn:EasyExporter",
+            
+        ]
+    }
+)
