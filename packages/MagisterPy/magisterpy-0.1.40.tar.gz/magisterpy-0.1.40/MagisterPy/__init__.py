9512763ec821b4996d3739278939cd67205de211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 15:08:06 +0000

    Add package MagisterPy/0.1.40/magisterpy-0.1.40.tar.gz

---
+++ packages/MagisterPy/magisterpy-0.1.40.tar.gz/magisterpy-0.1.40/MagisterPy/__init__.py
@@ -0,0 +1,5 @@
+from .magister_session import MagisterSession
+from .request_manager import LoginRequestsSender
+from .jsparser import *
+from .magister_errors import *
+from .response_items import *
