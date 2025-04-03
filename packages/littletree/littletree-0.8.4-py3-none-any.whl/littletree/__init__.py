6aa7ccd4edbabca3bad0d5249bbf39f7550df2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 19:05:25 +0000

    Add package littletree/0.8.4/littletree-0.8.4-py3-none-any.whl

---
+++ packages/littletree/littletree-0.8.4-py3-none-any.whl/littletree/__init__.py
@@ -0,0 +1,15 @@
+from abstracttree import MaxDepth
+
+from .basenode import BaseNode
+from .node import Node
+
+__all__ = [
+    "BaseNode",
+    "Node",
+    "TreeMixin",
+    "MaxDepth",
+]
+
+__version__ = "0.8.4"
+
+from .treemixin import TreeMixin
---
+++ packages/littletree/littletree-0.8.4-py3-none-any.whl/littletree/exceptions.py
@@ -0,0 +1,39 @@
+class TreeException(Exception):
+    pass
+
+
+class DuplicateParentError(TreeException):
+    def __init__(self, node):
+        self.node = node
+
+    def __str__(self):
+        return (f"{self.node} already has a parent {self.node.parent}. "
+                f"Consider calling node.copy() or node.detach() first.")
+
+
+class DuplicateChildError(TreeException):
+    def __init__(self, node, identifier):
+        self.node = node
+        self.identifier = identifier
+
+    def __str__(self):
+        return f"{self.identifier} is already present in {self.node}"
+
+
+class LoopError(TreeException):
+    def __init__(self, node, ancestor):
+        self.node = node
+        self.ancestor = ancestor
+
+    def __str__(self):
+        return f"{self.node} can't become parent of {self.ancestor}"
+
+
+# TODO Currently unused. It was used in old Route-class
+class DifferentTreeError(TreeException):
+    def __init__(self, root1, root2):
+        self.root1 = root1
+        self.root2 = root2
+
+    def __str__(self):
+        return f"Nodes have different roots: {self.root1} vs {self.root2}"
