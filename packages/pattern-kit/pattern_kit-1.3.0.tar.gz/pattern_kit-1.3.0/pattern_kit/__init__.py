0308b59c993795175acde88d531e7a67f8e8af35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:05:22 +0000

    Add package pattern-kit/1.3.0/pattern_kit-1.3.0.tar.gz

---
+++ packages/pattern-kit/pattern_kit-1.3.0.tar.gz/pattern_kit-1.3.0/pattern_kit/__init__.py
@@ -0,0 +1,33 @@
+from .architectural.service_locator import ServiceLocator
+
+from .behavioral.event import Event
+from .behavioral.event_emitter import EventEmitter
+from .behavioral.handler_pipeline import Handler, AsyncHandler, HandlerPipeline
+from .behavioral.observer import Observer, AsyncObserver, Observable
+
+from .creational.factory import Factory, register_factory
+from .creational.singleton import Singleton, singleton
+from .creational.object_pool import ObjectPool, AsyncObjectPool
+
+from .structural.delegate_mixin import DelegateMixin
+
+__all__ = [
+    # Architectural patterns
+    "ServiceLocator",
+
+    # Behavioral patterns
+    "Event",
+    "EventEmitter",
+    "Handler", "AsyncHandler", "HandlerPipeline",
+    "Observable", "Observer", "AsyncObserver",
+
+    # Creational patterns
+    "Factory", "register_factory",
+    "Singleton", "singleton",
+    "ObjectPool", "AsyncObjectPool",
+
+    # Structural patterns
+    "DelegateMixin",
+]
+
+__version__ = '1.3.0'
