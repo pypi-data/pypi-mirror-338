41dc1aad62aaa85deecd173331c7cae0d1d5a964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 18:05:04 +0000

    Add package maleo-core/0.2.22/maleo_core-0.2.22.tar.gz

---
+++ packages/maleo-core/maleo_core-0.2.22.tar.gz/maleo_core-0.2.22/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/general/blood_type.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from enum import StrEnum
+from pydantic import Field
+from typing import Union
+from uuid import UUID
+from maleo_core.models.base.transfers.parameters.general import BaseGeneralParameters
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.blood_type import MaleoAccessBloodTypeGeneralTransfers
+
+class MaleoAccessBloodTypeGeneralParameters:
+    class Identifiers(StrEnum):
+        ID = "id"
+        UUID = "uuid"
+        KEY = "key"
+        NAME = "name"
+
+    class GetSingle(BaseGeneralParameters.GetSingle):
+        identifier:MaleoAccessBloodTypeGeneralParameters.Identifiers = Field(..., description="Identifier")
+        value:Union[int, UUID, str] = Field(..., description="Value")
+
+    CreateOrUpdate = MaleoAccessBloodTypeGeneralTransfers.Base
