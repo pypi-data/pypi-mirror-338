ea25654ed79ff2c72a43779bc08b8c04c4050473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:04:56 +0000

    Add package pc-metrics/0.2.5/pc_metrics-0.2.5-py3-none-any.whl

---
+++ packages/pc-metrics/pc_metrics-0.2.5-py3-none-any.whl/pc_metrics/__init__.py
@@ -0,0 +1,16 @@
+from .metrics import (
+    CD,
+    EMD,
+    MMD,
+    Coverage,
+    JSD
+)
+
+__version__ = "0.2.4"
+__all__ = [
+    'CD',
+    'EMD',
+    'MMD',
+    'Coverage',
+    'JSD'
+]
