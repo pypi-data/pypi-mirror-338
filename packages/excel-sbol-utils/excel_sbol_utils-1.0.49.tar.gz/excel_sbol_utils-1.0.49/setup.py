c0bf4715de1392833fbc14369987c4cdc5932e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:46 +0000

    Add package excel-sbol-utils/1.0.49/excel_sbol_utils-1.0.49.tar.gz

---
+++ packages/excel-sbol-utils/excel_sbol_utils-1.0.49.tar.gz/excel_sbol_utils-1.0.49/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(name='excel-sbol-utils',
+      version='1.0.49',
+      url='https://github.com/SynBioDex/Excel-utilities/',
+      license='BSD 3-clause',
+      maintainer='Jet Mante',
+      maintainer_email='jet.mante@colorado.edu',
+      include_package_data=True,
+      description='help convert excel resources into sbol',
+      packages=find_packages(include=['excel_sbol_utils']),
+      long_description=open('README.md').read(),
+      install_requires=['sbol3>=1.0.1'],
+      zip_safe=False)
