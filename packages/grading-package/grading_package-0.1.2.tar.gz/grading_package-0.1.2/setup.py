d916f25d38e9afa8b61d60a63031483fe18af3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:33 +0000

    Add package grading-package/0.1.2/grading_package-0.1.2.tar.gz

---
+++ packages/grading-package/grading_package-0.1.2.tar.gz/grading_package-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grading_package",
+    version="0.1.2",
+    description="A brief description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Elton Morden",
+    author_email="eltonmorden029@gmail.com",
+    url="https://github.com/Elton133/GRADING-PACKAGE",
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here, e.g.,
+        # 'numpy', 'pandas'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
