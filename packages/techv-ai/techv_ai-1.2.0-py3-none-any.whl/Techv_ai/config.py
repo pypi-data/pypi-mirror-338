1599af729465285e34d60f7256f08ad65bf444f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 18:05:05 +0000

    Add package techv-ai/1.2.0/techv_ai-1.2.0-py3-none-any.whl

---
+++ packages/techv-ai/techv_ai-1.2.0-py3-none-any.whl/Techv_ai/config.py
@@ -0,0 +1,4 @@
+# techv_ai/config.py
+CLIENT_ID = "01f02222-c164-4a80-aa2c-1f2b0a5c4e89"  
+TENANT_ID = "b1f77b67-e669-425e-8590-7a20ab3ba8f0"   
+SCOPES = ["User.Read"]  # Keep this unless you need additional permissions
