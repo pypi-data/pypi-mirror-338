4505993ea23470ba72541dde120442f4a854e638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:17:01 +0000

    Add package pretix-custom-head/0.1.1/pretix_custom_head-0.1.1-py3-none-any.whl

---
+++ packages/pretix-custom-head/pretix_custom_head-0.1.1-py3-none-any.whl/pretix-custom-head/__init__.py
@@ -0,0 +1,17 @@
+from django.utils.translation import gettext_lazy as _
+from pretix.base.plugins import PluginConfig
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_custom_head"
+    verbose_name = _("Custom Head Injection & Plausible Tracking")
+
+    class PretixPluginMeta:
+        name = _("Custom Head Injection & Plausible Tracking")
+        author = "Bergruebe"
+        description = _("Fügt beliebigen Code in den <head> ein und trackt Bestellungen mit Plausible Analytics.")
+        visible = True
+        version = "0.1.1"
+        compatibility = "pretix>=4.0.0"
+
+default_app_config = "pretix_custom_head.PluginApp"
