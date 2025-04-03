a421f0203b82196242ddd4a40483c7eaff23322e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:26 +0000

    Add package ml-pipeline-engine/2.1.1/ml_pipeline_engine-2.1.1-py3-none-any.whl

---
+++ packages/ml-pipeline-engine/ml_pipeline_engine-2.1.1-py3-none-any.whl/ml_pipeline_engine/node/base_nodes.py
@@ -0,0 +1,28 @@
+import typing as t
+
+from ml_pipeline_engine.node.enums import NodeType
+from ml_pipeline_engine.types import AdditionalDataT
+from ml_pipeline_engine.types import NodeBase
+from ml_pipeline_engine.types import NodeResultT
+from ml_pipeline_engine.types import Recurrent
+from ml_pipeline_engine.types import RecurrentProtocol
+
+
+class ProcessorBase(NodeBase):
+    """
+    Базовый класс для обработчиков общего назначения
+    """
+
+    node_type = NodeType.processor.value
+
+    def process(self, *args: t.Any, **kwargs: t.Any) -> NodeResultT:
+        raise NotImplementedError('Method process() is not implemented')
+
+
+class RecurrentProcessor(ProcessorBase, RecurrentProtocol):
+    """
+    Узел процессора, который может быть исполнен в рекуррентном подграфе
+    """
+
+    def next_iteration(self, data: AdditionalDataT) -> Recurrent:
+        return Recurrent(data=data)
