0b5523f0686d80060b0d439c1b0658852191e82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:10 +0000

    Add package pure-protobuf/3.1.4/pure_protobuf-3.1.4.tar.gz

---
+++ packages/pure-protobuf/pure_protobuf-3.1.4.tar.gz/pure_protobuf-3.1.4/pure_protobuf/helpers/_typing.py
@@ -0,0 +1,38 @@
+from collections.abc import Iterable
+from typing import Any, Union
+
+from typing_extensions import TypeGuard, get_args, get_origin
+
+try:
+    from types import NoneType  # type: ignore
+except ImportError:
+    NoneType = type(None)  # type: ignore
+
+
+class Sentinel:  # pragma: no cover
+    """Sentinel object used for defaults."""
+
+    def __str__(self) -> str:  # pragma: no cover
+        return "DEFAULT"
+
+    def __repr__(self) -> str:  # pragma: no cover
+        return "DEFAULT"
+
+
+DEFAULT = Sentinel()
+
+
+def extract_repeated(hint: Any) -> tuple[Any, TypeGuard[list]]:
+    """Extract a possible repeated flag."""
+    origin = get_origin(hint)
+    if isinstance(origin, type) and (origin is Iterable or issubclass(origin, list)):
+        return get_args(hint)[0], True
+    return hint, False
+
+
+def extract_optional(hint: Any) -> tuple[Any, bool]:
+    """Extract a possible optional flag."""
+    if get_origin(hint) is Union:
+        cleaned_args = tuple(arg for arg in get_args(hint) if arg is not NoneType)
+        return Union[cleaned_args], True
+    return hint, False
