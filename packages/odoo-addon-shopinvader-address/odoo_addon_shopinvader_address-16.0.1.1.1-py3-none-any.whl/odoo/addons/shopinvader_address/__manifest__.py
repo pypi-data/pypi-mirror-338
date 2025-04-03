343ef9df4fbdcaa5742d58b4eff46b771f800dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:20 +0000

    Add package odoo-addon-shopinvader-address/16.0.1.1.1/odoo_addon_shopinvader_address-16.0.1.1.1-py3-none-any.whl

---
+++ packages/odoo-addon-shopinvader-address/odoo_addon_shopinvader_address-16.0.1.1.1-py3-none-any.whl/odoo/addons/shopinvader_address/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Address",
+    "summary": """
+        Shopinvader Address: Delivery,Invoicing""",
+    "version": "16.0.1.1.1",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": [
+        "sale",
+    ],
+    "data": [],
+    "demo": [],
+}
