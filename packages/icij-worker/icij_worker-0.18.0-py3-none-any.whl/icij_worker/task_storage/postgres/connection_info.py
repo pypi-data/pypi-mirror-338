6bd353abd6dad92be4769899ed0292fe494070f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:06:17 +0000

    Add package icij-worker/0.18.0/icij_worker-0.18.0-py3-none-any.whl

---
+++ packages/icij-worker/icij_worker-0.18.0-py3-none-any.whl/icij_worker/task_storage/postgres/connection_info.py
@@ -0,0 +1,28 @@
+from functools import cached_property
+from pydantic import BaseModel
+
+from icij_common.pydantic_utils import icij_config
+
+
+class PostgresConnectionInfo(BaseModel):
+    model_config = icij_config()
+
+    host: str = "127.0.0.1"
+    password: str = "changeme"
+    port: int = 5432
+    use_ssl: bool = False
+    user: str = "postgres"
+    connect_timeout_s: float = 2.0
+
+    def url(self, db: str = "postgres") -> str:
+        url = f"postgres://{self.user}:{self.password}@{self.host}:{self.port}/{db}"
+        if not self.use_ssl:
+            url += "?sslmode=disable"
+        return url
+
+    @cached_property
+    def kwargs(self) -> dict:
+        kwargs = self.model_dump()
+        kwargs.pop("use_ssl")
+        kwargs["connect_timeout"] = int(kwargs.pop("connect_timeout_s"))
+        return kwargs
