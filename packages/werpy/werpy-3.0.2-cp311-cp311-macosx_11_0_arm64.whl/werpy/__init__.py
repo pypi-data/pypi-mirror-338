3e287287d6ec954f519e2a60ba8780a98205f883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 18:05:36 +0000

    Add package werpy/3.0.2/werpy-3.0.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/werpy/werpy-3.0.2-cp311-cp311-macosx_11_0_arm64.whl/werpy/__init__.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2023 Analytics in Motion <https://www.analyticsinmotion.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+The werpy package provides tools for calculating word error rates (WERs) and related metrics on text data.
+"""
+
+from .errorhandler import error_handler
+from .normalize import normalize
+from .metrics import metrics
+from .wer import wer
+from .wers import wers
+from .werp import werp
+from .werps import werps
+from .summary import summary
+from .summaryp import summaryp
+
+__all__ = [
+    "error_handler",
+    "normalize",
+    "metrics",
+    "wer",
+    "wers",
+    "werp",
+    "werps",
+    "summary",
+    "summaryp",
+]
