16c880af14a01e711755b4159a7f9efebff3cdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:04:48 +0000

    Add package kickcom.py/0.0.51/kickcom_py-0.0.51-py3-none-any.whl

---
+++ packages/kickcom.py/kickcom_py-0.0.51-py3-none-any.whl/kickpy/models/livestreams.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from kickpy.models.categories import Category
+
+# {
+#       "broadcaster_user_id": 1,
+#       "category": {
+#         "id": 1,
+#         "name": "text",
+#         "thumbnail": "text"
+#       },
+#       "channel_id": 1,
+#       "has_mature_content": true,
+#       "language": "text",
+#       "slug": "text",
+#       "started_at": "text",
+#       "stream_title": "text",
+#       "thumbnail": "text",
+#       "viewer_count": 1
+#     }
+
+
+@dataclass
+class LiveStream:
+    """Represents a Kick.com livestream."""
+
+    broadcaster_user_id: int
+    category: Category
+    channel_id: int
+    has_mature_content: bool
+    language: str
+    slug: str
+    started_at: datetime
+    stream_title: str
+    thumbnail: str
+    viewer_count: int
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            broadcaster_user_id=data["broadcaster_user_id"],
+            category=Category(**data["category"]),
+            channel_id=data["channel_id"],
+            has_mature_content=data["has_mature_content"],
+            language=data["language"],
+            slug=data["slug"],
+            started_at=datetime.fromisoformat(data["started_at"]),
+            stream_title=data["stream_title"],
+            thumbnail=data["thumbnail"],
+            viewer_count=data["viewer_count"],
+        )
---
+++ packages/kickcom.py/kickcom_py-0.0.51-py3-none-any.whl/kickpy/models/stream.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Stream:
+    """Represents a Kick.com stream."""
+
+    url: str
+    key: str
+    is_live: bool
+    is_mature: bool
+    language: str
+    start_time: datetime
+    viewer_count: int
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            url=data["url"],
+            key=data["key"],
+            is_live=data["is_live"],
+            is_mature=data["is_mature"],
+            language=data["language"],
+            start_time=datetime.fromisoformat(data["start_time"]),
+            viewer_count=data["viewer_count"],
+        )
