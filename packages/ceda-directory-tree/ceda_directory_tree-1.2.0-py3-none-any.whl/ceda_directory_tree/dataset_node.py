2554a8c4b196dad066616ee06ae6f7b806a34b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:06:26 +0000

    Add package ceda-directory-tree/1.2.0/ceda_directory_tree-1.2.0-py3-none-any.whl

---
+++ packages/ceda-directory-tree/ceda_directory_tree-1.2.0-py3-none-any.whl/ceda_directory_tree/dataset_node.py
@@ -0,0 +1,126 @@
+"""
+This extension of DirectoryNode is different in that when it is searched,
+it will not return the closest directory in the tree that matched.
+Instead it will return the closest directory in the tree that you actually added explicitly (dataset), not any implicit connecting directories that were added.
+
+For example if you added ``/neodc/arsf/1986/86_09`` with add_child, it will implicitly add:
+
+- ``/neodc/``
+- ``/neodc/arsf/``
+- ``/neodc/arsf/1986/``
+
+Then the directory that you actually asked to be added, `/neodc/arsf/1986/86_09`, will be added as a "dataset".
+When using search, only the closest matching dataset will be returned.
+
+Example:
+--------
+
+- ``search(/neodc/arsf/1986/86_09/file)` --> ``/neodc/arsf/1986/86_09/``
+- ``search(/neodc/arsf/1986/file)`` --> ``None``
+"""
+
+from ceda_directory_tree.directory_node import DirectoryNode
+from anytree import Node
+
+from typing import List
+
+
+class DatasetNode(DirectoryNode):
+    """
+    Subclass of ``ceda_directory_tree.directory_node.DirectoryNode`` to provide more
+    explicit dataset matching.
+    """
+    def __init__(self, name=None, parent=None, children=None, **kwargs):
+        """
+        Creates a new dataset node, uses parent's constructor (ceda_directory_tree.DirectoryNode).
+        If no name is given it is assumed that this is the root node of the tree and name is an empty string.
+        """
+        self.dataset = False
+        super().__init__(name, parent=parent, children=children, **kwargs)
+        
+    def add_child(self, child_name: str, **kwargs) -> None:
+        """
+        Adds a child to the node, will add any missing children needed to add the child.
+
+        :param child_name: Name of dataset to be added to the node. Example: ``/neodc/arsf/1986/86_09``
+        :param kwargs: Additional attributes added to the node.
+
+        """
+
+        if not self.valid_node(child_name):
+            raise ValueError(f"Invalid argument {child_name}")
+
+        node = self
+        
+        # Strip leading slash
+        child_name = child_name.lstrip("/")
+
+        # Strip trailing slash
+        child_name = child_name.rstrip("/")
+
+        for part in child_name.split("/"):
+            for child in node.children:
+                if child_name == child.name:
+                    child = DatasetNode(name=part, parent=node, **kwargs)
+                    node = child
+                    node.dataset = True
+                    return
+                if part == child.name:
+                    node = child
+            if part != node.name:
+                child = DatasetNode(name=part, parent=node, **kwargs)
+                node = child
+        if not node.dataset:
+            node.update(**kwargs)
+        node.dataset = True
+
+    def search_all(self, query: str) -> List[Node]:
+        """
+        Return all dataset nodes in the path
+
+        :param query: Name of child dataset to be searched for under the node. Example: ``/neodc/arsf/1986/86_09``
+        :return: List of matching dataset nodes
+        """
+
+        if not self.valid_node(query):
+            raise ValueError(f"Invalid argument {query}")
+
+        node = self
+        matches = []
+        for part in query.split("/")[1:]:
+            for child in node.children:
+                if part == child.name:
+                    node = child
+                    if node.dataset:
+                        matches.append(node)
+
+            if part != node.name:
+                return matches
+        return matches
+
+    def search(self, query: str) -> Node:
+        """
+        Search for a node using the tree nature of node's children.
+        Returns the found node or the closest directory explicitly added when children were added.
+
+        :param query: Name of child dataset to be searched for under the node. Example: ``/neodc/arsf/1986/86_09``
+        :returns: node
+        """
+
+        if not self.valid_node(query):
+            raise ValueError(f"Invalid argument {query}")
+
+        node = self
+        match = None
+        for part in query.split("/")[1:]:
+            for child in node.children:
+                if part == child.name:
+                    node = child
+                    if node.dataset:
+                        match = node
+
+                    # Once you have a match, break the loop
+                    break
+            if part != node.name:
+                return match
+        return match
---
+++ packages/ceda-directory-tree/ceda_directory_tree-1.2.0-py3-none-any.whl/ceda_directory_tree/directory_node.py
@@ -0,0 +1,153 @@
+"""
+Directory Node creates a hierarchical search tree from directory paths.
+Directories are added implicitly with add_child. e.g.
+
+Adding ``/neodc/arsf/1986/86_09`` with add_child, it will implicitly add:
+
+- ``/neodc/``
+- ``/neodc/arsf/``
+- ``/neodc/arsf/1986/``
+- ``/neodc/arsf/1986/86_09``
+
+These names are what is returned with directory_path function, the actual nodes names are just the name of the directory and not the full path.
+When using search, the closest directory to the search term will be returned.
+
+Example Searches:
+-----------------
+
+- ``search(/neodc/arsf/1986/86_09/file)`` --> ``/neodc/arsf/1986/86_09/``
+- ``search(/neodc/arsf/1986/file)`` --> ``/neodc/arsf/1986/``
+- ``search(/neodc/arsf/1986/file/file1/file2/file3)`` --> ``/neodc/arsf/1986/``
+- ``search(file)`` --> ``None``
+
+Example Usage:
+--------------
+
+.. code-block:: python
+
+    ceda_directory_tree = DirectoryNode()
+
+    for path in path_list:
+        ceda_directory_tree.add_child(path)
+
+    ceda_directory_tree.search(test_path)
+
+
+"""
+
+from anytree import Node
+
+
+class DirectoryNode(Node):
+    """
+    Class which provides methods for building and querying the tree
+    """
+
+    def __init__(self, name=None, parent=None, children=None, **kwargs):
+        """
+        Creates a new directory node, uses parent's constructor (anytree.Node).
+        If no name is given it is assumed that this is the root node of the tree and name is an empty string.
+        """
+        if not name:
+            name = ""
+        super().__init__(name, parent=parent, children=children, **kwargs)
+
+    def update(self, **kwargs):
+        """
+        Update the values of an existing node with new values.
+        """
+        for key, value in kwargs.items():
+            self.__setattr__(key, value)
+
+    @staticmethod
+    def valid_node(value: str) -> bool:
+        """
+        :param value: Name of node to be tested before adding to the tree
+
+        :returns: Validity of test string
+
+        Tests if a value is allowed to go into a directory tree.
+        Nodes have to be non-empty strings.
+        """
+        if not isinstance(value, str):
+            return False
+        if not value:
+            return False
+        
+        return True
+
+    def directory_path(self) -> str:
+        """
+        Get the full path of a directory node in the tree, not just the name of this directory.
+
+        :returns: name.
+        """
+        names = [ancestor.name for ancestor in self.ancestors]
+        names.append(self.name)
+
+        return f'{"/".join(names)}/'
+        
+    def add_child(self, child_name: str, **kwargs) -> None:
+        """
+        Adds a child to the node, will add any missing children needed to add the child.
+
+        :param child_name: Name of directory to be added to the node.
+                            Example: `'/neodc/arsf/1986/86_09`' will add: ``neodc`` -> ``arsf`` -> ``1986`` -> ``86_09``
+        :param kwargs: Addional node attributes
+        """
+        if not self.valid_node(child_name):
+            raise ValueError(f"Invalid argument {child_name}")
+
+        node = self
+        if child_name[0] == "/":
+            child_name = child_name[1:]
+        for part in child_name.split("/"):
+            for child in node.children:
+                if part == child.name:
+                    node = child
+            if part != node.name:
+                child = DirectoryNode(name=part, parent=node, **kwargs)
+                node = child
+
+    def search(self, query: str) -> Node:
+        """
+        Search for a node using the tree nature of node's children.
+        Returns the found node or as close as it got.
+
+        :param query: Name of child directory to be searched for under the node. Example: ``/neodc/arsf/1986/86_09``.
+
+        :returns: node (Node).
+        """
+        if not self.valid_node(query):
+            raise ValueError(f"Invalid argument {query}")
+
+        node = self
+        for part in query.split("/")[1:]:
+            for child in node.children:
+                if part == child.name:
+                    node = child
+            if part != node.name:
+                return node
+        if node == self:
+            return
+        return node
+
+    def search_name(self, query: str) -> str:
+        """
+        Search for a node using the tree nature of node's children.
+        Returns the found node's name or as close as it got.
+
+        :param query: Name of child directory to be searched for under the node. Example: ``/neodc/arsf/1986/86_09``
+
+        :returns: name
+        """
+
+        if not self.valid_node(query):
+            raise ValueError(f"Invalid argument {query}")
+
+        match = self.search(query)
+
+        if match:
+            return match.directory_path()
+        else:
+            return
