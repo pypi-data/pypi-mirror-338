5475a1af5f604541ad0d72969612c7e62db05140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 16:05:14 +0000

    Add package bigfunctions/0.32/bigfunctions-0.32-py3-none-any.whl

---
+++ packages/bigfunctions/bigfunctions-0.32-py3-none-any.whl/bigfun/templates/function_py_test.py
@@ -0,0 +1,14 @@
+CACHE = {}
+
+{{ init_code }}
+
+
+def run():
+    {% for name, value in arguments %}
+    {{ name }} = {{ value | indent(4) }}
+    {% endfor %}
+
+    {{ code | indent(4) }}
+
+result = run()
+print(result)
