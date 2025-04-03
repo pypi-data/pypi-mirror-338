8e1e3077267bc8392574f11490bc6ccf3644fc7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:06:55 +0000

    Add package xhs-mcp-server/0.1.3/xhs_mcp_server-0.1.3-py3-none-any.whl

---
+++ packages/xhs-mcp-server/xhs_mcp_server-0.1.3-py3-none-any.whl/xhs_mcp_server/server.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+from mcp.server import  FastMCP
+from mcp.types import TextContent
+
+from .write_xiaohongshu import XiaohongshuPoster
+mcp = FastMCP("xhs")
+phone = os.getenv("phone","")
+def login():
+    poster = XiaohongshuPoster()
+    poster.login(phone)
+    time.sleep(1)
+    poster.close()
+
+@mcp.tool()
+def create_note(title: str, content: str, images: list) -> list[TextContent]:
+    """Create a note with title, description, and images"""
+    poster = XiaohongshuPoster()
+    poster.login(phone)
+    res=""
+    try:
+        poster.post_article(title, content, images)
+        poster.close()
+        res="success"
+    except Exception as e:
+        res= "error:"+str(e)
+
+    return [TextContent(type="text", text=res)]
+def main():
+    mcp.run()
+if __name__ == "__main__":
+    main()
