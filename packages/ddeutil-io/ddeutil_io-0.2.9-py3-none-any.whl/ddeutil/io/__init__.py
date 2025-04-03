049c726b7c066435990696a6f87784a14654a795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 05:05:11 +0000

    Add package ddeutil-io/0.2.9/ddeutil_io-0.2.9-py3-none-any.whl

---
+++ packages/ddeutil-io/ddeutil_io-0.2.9-py3-none-any.whl/ddeutil/io/__init__.py
@@ -0,0 +1,51 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# ------------------------------------------------------------------------------
+from . import files as base
+from .__about__ import __version__
+from .__regex import RegexConf
+from .config import (
+    UPDATE_KEY,
+    VERSION_KEY,
+    Params,
+    Paths,
+    Rule,
+    Stage,
+)
+from .dirs import Dir
+from .exceptions import (
+    IOBaseError,
+    StoreArgumentError,
+    StoreNotFound,
+)
+from .files import (
+    CsvFl,
+    CsvPipeFl,
+    EnvFl,
+    Fl,
+    JsonEnvFl,
+    JsonFl,
+    JsonLineFl,
+    MarshalFl,
+    MsgpackFl,
+    PickleFl,
+    TomlEnvFl,
+    TomlFl,
+    YamlEnvFl,
+    YamlFl,
+    YamlFlResolve,
+)
+from .paths import PathSearch
+from .register import ArchiveRegister, Register
+from .stores import BaseStore, Store, StoreJsonToCsv, StoreToJsonLine
+from .utils import (
+    map_func,
+    rm,
+    search_env,
+    search_env_replace,
+    template_func,
+    template_secret,
+    touch,
+)
