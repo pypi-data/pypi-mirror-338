9c8174ef3a7dd40a0947258643c311629a4ce311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 20:08:18 +0000

    Add package plex-nfs-watchdog/0.0.11/plex_nfs_watchdog-0.0.11.tar.gz

---
+++ packages/plex-nfs-watchdog/plex_nfs_watchdog-0.0.11.tar.gz/plex_nfs_watchdog-0.0.11/src/plex_nfs_watchdog/modules/config/shared.py
@@ -0,0 +1,14 @@
+import sys
+from argparse import Namespace
+from pathlib import Path
+
+system_paths: dict[str, str] = {
+    'win32': f"AppData/Roaming",
+    'linux': f".local/share",
+    'darwin': f"Library/Application Support",
+}
+cache_path: Path = Path(f"{str(Path.home())}/{system_paths[sys.platform]}/plex_nfs_watchdog_cache/plex_config.json")
+
+listeners_type: list[str] = ["move", "modify", "create", "delete", "io_close", "io_open"]
+VERSION: str = "0.0.11"
+user_input: Namespace
