433059b0b0663ec9751e40acbd23064b8f9fedd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:45 +0000

    Add package mteb/1.36.35/mteb-1.36.35-py3-none-any.whl

---
+++ packages/mteb/mteb-1.36.35-py3-none-any.whl/mteb/tasks/Image/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .Any2AnyMultiChoice import *
+from .Any2AnyRetrieval import *
+from .Clustering import *
+from .ImageClassification import *
+from .ImageMultilabelClassification import *
+from .ImageTextPairClassification import *
+from .VisualSTS import *
+from .ZeroShotClassification import *
