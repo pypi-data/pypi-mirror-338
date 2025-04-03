68550d7492db797e0b1ea41ba8616afdaf120238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:45 +0000

    Add package ydata-sdk/2.1.1/ydata_sdk-2.1.1-cp312-cp312-musllinux_1_2_i686.whl

---
+++ packages/ydata-sdk/ydata_sdk-2.1.1-cp312-cp312-musllinux_1_2_i686.whl/ydata/dataset/__init__.py
@@ -0,0 +1,9 @@
+"""The :mod:`ydata.dataset` Model that gathers the dataset information
+including its metadata."""
+from ydata.dataset.dataset import Dataset
+from ydata.dataset.dataset_type import DatasetType
+from ydata.dataset.multidataset import MultiDataset
+
+__all__ = ["Dataset",
+           "MultiDataset",
+           "DatasetType"]
