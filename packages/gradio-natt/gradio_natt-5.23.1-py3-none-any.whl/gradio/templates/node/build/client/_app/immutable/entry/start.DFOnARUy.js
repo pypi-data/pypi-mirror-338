95034da6789ed7f5882712e8d1fc345128b69f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 03:16:41 +0000

    Add package gradio-natt/5.23.1/gradio_natt-5.23.1-py3-none-any.whl

---
+++ packages/gradio-natt/gradio_natt-5.23.1-py3-none-any.whl/gradio/templates/node/build/client/_app/immutable/entry/start.DFOnARUy.js
@@ -0,0 +1,4 @@
+import { s } from "../chunks/client.nSHfXtwW.js";
+export {
+  s as start
+};
---
+++ packages/gradio-natt/gradio_natt-5.23.1-py3-none-any.whl/gradio/templates/node/build/server/chunks/color-CADSL86_.js
@@ -0,0 +1,8 @@
+import { al as ordered_colors } from './2-D2ULSBj6.js';
+
+const get_next_color = (index) => {
+  return ordered_colors[index % ordered_colors.length];
+};
+
+export { get_next_color as g };
+//# sourceMappingURL=color-CADSL86_.js.map
