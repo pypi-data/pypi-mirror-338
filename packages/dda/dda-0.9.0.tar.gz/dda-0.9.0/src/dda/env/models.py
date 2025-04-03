8c22a44942ac2741da8b3fdcda6517f4818dd954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 15:08:53 +0000

    Add package dda/0.9.0/dda-0.9.0.tar.gz

---
+++ packages/dda/dda-0.9.0.tar.gz/dda-0.9.0/src/dda/env/models.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2024-present Datadog, Inc. <dev@datadoghq.com>
+#
+# SPDX-License-Identifier: MIT
+from __future__ import annotations
+
+from enum import StrEnum
+
+from msgspec import Struct
+
+
+class EnvironmentState(StrEnum):
+    STARTED = "started"
+    STOPPED = "stopped"
+    STARTING = "starting"
+    STOPPING = "stopping"
+    ERROR = "error"
+    NONEXISTENT = "nonexistent"
+    UNKNOWN = "unknown"
+
+
+class EnvironmentStatus(Struct, frozen=True, forbid_unknown_fields=True):
+    state: EnvironmentState
+    info: str = ""
