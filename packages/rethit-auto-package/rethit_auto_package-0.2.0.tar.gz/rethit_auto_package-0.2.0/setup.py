a3a9ccde87b49f3a79f678d16511803f80b84f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 15:09:01 +0000

    Add package rethit-auto-package/0.2.0/rethit_auto_package-0.2.0.tar.gz

---
+++ packages/rethit-auto-package/rethit_auto_package-0.2.0.tar.gz/rethit_auto_package-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rethit_auto_package",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=['packaging==24.2', 'requests==2.32.3', 'setuptools==78.1.0'],
+    entry_points={
+        'console_scripts': [
+            'rethit_auto_package = rethit_auto_package.main:main',
+        ],
+    },
+    python_requires='>=3.6',
+    author="gigascake",
+    author_email="gigascake@gmail.com",
+    description="A CLI tool generated from source based python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
