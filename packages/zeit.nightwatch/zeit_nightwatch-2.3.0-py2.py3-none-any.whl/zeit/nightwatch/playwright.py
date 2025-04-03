63d630b9d65af8cf3502286c113b567c45ff6a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 18:05:41 +0000

    Add package zeit.nightwatch/2.3.0/zeit_nightwatch-2.3.0-py2.py3-none-any.whl

---
+++ packages/zeit.nightwatch/zeit_nightwatch-2.3.0-py2.py3-none-any.whl/zeit/nightwatch/playwright.py
@@ -0,0 +1,17 @@
+from playwright.sync_api._generated import Page
+
+
+def sso_login(self, url, username, password):
+    self.goto(url)
+    if self.locator("#kc-login").count() == 1:
+        self.locator("#username").fill(username)
+        self.locator("#password").fill(password)
+        self.locator("#kc-login").click()
+    else:
+        self.locator("#login_email").fill(username)
+        self.locator("#login_pass").fill(password)
+        self.locator("input.submit-button.log").click()
+    self.wait_for_url("**/konto")
+
+
+Page.sso_login = sso_login
