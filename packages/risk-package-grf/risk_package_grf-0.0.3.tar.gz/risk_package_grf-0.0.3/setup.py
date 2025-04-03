fa2013bb51733acaa234634dbd9e8090abdaa884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 14:05:16 +0000

    Add package risk-package-grf/0.0.3/risk_package_grf-0.0.3.tar.gz

---
+++ packages/risk-package-grf/risk_package_grf-0.0.3.tar.gz/risk_package_grf-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='risk_package_grf', # Como se descargará el paquete, i.e. el nombre.
+        version='0.0.3',
+        author='Mateo Rodriguez',
+        author_email='mateoalejandrorr641@gmail.com',
+        description='Simple test package',
+        long_description=open("README.md").read(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=['networkx'],
+        license_files=["LICENSE.txt"],
+)
