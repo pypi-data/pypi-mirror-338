d479b3211c4a45af7811e359aadd6931bf281ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-03 04:04:44 +0000

    Add package lightblue-ai/0.0.3/lightblue_ai-0.0.3.tar.gz

---
+++ packages/lightblue-ai/lightblue_ai-0.0.3.tar.gz/lightblue_ai-0.0.3/lightblue_ai/settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    default_model: str
+    sub_agent_model: str | None = None
+
+    tavily_api_key: str | None = None
+    bfl_api_key: str | None = None
+    urlbox_api_key: str | None = None
+
+    mcp_config_path: str = (Path.cwd() / "./mcp.json").expanduser().resolve().absolute().as_posix()
+
+    model_config = SettingsConfigDict(case_sensitive=False, frozen=True, env_file=".env")
