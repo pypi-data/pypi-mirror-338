ce4078d99e6b6c1af4fb51d25cf4e48563fe7607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:52 +0000

    Add package square-logger/1.0.7/square_logger-1.0.7.tar.gz

---
+++ packages/square-logger/square_logger-1.0.7.tar.gz/square_logger-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="square_logger",
+    version="1.0.7",
+    packages=find_packages(),
+    install_requires=[],
+    author="thePmSquare, Amish Palkar",
+    author_email="thepmsquare@gmail.com, amishpalkar302001@gmail.com",
+    description="python logger for my personal use.",
+    url="https://github.com/thepmsquare/square_logger",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
