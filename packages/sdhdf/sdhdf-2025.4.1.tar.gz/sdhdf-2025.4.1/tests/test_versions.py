e0e9386f949114565a72c02712223ed2614f73e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:14 +0000

    Add package sdhdf/2025.4.1/sdhdf-2025.4.1.tar.gz

---
+++ packages/sdhdf/sdhdf-2025.4.1.tar.gz/sdhdf-2025.4.1/tests/test_versions.py
@@ -0,0 +1,17 @@
+# SDHDF v1.9.3 (NOTE: This file has a single integration only)
+# fname = "./test_data/sdhdf_v1.9.3.hdf"
+#
+# SDHDF v2.0
+# fname = "./test_data/sdhdf_v2.0.hdf"
+#
+# SDHDF v2.1
+# fname = "./test_data/sdhdf_v2.1.hdf"
+#
+# SDHDF v2.2
+# fname = "./test_data/sdhdf_v2.2.hdf"
+#
+# SDHDF v3.0
+# fname = "./test_data/sdhdf_v3.0.hdf"
+#
+# SDHDF v4.0 (NOTE: this version onwards has compound attributes)
+# fname = "./test_data/sdhdf_v4.0.hdf"
