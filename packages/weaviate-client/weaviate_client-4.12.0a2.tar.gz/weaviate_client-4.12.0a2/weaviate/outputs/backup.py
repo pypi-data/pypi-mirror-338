08b9e2ce9cded780620e584d5ce6d6a75f77e765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 16:06:27 +0000

    Add package weaviate-client/4.12.0a2/weaviate_client-4.12.0a2.tar.gz

---
+++ packages/weaviate-client/weaviate_client-4.12.0a2.tar.gz/weaviate_client-4.12.0a2/weaviate/outputs/backup.py
@@ -0,0 +1,8 @@
+from weaviate.backup.executor import BackupStatus, BackupStatusReturn, BackupStorage, BackupReturn
+
+__all__ = [
+    "BackupStatus",
+    "BackupStatusReturn",
+    "BackupStorage",
+    "BackupReturn",
+]
---
+++ packages/weaviate-client/weaviate_client-4.12.0a2.tar.gz/weaviate_client-4.12.0a2/weaviate/outputs/users.py
@@ -0,0 +1,3 @@
+from weaviate.users.executor import UserDB, UserTypes, OwnUser
+
+__all__ = ["OwnUser", "UserDB", "UserTypes"]
