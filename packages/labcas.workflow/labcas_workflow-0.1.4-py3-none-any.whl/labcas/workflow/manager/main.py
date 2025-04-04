f80e723f853220325ba6af4ece34f8214b87db78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:04:48 +0000

    Add package labcas.workflow/0.1.4/labcas_workflow-0.1.4-py3-none-any.whl

---
+++ packages/labcas.workflow/labcas_workflow-0.1.4-py3-none-any.whl/labcas/workflow/manager/main.py
@@ -0,0 +1,24 @@
+from labcas.workflow.manager import process_collection
+from labcas.workflow.steps.alphan.process import process_img
+from distributed import Client
+
+from distributed.security import Security
+
+# TODO: add ssl certification to access the dask cluster
+# sec = Security(
+#     tls_ca_file=os.getenv("DASK_TLS_CA"),
+#     tls_client_cert=os.getenv("DASK_TLS_CERT"),
+#     tls_client_key=os.getenv("DASK_TLS_KEY"),
+#     require_encryption=True
+# )
+
+if __name__ == '__main__':
+    client = Client('tcp://127.0.0.1:8786')
+    process_collection(
+        'edrn-bucket',
+        'nebraska_images/',
+        'nebraska_images_nuclei/',
+        process_img,
+        dict(tile_size=64)
+    )
+
