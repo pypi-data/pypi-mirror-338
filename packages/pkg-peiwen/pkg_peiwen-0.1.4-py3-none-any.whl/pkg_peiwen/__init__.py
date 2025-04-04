d1cac24540510948a0fa7dce1e2c4833f6ff5e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:28 +0000

    Add package pkg-peiwen/0.1.4/pkg_peiwen-0.1.4-py3-none-any.whl

---
+++ packages/pkg-peiwen/pkg_peiwen-0.1.4-py3-none-any.whl/pkg_peiwen/__init__.py
@@ -0,0 +1,9 @@
+"""
+this is the doc string for the pkg_peiwen package.
+
+we modify attributes of the package by editing __init__.py file.
+"""
+
+__version__ = "0.1.4" 
+# manually set the version of the package here, 
+# it should match the version in toml file.
---
+++ packages/pkg-peiwen/pkg_peiwen-0.1.4-py3-none-any.whl/pkg_peiwen/function.py
@@ -0,0 +1,12 @@
+"""
+This is the doc for the `pkg_peiwen.function` module.
+Put module-level docstring at the top of the module file.
+"""
+
+def add_one(x):
+    """
+    An example function from my example pacakge.
+
+    Add one to the input number.
+    """
+    return x + 1
