51dc5d92d093ef635d002f55619b76d2b9adbe66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 16:06:38 +0000

    Add package muutils/0.8.5/muutils-0.8.5-py3-none-any.whl

---
+++ packages/muutils/muutils-0.8.5-py3-none-any.whl/muutils/__init__.py
@@ -0,0 +1,31 @@
+"""
+.. include:: ../README.md
+"""
+
+from __future__ import annotations
+
+__all__ = [
+    # submodules (with sub-submodules)
+    "json_serialize",
+    "logger",
+    "misc",
+    "nbutils",
+    # submodules
+    "console_unicode",
+    "dbg",
+    "dictmagic",
+    "errormode",
+    "group_equiv",
+    "interval",
+    "jsonlines",
+    "kappa",
+    "mlutils",
+    "parallel",
+    "spinner",
+    "statcounter",
+    "sysinfo",
+    "tensor_info",
+    "tensor_utils",
+    "timeit_fancy",
+    "validate_type",
+]
