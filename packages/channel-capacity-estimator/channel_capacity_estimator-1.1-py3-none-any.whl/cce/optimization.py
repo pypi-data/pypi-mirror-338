da26910d6f510983727269c7290487c38d95b0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 08:05:12 +0000

    Add package channel-capacity-estimator/1.1/channel_capacity_estimator-1.1-py3-none-any.whl

---
+++ packages/channel-capacity-estimator/channel_capacity_estimator-1.1-py3-none-any.whl/cce/optimization.py
@@ -0,0 +1,75 @@
+# This file is part of Channel Capacity Estimator,
+# licenced under GNU GPL 3 (see file License.txt).
+# Homepage: http://pmbm.ippt.pan.pl/software/cce 
+
+from collections import Counter
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+def weight_optimizer(neighb_count, labels) -> (float, list):
+    """Returns loss and optimized weights for given neighbors description.
+
+    Parameters
+    ----------
+    neighb_count : numpy array of shape (# of data points, # of labels)
+        describes for each point the number of neighbors with each label
+
+    labels : numpy array of shape (# of data points, )
+        label for each point
+
+    Returns
+    -------
+    float
+        loss
+    list
+        weight for each label
+    """
+    # reset graph before each run
+    tf.reset_default_graph()
+
+    num_data, num_labels = neighb_count.shape
+    label_counts = np.zeros([num_labels])
+    for label, count in Counter(labels).most_common():
+        label_counts[label] = count
+
+    # neighbors matrix
+    neigh_matx = tf.constant(neighb_count, dtype=tf.float32)
+
+    # label count vector
+    label_cnts = tf.constant(label_counts, dtype=tf.float32)
+
+    # logits -- to be optimized
+    logits = tf.Variable(np.ones(num_labels), dtype=tf.float32)
+
+    # weights
+    w = tf.nn.softmax(logits)
+
+    # weight lookup list
+    w_list = tf.reduce_sum(tf.one_hot(labels, num_labels) * w, axis=1)
+
+    # label cnts lookup list
+    label_cnts_list = tf.reduce_sum(tf.one_hot(labels, num_labels)
+                                    * label_cnts, axis=1)
+    nx = w * num_data
+
+    ny = label_cnts_list / w_list \
+         * tf.reduce_sum(neigh_matx * (w/label_cnts), axis=1)
+
+    loss = (tf.reduce_sum(tf.digamma(nx) * w) +
+            tf.reduce_sum(tf.digamma(ny) * w_list / label_cnts_list))
+
+    optimizer = tf.train.AdamOptimizer()
+    train = optimizer.minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        #print("Starting training...")
+        for _ in range(5000):
+            curr_loss, curr_w, __ = sess.run([loss, w, train])
+            #if _ % 250 == 0:
+            #    print("steps: %s, loss: %s, w: %s"
+            #        % (_, curr_loss, curr_w))
+        #print("Done.")
+        return sess.run([loss, w])
---
+++ packages/channel-capacity-estimator/channel_capacity_estimator-1.1-py3-none-any.whl/cce/preprocessing.py
@@ -0,0 +1,93 @@
+# This file is part of Channel Capacity Estimator,
+# licenced under GNU GPL 3 (see file License.txt).
+# Homepage: http://pmbm.ippt.pan.pl/software/cce 
+
+"""Input data preprocessing functions"""
+
+import numpy as np
+
+
+def _project_labels(data: list) -> list:
+    return [x[0] for x in data]
+
+
+def _project_coords(data: list) -> list:
+    return [x[1] for x in data]
+
+
+def normalize(data: list) -> list:
+    """Performs input data normalization.
+
+    Parameters
+    ----------
+    data : list
+        data is a list of tuples. Each tuple has form (label, value),
+        where label can be either int or string and value is a
+        one-dimensional numpy array/list representing coordinates.
+
+    Returns
+    -------
+    list
+        list of data points. Each data point is normalized to interval
+        [0, 1].
+    """
+    lab = _project_labels(data)
+    arr = _project_coords(data)
+    arr = np.array(arr)
+
+    max_ = np.amax(arr)
+    min_ = np.amin(arr)
+
+    arr = (arr - min_)/(max_ - min_)
+
+    return list(zip(lab, arr))
+
+
+def unique(arr) -> bool:
+    """Checks if all points in the array of coordinates are unique.
+
+    Parameters
+    ----------
+    arr : iterable
+        arr [[float, float, ...], ...]
+
+    Returns
+    --------
+    bool:
+        True if points are distinct, False otherwise
+    """
+    return len(arr) == len({tuple(p) for p in arr})
+
+
+def add_noise_if_duplicates(data: list) -> list:
+    """Adds noise to input data.
+
+    Parameters
+    ----------
+    data : list
+        data [(label, value), ...], where value is list of floats
+
+    Returns
+    -------
+    list
+        data with added noise
+    """
+
+    lab, arr = _project_labels(data), _project_coords(data)
+    arr = np.array(arr)
+    if not unique(arr):
+        print("WARNING: data contains points that are not unique.",
+              "Data will be perturbed to avoid numerical issues.")
+        eps = min(filter(lambda x: x > 0,
+                         {x for x in np.ndarray.flatten(arr)}))
+        eps /= 2
+
+        arr += eps * np.random.rand(*arr.shape)
+        while not unique(arr):
+            eps /= 2
+            arr += eps * np.random.rand(*arr.shape)
+            if eps == 0.:
+                raise Exception("Cannot add noise to input data.")
+        print("eps used:", eps)
+
+    return list(zip(lab, arr))
