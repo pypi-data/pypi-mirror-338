b6f2632df649df10294b3ce20bbecf1bc5d52eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:05:02 +0000

    Add package mn2/0.0.13/mn2-0.0.13-py3-none-any.whl

---
+++ packages/mn2/mn2-0.0.13-py3-none-any.whl/mn2/variances.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import torch as th
+
+class Random(th.nn.ModuleList):
+
+    def __init__(self, *args):        
+        super().__init__(*args)
+        
+        self.indices = [ term.getr() for term in self ]
+        self.slices = [ term.getq() for term in self ]
+
+    def forward(self, theta : th.tensor):
+        
+        thetas = th.split(theta, self.indices)        
+        blocks = [ term.forward(param) for term,param in zip(self, thetas) ]
+
+        ZLambda = th.cat(blocks, dim=1)
+        
+        return ZLambda
+
+    def getr(self): return sum(self.indices)
+    def getq(self): return sum(self.slices)
+    def getrs(self): return self.indices
+    def getqs(self): return self.slices
