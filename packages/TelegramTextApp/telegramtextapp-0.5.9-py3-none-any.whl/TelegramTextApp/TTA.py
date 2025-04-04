e69faddad8024cbd9db7e0b8b23d48ffc151ce5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:41 +0000

    Add package TelegramTextApp/0.5.9/telegramtextapp-0.5.9-py3-none-any.whl

---
+++ packages/TelegramTextApp/telegramtextapp-0.5.9-py3-none-any.whl/TelegramTextApp/TTA.py
@@ -0,0 +1,202 @@
+import telebot
+import threading
+from TelegramTextApp import TTA_menus
+from TelegramTextApp import TTA_scripts
+import inspect
+from telebot import apihelper
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s [%(asctime)s]   %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+VERSION="0.5.9"
+
+def start(api, menus, debug=False, tta_experience=False, formating_text=None, app=None, port=5000):
+    TTA_scripts.create_file_menus(f"{menus}.json")
+
+    # путь файла, откуда был запущен TTA
+    current_frame = inspect.currentframe()
+    caller_frame = current_frame.f_back
+    caller_filename = caller_frame.f_code.co_filename # полный путь
+
+    locale = TTA_menus.settings_menu(f"{menus}.json", caller_filename, formating_text, tta_experience) # получение всего json файла
+
+    # добавление функций в глобальный список функций из файла запуска 
+    import sys
+    from importlib.util import spec_from_file_location, module_from_spec
+    sys.path.append("scripts.py")
+    module = module_from_spec(spec_from_file_location("scripts", caller_filename))
+    module.__spec__.loader.exec_module(module)
+    globals().update(vars(module))
+
+    if app == True: # запуск приложения
+        from TelegramTextApp.developer_application import app
+        app.start_app(f"{menus}.json", caller_filename, port=port)
+
+    # установка команд бота
+    TTA_scripts.get_config()
+    bot = telebot.TeleBot(api)
+    commands = []
+    for command in locale["commands"]:
+        commands.append(telebot.types.BotCommand(command, locale["commands"][command]["text"]))
+    bot.set_my_commands(commands)
+
+    # установка описаний бота
+    if locale.get('bot'):
+        try:
+            bot.set_my_name(locale['bot'].get('name'))
+            bot.set_my_description(locale['bot'].get('description'))
+            bot.set_my_short_description(locale['bot'].get('short_description'))
+        except:
+            logging.error("Не удалось обновить данные бота")
+            pass
+
+# ----------------------------------------------------------
+
+    def step_handler(message, bot_data, telegram_data): # ожидание ввода от пользователя (пока только сообщения)
+        if tta_experience == True:
+            bot.delete_message(TTA_scripts.user_tg_data(telegram_data), message.message_id)
+        input_text = message.text
+        handler_data = bot_data["handler"]
+        function_name = (handler_data['function'])
+        menu_name = (handler_data['menu'])
+        menu_base = menu_name.split(":")
+        menu_name = menu_base[0]
+        data = (menu_name).replace(f"{menu_base[0]}:", "")
+        call_data = {"menu":menu_name, "data":data, "input_text":input_text} 
+        if debug == True:
+            logging.info(f"{telegram_data.message.chat.id}: handler > {menu_name} ({input_text})")
+
+        function = globals()[function_name]
+        han_data = function({"telegram_data":telegram_data, "call_data":call_data})
+        handler_data = {"input_text":message.text, "menu":menu_name, "error":False, "data":None}
+        if han_data == False: handler_data["error"] = True
+        else: handler_data["data"] = han_data
+        processing_data(telegram_data, handler_data=handler_data)
+
+
+    def send_menu(data, bot_data, handler_data=None):
+        type_send = bot_data["send"]
+        recipient = type_send["recipient"]
+        menu = type_send.get("menu")
+        send_data = {"menu":menu, "handler_data":handler_data}
+        menu_data = TTA_menus.open_menu(data=data, send_data=send_data)
+        user_id = TTA_scripts.SQL_request("SELECT telegram_id FROM TTA WHERE telegram_id = ?", (recipient,))
+
+        if menu:
+            send_text = menu_data["text"]
+            menu_name = menu
+        else:
+            send_text = type_send["text"]
+            menu_name = send_text
+
+        if debug == True:
+            if user_id:
+                recipient = user_id[0]
+            logging.info(f"{data.message.chat.id}: send > {menu_name} > {recipient}")
+
+        if recipient == 'all':
+            role_users = TTA_scripts.SQL_request("SELECT telegram_id FROM TTA WHERE role IS NOT NULL",(), True)
+        elif user_id:
+            role_users = [user_id]
+        else:
+            role_users = TTA_scripts.SQL_request("SELECT telegram_id FROM TTA WHERE role=?",(recipient,), True)
+
+        for user_id in role_users:
+            bot.send_message(user_id[0], send_text, reply_markup=menu_data["keyboard"], parse_mode="MarkdownV2")
+
+# -----
+
+    def processing_data(data, handler_data=None):  # обработка данных, получаемых после открытия меню
+        bot_data = TTA_menus.open_menu(data=data, handler_data=handler_data)
+
+        if hasattr(data, 'data') and data.data is not None:
+            user_id, menu_id = TTA_scripts.update_user(call=data)
+            if bot_data.get("loading"):
+                try:
+                    bot.edit_message_text(chat_id=user_id, message_id=menu_id, text=bot_data["text"], parse_mode="MarkdownV2")
+                except apihelper.ApiTelegramException as e: # проверка на двойное нажатие
+                    if "message is not modified" in str(e): pass
+                bot_data = TTA_menus.open_menu(data, loading=True)
+            if bot_data.get("handler"):
+                bot.register_next_step_handler(data.message, step_handler, bot_data, data)
+            if bot_data.get("send"):
+                send_menu(data, bot_data, handler_data)
+            if bot_data.get("query"):
+                bot.answer_callback_query(callback_query_id=data.id,text=bot_data['query']['text'], show_alert=bot_data['query']['show_alert'])
+    
+            try:
+                bot.edit_message_text(chat_id=user_id, message_id=menu_id, text=bot_data["text"], reply_markup=bot_data["keyboard"], parse_mode="MarkdownV2")
+            except apihelper.ApiTelegramException as e: # проверка на двойное нажатие
+                if "message is not modified" in str(e): pass
+
+        elif hasattr(data, 'text') and data.text is not None:
+            if bot_data.get("handler"):
+                return
+            user_id = data.chat.id
+            if bot_data.get("loading"):
+                new_message = bot.send_message(data.chat.id, bot_data["text"], parse_mode="MarkdownV2")
+                TTA_scripts.update_user(message=new_message) # обновление данных пользователя
+                bot_data = TTA_menus.open_menu(data, loading=True)
+                bot.edit_message_text(chat_id=user_id, message_id=new_message.message_id, text=bot_data["text"], reply_markup=bot_data["keyboard"], parse_mode="MarkdownV2")
+            else:
+                new_message = bot.send_message(data.chat.id, bot_data["text"], reply_markup=bot_data["keyboard"], parse_mode="MarkdownV2")
+                TTA_scripts.update_user(message=new_message)
+
+            if bot_data.get("send"):
+                send_menu(data, bot_data, handler_data)
+
+
+    @bot.message_handler()
+    def text_handler(message): # обработка полученного текста
+        user_id = message.chat.id
+        old_menu = None
+
+        if message.text[0] == "/": # проверка на команду
+            old_menu = TTA_scripts.SQL_request("SELECT menu_id FROM TTA WHERE telegram_id = ?", (user_id,))
+            if old_menu and tta_experience == True:
+                try:
+                    bot.delete_message(user_id, int(old_menu[0]))
+                except: pass
+            if debug == True:
+                logging.info(f"{user_id}: {message.text}")
+
+            processing_data(message)
+
+        if tta_experience == True:
+            bot.delete_message(user_id, message.message_id)
+    
+    
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_query(call):  # работа с вызовами inline кнопок
+        user_id, menu_id = TTA_scripts.update_user(call=call)
+        bot.clear_step_handler_by_chat_id(chat_id=user_id)
+        if debug == True:
+            logging.info(f"{user_id}: {call.data}")
+            
+        if (call.data).split("-")[0] == "none": return
+    
+        elif (call.data).split("-")[0] == "notification":
+            bot.delete_message(user_id, menu_id)
+            return
+    
+        processing_data(call)
+    
+    
+    def start_polling():
+        logging.info(f"бот запущен...")
+        while True:
+            try:
+                bot.polling(none_stop=True, timeout=60)
+            except Exception as e:
+                logging.error(e)
+                logging.info(f"Перезапуск...")
+    if debug == False: start_polling()
+    else:
+        logging.info(f"Режим разработчика")
+        logging.info(f"Версия TTA: {VERSION}")
+        logging.info(f"бот запущен...")
+        bot.polling()
---
+++ packages/TelegramTextApp/telegramtextapp-0.5.9-py3-none-any.whl/TelegramTextApp/TTA_menus.py
@@ -0,0 +1,247 @@
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telebot import types
+from TelegramTextApp.TTA_scripts import *
+import json
+import logging
+import inspect
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s [%(asctime)s]   %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+LOCALE_PATH = None
+TTA_EXPERIENCE = False
+
+def settings_menu(menus, script_path, formating_text, tta_experience):
+    global LOCALE_PATH, format_text, TTA_EXPERIENCE
+    TTA_EXPERIENCE = tta_experience
+    LOCALE_PATH = menus
+    format_text = formating_text
+    import sys
+    from importlib.util import spec_from_file_location, module_from_spec
+    sys.path.append("scripts.py")
+    module = module_from_spec(spec_from_file_location("scripts", script_path))
+    module.__spec__.loader.exec_module(module)
+    globals().update(vars(module))
+
+    with open(LOCALE_PATH, 'r', encoding='utf-8') as file:
+        commands = json.load(file)
+    return commands
+
+def get_locale():
+    with open(LOCALE_PATH, 'r', encoding='utf-8') as file:
+        locale = json.load(file)
+        return locale
+
+def processing_text(text, tta_data, edit="text"):
+    user_id = user_tg_data(tta_data['telegram_data'])
+    text = data_formated(text, user_id)
+    if format_text:
+        function_format = globals()[format_text]
+        text = function_format(tta_data, text, edit)
+    text = markdown(text)
+    return text
+
+
+def create_buttons(tta_data, custom_buttons=None):
+    locale = get_locale()
+    data_menu = tta_data["call_data"].get('data') # значение для навигационных кнопок
+    menu = tta_data["call_data"]["menu"] # название меню, для навигационных кнопок
+    page = int(tta_data["call_data"]["page"])
+    buttons_data = tta_data["menu_data"].get("buttons")
+    if isinstance(buttons_data, str):
+        buttons_data = custom_buttons
+    if buttons_data is None: buttons_data = {}
+
+    btn_role = 'user'
+    user_id = user_tg_data(tta_data["telegram_data"])
+    role =  SQL_request("SELECT role FROM TTA WHERE telegram_id = ?", (int(user_id),))[0]
+
+    list_page = (tta_data["menu_data"].get("list_page"))
+    if list_page is None: list_page = 20
+
+    width = (tta_data["menu_data"].get("width"))
+    if width is None: width = 2
+    keyboard = InlineKeyboardMarkup(row_width=width)
+
+    buttons = []
+    nav_buttons = []
+    start_index = int(page) * list_page
+    end_index = start_index + list_page
+    paginated_data = list(buttons_data.items())[start_index:end_index]
+    
+    for data, text in paginated_data:
+        slash  = text
+        callback = data
+        data_button = ""
+        text = text.replace("\\","")
+        if len(data.split(":")) > 1:
+            callback = data.split(":")[0]
+            data_button = data.replace(f"{callback}:", "")
+            if format_text:
+                function_format = globals()[format_text]
+                data_button = function_format(tta_data, data_button)
+
+        var_button = locale["var_buttons"].get(callback)
+        if var_button:
+            callback_button = text
+            if isinstance(var_button, dict):
+                text = var_button["text"]
+                btn_role = var_button["role"]
+            else:
+                text = locale["var_buttons"][callback]
+            callback = callback_button
+
+        if btn_role == "user" or btn_role == role:
+            if callback == "url":
+                button = types.InlineKeyboardButton(text, url=data_button)
+            elif callback == "app":
+                button = types.InlineKeyboardButton(text, web_app=types.WebAppInfo(url=data_button))
+            else:
+                button = types.InlineKeyboardButton(text, callback_data=f'{callback}-{page}:{data_button}')
+        else:
+            continue
+    
+        if slash[0] == "\\":
+            if buttons:
+                keyboard.add(*buttons)
+                buttons = []
+                buttons.append(button)
+        else:
+            buttons.append(button)
+    if buttons:
+        keyboard.add(*buttons)
+
+    if len(buttons_data) > list_page:
+        if data_menu == "None": data_menu = None
+        nav_buttons = []
+        if int(page) > 0:
+            nav_buttons.append(types.InlineKeyboardButton(f'⬅️ • {page} •', callback_data=f'{menu}-{page-1}:{data_menu}'))
+        if end_index < len(buttons_data):
+            nav_buttons.append(types.InlineKeyboardButton(f'• {page+1+1} • ➡️', callback_data=f'{menu}-{page+1}:{data_menu}'))
+        keyboard.add(*nav_buttons)
+    
+    return keyboard
+
+
+def menu_layout(data, handler_data, send_data):    
+    try:
+        if hasattr(data, 'data') and data.data is not None:
+            menu_base = (data.data).split(":")
+            menu_name = menu_base[0].split("-")[0]
+            menu_page = menu_base[0].split("-")[1]
+            get_data = (data.data).replace(f"{menu_base[0]}:", "")
+            if get_data == "": get_data = None
+
+        elif hasattr(data, 'text') and data.text is not None:
+            locale = get_locale()
+            command = (data.text).replace("/", "")
+            menu_name = "error_command"
+            if locale["commands"].get(command):
+                menu = locale["commands"][command]["menu"]
+            get_data = None
+            menu_base = menu.split(":")
+            menu_name = menu_base[0]
+            get_data = (menu).replace(f"{menu_name}:", "")
+            menu_page = "0"
+            if command == "start":
+                registration(data)
+        
+        input_text = None
+        if handler_data:
+            get_data =  f'{get_data}/{handler_data["data"]}'
+            input_text = handler_data.get("input_text")
+        if send_data and send_data.get("menu"):
+            menu_base = (send_data["menu"]).split(":")
+            menu_name = menu_base[0].split("-")[0]
+            get_data = (send_data["menu"]).replace(f"{menu_base[0]}:", "")
+            input_text = send_data['handler_data'].get("input_text")
+
+        call_data = {"menu":menu_name, "page":menu_page, "data":get_data, "input_text":input_text} 
+    except Exception as e:
+        logging.error(e)
+        call_data = {"menu":"error_command", "page":"0", "data":None, "input_text":None}
+    return call_data
+
+def open_menu(data, loading=False, handler_data=None, send_data=None):
+    call_data = menu_layout(data, handler_data, send_data) # данные, передаваемые в меню
+
+    error = None
+    locale = get_locale() # весь json файл
+    menus = locale["menus"] # все меню
+    menu = menus.get(call_data['menu']) # меню, которое будем обрабатывать
+    if handler_data:
+        error = handler_data["error"]
+        if error != True:
+            menu = menus.get(handler_data["menu"]) # меню, которое отдает handler   
+    if menu is None: menu = menus['error'] # если меню отстутсвует, то обрабатываем меню error
+
+
+    tta_data = {"menu_data":menu, "call_data":call_data, "telegram_data":data}
+    bot_data = {} # контейнер данных, для бота
+
+
+    if menu.get('loading') is not None and loading == False: # отправляется только текст, что бы потом обработать всё меню
+        bot_data["text"] = markdown(menu['loading'])
+        bot_data['loading'] = True
+        return bot_data
+
+    if menu.get('function') is not None: # выполнение указанной функции
+        function_name = (menu['function'])
+        function = globals()[function_name]
+        function_data = function(tta_data)
+        if function_data:
+            tta_data = function_data
+
+
+# ---
+
+    if menu.get('text') is not None:
+        text = processing_text(menu['text'], tta_data)
+    else: text = None
+    bot_data["text"] = text
+
+    if error == True and menu.get("error_text"): # добавление ошибочного текста
+        bot_data["text"] = processing_text(menu["error_text"], tta_data)
+
+# ---
+
+
+    if isinstance(menu.get("buttons"), str): # добавление кнопок
+        function_name = menu['buttons']
+        function = globals()[function_name]
+        function_data = function(tta_data)
+        keyboard = create_buttons(tta_data, custom_buttons=function_data)
+    else:
+        keyboard = create_buttons(tta_data)
+
+    if menu.get('return') is not None: # кнопка возврата
+        btn_return = InlineKeyboardButton((locale["var_buttons"]['return']), callback_data=f'{menu["return"]}-0:')
+        keyboard.add(btn_return)
+
+# ---
+
+    if menu.get('handler') is not None: # ожидание ввода
+        bot_data["handler"] = menu["handler"]
+        function_format = globals()[format_text]
+        bot_data["handler"]["menu"] = function_format(tta_data, bot_data["handler"]["menu"]) # 4
+
+    if menu.get('send') is not None: # Отправка сообщения
+        if menu['send'].get("text"):
+            menu['send']['text'] = processing_text(menu['send']['text'], tta_data)
+        elif menu['send'].get("menu"):
+            menu['send']['menu'] = processing_text(menu['send']['menu'], tta_data, None)
+        menu['send']['recipient'] = processing_text(menu['send']['recipient'], tta_data, None)
+        bot_data["send"] = menu["send"] # 3
+                                                                                                                                                                     
+        if TTA_EXPERIENCE == True and menu.get("text") is None:
+            btn_notif = InlineKeyboardButton((locale["var_buttons"]['notification']), callback_data=f'notification')
+            keyboard.add(btn_notif)
+
+    if menu.get('query') is not None:
+        bot_data['query'] = menu['query'] # 2
+
+    bot_data["keyboard"] = keyboard # 1
+    return bot_data
