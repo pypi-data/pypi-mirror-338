4a9fbe411bee8599a4001b3983159187774df239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 03:17:11 +0000

    Add package grlc/1.3.10/grlc-1.3.10.tar.gz

---
+++ packages/grlc/grlc-1.3.10.tar.gz/grlc-1.3.10/src/queryTypes.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2022 Albert Meroño, Rinke Hoekstra, Carlos Martínez
+#
+# SPDX-License-Identifier: MIT
+
+"""Definition of grlc query types."""
+
+qType = {"SPARQL": "sparql", "TPF": "tpf", "JSON": "json"}
+
+
+def guessQueryType(queryUrl):
+    queryUrl = queryUrl.lower()
+    if queryUrl.endswith(".rq"):
+        return qType["SPARQL"]
+    elif queryUrl.endswith(".sparql"):
+        return qType["SPARQL"]
+    elif queryUrl.endswith(".tpf"):
+        return qType["TPF"]
+    elif queryUrl.endswith(".json"):
+        return qType["JSON"]
+    else:
+        raise Exception("Unknown query type: " + queryUrl)
