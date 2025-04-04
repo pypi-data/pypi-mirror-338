6b8f0bf1a74efe42c6e9832cd136dc3c283a827b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 07:06:25 +0000

    Add package hfppl/0.1.1/hfppl-0.1.1-py3-none-any.whl

---
+++ packages/hfppl/hfppl-0.1.1-py3-none-any.whl/hfppl/__init__.py
@@ -0,0 +1,8 @@
+"""Probabilistic programming with HuggingFace Transformer models."""
+
+from .chunks import *
+from .distributions import *
+from .inference import *
+from .llms import *
+from .modeling import *
+from .util import *
