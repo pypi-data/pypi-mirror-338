bae2d12e96116555d374777bafb234d8e0a57337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:25 +0000

    Add package montepy/1.0.0/montepy-1.0.0.tar.gz

---
+++ packages/montepy/montepy-1.0.0.tar.gz/montepy-1.0.0/montepy/surfaces/general_plane.py
@@ -0,0 +1,44 @@
+# Copyright 2024, Battelle Energy Alliance, LLC All Rights Reserved.
+from typing import Union
+
+import montepy
+from montepy.errors import *
+from montepy.surfaces.surface_type import SurfaceType
+from montepy.surfaces.surface import Surface, InitInput
+
+
+class GeneralPlane(Surface):
+    """Represents P
+
+    .. versionchanged:: 1.0.0
+
+        Added number parameter
+
+    Parameters
+    ----------
+    input : Union[Input, str]
+        The Input object representing the input
+    number : int
+        The number to set for this object.
+    """
+
+    def __init__(
+        self,
+        input: InitInput = None,
+        number: int = None,
+    ):
+        super().__init__(input, number)
+        if input:
+            if self.surface_type != SurfaceType.P:
+                raise ValueError("A GeneralPlane must be a surface of type P")
+            if len(self.surface_constants) not in {4, 9}:
+                raise ValueError(
+                    "A GeneralPlane must have either 4 or 9 surface constants"
+                )
+
+    def validate(self):
+        super().validate()
+        if len(self.surface_constants) not in {4, 9}:
+            raise IllegalState(
+                f"Surface: {self.number} does not have constants set properly."
+            )
