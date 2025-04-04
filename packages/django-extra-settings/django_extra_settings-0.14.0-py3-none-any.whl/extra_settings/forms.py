1cc0a6b156c12b3ef66cb14a3d1c859333de09a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:51 +0000

    Add package django-extra-settings/0.14.0/django_extra_settings-0.14.0-py3-none-any.whl

---
+++ packages/django-extra-settings/django_extra_settings-0.14.0-py3-none-any.whl/extra_settings/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.conf import settings
+
+from extra_settings.models import Setting
+from extra_settings.utils import enforce_uppercase_setting
+
+
+class SettingForm(forms.ModelForm):
+    class Meta:
+        model = Setting
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # resize text area
+        if "description" in self.fields:
+            self.fields["description"].widget = forms.Textarea(
+                attrs={"rows": 3, "cols": 51}
+            )
+        if "value_text" in self.fields:
+            self.fields["value_text"].widget = forms.Textarea(
+                attrs={"rows": 5, "cols": 51}
+            )
+
+    def clean_name(self):
+        value = self.cleaned_data.get("name", "")
+        if settings.EXTRA_SETTINGS_ENFORCE_UPPERCASE_SETTINGS:
+            value = enforce_uppercase_setting(value)
+        if hasattr(settings, value):
+            raise forms.ValidationError(
+                f"Invalid setting name, settings.{value} already "
+                "defined in django.conf.settings."
+            )
+        return value
