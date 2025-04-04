33347744a5e8e2cedce9618988d197bbd330bcce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:19 +0000

    Add package menger-curvature/1.0.0/menger_curvature-1.0.0-py3-none-any.whl

---
+++ packages/menger-curvature/menger_curvature-1.0.0-py3-none-any.whl/menger/tests/test_menger.py
@@ -0,0 +1,19 @@
+"""
+Unit and regression test for the menger package.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+import pytest
+import menger
+import numpy as np
+
+
+def test_menger_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "menger" in sys.modules
+
+
+
+
+
