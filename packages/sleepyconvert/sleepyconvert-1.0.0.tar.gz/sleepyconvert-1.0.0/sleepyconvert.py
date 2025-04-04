06e6bc9458d140b5d0a39616241c214ce6a5a517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:06:03 +0000

    Add package sleepyconvert/1.0.0/sleepyconvert-1.0.0.tar.gz

---
+++ packages/sleepyconvert/sleepyconvert-1.0.0.tar.gz/sleepyconvert-1.0.0/sleepyconvert.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''README
+
+Usage:
+  ./sleepyconvert.py data <input_path> <output_path> [options]
+'''
+
+from sleepyconvert_toolchain.commands import app
+
+if __name__ == "__main__":
+  app()
