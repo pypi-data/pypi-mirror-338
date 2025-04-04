0bd2756d2e02b2849662705c9d38fc09028cbb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:29 +0000

    Add package zmp-openapi-mcp-server/0.1.9/zmp_openapi_mcp_server-0.1.9-py3-none-any.whl

---
+++ packages/zmp-openapi-mcp-server/zmp_openapi_mcp_server-0.1.9-py3-none-any.whl/zmp_openapi_mcp_server/__init__.py
@@ -0,0 +1,17 @@
+import logging
+# for stdio
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
+# for sse
+# logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+
+logging.getLogger("zmp_openapi_toolkit.openapi.zmpapi_models").setLevel(logging.WARNING)
+logging.getLogger("zmp_openapi_toolkit.toolkits.toolkit").setLevel(logging.INFO)
+logging.getLogger("httpcore.http11").setLevel(logging.INFO)
