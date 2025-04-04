768e7ce84c2d6af9e8b5ddf2751c6ced480a5148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:54 +0000

    Add package Drive-share-library/0.1/drive_share_library-0.1.tar.gz

---
+++ packages/Drive-share-library/drive_share_library-0.1.tar.gz/drive_share_library-0.1/Drive_share_library/__init__.py
@@ -0,0 +1,2 @@
+from .utilities import calculate_fare
+from .utilities import calculate_route_fare
---
+++ packages/Drive-share-library/drive_share_library-0.1.tar.gz/drive_share_library-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Drive_share_library',
+      version='0.1',
+      description='route and cost calculation library',
+      packages=['Drive_share_library'],
+      author_email='palnatisaikumarreddy150@gmail.com',
+      zip_safe=False)
