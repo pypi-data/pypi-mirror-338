6d0e90c4288e1ec76e707a4b7bc927bd7ca85df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:43 +0000

    Add package ai2soft.automation/0.0.8/ai2soft.automation-0.0.8.tar.gz

---
+++ packages/ai2soft.automation/ai2soft.automation-0.0.8.tar.gz/ai2soft.automation-0.0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai2soft.automation',
+    version='0.0.8',
+    packages=find_packages(),
+    description='用于完与一些自动化的操作',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    author='ai2soft.cn',
+    author_email='goldli@live.cn',
+    license='MIT',
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ]
+)
