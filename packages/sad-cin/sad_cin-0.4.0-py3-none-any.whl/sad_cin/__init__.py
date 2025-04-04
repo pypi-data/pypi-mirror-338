3fb05083e2179fa2302f6739493697363b6a285c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:05:24 +0000

    Add package sad-cin/0.4.0/sad_cin-0.4.0-py3-none-any.whl

---
+++ packages/sad-cin/sad_cin-0.4.0-py3-none-any.whl/sad_cin/__init__.py
@@ -0,0 +1,17 @@
+# sad_cin/__init__.py
+
+"""
+Pacote sad_cin: Métodos de apoio à decisão  multi critério
+
+Uso:
+  from sad_cin import decision_support
+
+Versão:
+  0.4.0
+"""
+
+__version__ = "0.4.0"
+
+from .main import decision_support
+
+__all__ = [ "decision_support" ]
