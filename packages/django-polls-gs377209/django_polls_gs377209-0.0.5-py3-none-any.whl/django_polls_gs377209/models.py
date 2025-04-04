34218e930ed2629b34d11bb44e65294cfda29fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:30 +0000

    Add package django-polls-gs377209/0.0.5/django_polls_gs377209-0.0.5-py3-none-any.whl

---
+++ packages/django-polls-gs377209/django_polls_gs377209-0.0.5-py3-none-any.whl/django_polls_gs377209/models.py
@@ -0,0 +1,59 @@
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+
+class Question(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
---
+++ packages/django-polls-gs377209/django_polls_gs377209-0.0.5-py3-none-any.whl/django_polls_gs377209/views.py
@@ -0,0 +1,108 @@
+"""
+polls view
+"""
+
+
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    """index view
+
+    Args:
+        generic (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by("-pub_date")[
+            :5
+        ]
+
+
+class DetailView(generic.DetailView):
+    """_summary_
+
+    Args:
+        generic (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    """_summary_
+
+    Args:
+        generic (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    model = Question
+    template_name = "polls/results.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+def vote(request, question_id):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+        question_id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+
+    selected_choice.votes = F("votes") + 1
+    selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse("polls:results",
+                                        args=(question.id,)))
