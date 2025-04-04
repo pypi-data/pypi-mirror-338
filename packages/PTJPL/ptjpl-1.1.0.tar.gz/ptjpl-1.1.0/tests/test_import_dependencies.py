19b2e8c9b2a42099cc20c5a84495301e2940db94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:04:49 +0000

    Add package PTJPL/1.1.0/ptjpl-1.1.0.tar.gz

---
+++ packages/PTJPL/ptjpl-1.1.0.tar.gz/ptjpl-1.1.0/tests/test_import_dependencies.py
@@ -0,0 +1,18 @@
+import pytest
+
+# List of dependencies
+dependencies = [
+    "ECOv002_CMR",
+    "ECOv002_granules",
+    "GEOS5FP",
+    "numpy",
+    "pandas",
+    "rasters",
+    "sklearn",
+    "verma_net_radiation"
+]
+
+# Generate individual test functions for each dependency
+@pytest.mark.parametrize("dependency", dependencies)
+def test_dependency_import(dependency):
+    __import__(dependency)
