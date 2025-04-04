8861d21be85d8c45f04ebd57f6339490c66fbb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:05:58 +0000

    Add package meu-investimento-re-2021/0.1/meu_investimento_re_2021-0.1.tar.gz

---
+++ packages/meu-investimento-re-2021/meu_investimento_re_2021-0.1.tar.gz/meu_investimento_re_2021-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_re_2021',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='renan melo',
+   author_email='renanfafasf@gmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/tadrianonet/meu_investimento_re_2021',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
