6447238088cf5780ac9ba326483d0333563a23ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:25 +0000

    Add package lightem/0.1.6/lightem-0.1.6-py3-none-any.whl

---
+++ packages/lightem/lightem-0.1.6-py3-none-any.whl/lightem/__init__.py
@@ -0,0 +1,6 @@
+from .EntityMatcher import EntityMatcher
+from .Embedder import Embedder, EmbedderTypes
+from .Matcher import Matcher, MatcherTypes
+from .Clusterer import Clusterer
+from .Table import Table, TableManager
+from .PackageInfo import __version__, __author__, __url__
