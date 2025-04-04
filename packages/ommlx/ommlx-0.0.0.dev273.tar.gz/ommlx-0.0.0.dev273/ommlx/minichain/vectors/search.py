0d8f8a56b7b2c0b9a91e4e54a6d13ac3c6c6af8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:58 +0000

    Add package ommlx/0.0.0.dev273/ommlx-0.0.0.dev273.tar.gz

---
+++ packages/ommlx/ommlx-0.0.0.dev273.tar.gz/ommlx-0.0.0.dev273/ommlx/minichain/vectors/search.py
@@ -0,0 +1,88 @@
+import typing as ta
+
+from omlish import dataclasses as dc
+from omlish import lang
+
+from ..options import Option
+from ..services import Service
+from ..services import ServiceOption
+from ..services import ServiceRequest
+from ..services import ServiceResponse
+from .similarity import Similarity
+from .vectors import Vector
+
+
+##
+
+
+class VectorSearchOption(Option, lang.Abstract):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class VectorSearchSimilarity(Option, lang.Final):
+    similarity: Similarity
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class VectorSearch(lang.Final):
+    vec: Vector
+
+    _: dc.KW_ONLY
+
+    k: int = 10
+
+
+@dc.dataclass(frozen=True)
+class VectorHit(lang.Final):
+    v: ta.Any
+    score: float
+
+
+@dc.dataclass(frozen=True)
+class VectorHits(lang.Final):
+    l: ta.Sequence[VectorHit]
+
+
+##
+
+
+VectorSearchInput: ta.TypeAlias = VectorSearch
+VectorSearchNew: ta.TypeAlias = str
+VectorSearchOutput: ta.TypeAlias = str
+
+VectorSearchOptions: ta.TypeAlias = ServiceOption | VectorSearchOption
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class VectorSearchRequest(
+    ServiceRequest[
+        VectorSearchInput,
+        VectorSearchOptions,
+        VectorSearchNew,
+    ],
+    lang.Final,
+):
+    @dc.validate
+    def _validate_v(self) -> bool:
+        return isinstance(self.v, str)
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class VectorSearchResponse(ServiceResponse[VectorSearchOutput], lang.Final):
+    pass
+
+
+class VectorSearchService(  # noqa
+    Service[
+        VectorSearchRequest,
+        VectorSearchOptions,
+        VectorSearchNew,
+        VectorSearchResponse,
+    ],
+    lang.Abstract,
+):
+    pass
