e5a38a4ccb4081f9342b8d6cbf4749af55624b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 03:17:09 +0000

    Add package nace/0.0.26/nace-0.0.26.tar.gz

---
+++ packages/nace/nace-0.0.26.tar.gz/nace-0.0.26/nace/color_codes.py
@@ -0,0 +1,10 @@
+"""
+Color constants used in terminal output
+"""
+
+color_code_black_on_white = f"\033[0m"
+color_code_white_on_red = f"\033[97;41m"
+color_code_white_on_black = f"\033[97;40m"
+color_code_white_on_blue = f"\033[97;44m"
+
+color_code_black_on_white_v2 = "\033[1;1H\033[2J"
