92a8338767aad1e5f0b37d8f5aadadaf09101361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 16:06:26 +0000

    Add package suyashmodule/0.4/suyashmodule-0.4.tar.gz

---
+++ packages/suyashmodule/suyashmodule-0.4.tar.gz/suyashmodule-0.4/mymodule.py
@@ -0,0 +1,10 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+
+def find():
+    print("Searching....")
+    print("Found!")
+    print("Found it!")  
+    print("Found it again!")
+    print("Your father is Suyash Guliani!")
