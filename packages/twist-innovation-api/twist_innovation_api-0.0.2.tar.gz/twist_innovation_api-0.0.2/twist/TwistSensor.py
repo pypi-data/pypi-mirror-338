b6e2ebb200f40e3b0ff86d5ccafddac90e9bf56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 03:18:27 +0000

    Add package twist-innovation-api/0.0.2/twist_innovation_api-0.0.2.tar.gz

---
+++ packages/twist-innovation-api/twist_innovation_api-0.0.2.tar.gz/twist_innovation_api-0.0.2/twist/TwistSensor.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2025 Twist Innovation
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU General Public License for more details:
+# https://www.gnu.org/licenses/gpl-3.0.html
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from TwistDevice import TwistDevice
+
+from .TwistDevice import TwistModel
+
+
+class TwistSensor(TwistModel):
+    def __init__(self, model_id: int, parent_device: TwistDevice):
+        super().__init__(model_id, parent_device)
+
+    async def context_msg(self, payload: str):
+        self.parse_general_context(payload)
+
+    def print_context(self):
+        print(f"Sensor Device: {self.parent_device.twist_id}, Model: {self.model_id}")
---
+++ packages/twist-innovation-api/twist_innovation_api-0.0.2.tar.gz/twist_innovation_api-0.0.2/twist/__init__.py
@@ -0,0 +1,9 @@
+"""
+Twist Innovation API Library
+A Python library to interact with Twist Innovation devices
+"""
+from .TwistModel import TwistModel
+from .TwistAPI import TwistAPI
+
+__all__ = ["TwistAPI", "TwistModel"]
+
