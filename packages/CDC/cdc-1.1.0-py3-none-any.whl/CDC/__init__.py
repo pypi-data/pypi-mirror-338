bcaf2b0d2b502d71221ba76b3eb4b043b3bf8296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 03:18:28 +0000

    Add package CDC/1.1.0/cdc-1.1.0-py3-none-any.whl

---
+++ packages/CDC/cdc-1.1.0-py3-none-any.whl/CDC/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+# Current version
+__version__ = "1.1.0"
+
+# import CDC objects
+from .color_models import *
+from .orthomosaic_tiler import *
+from .tiled_color_based_distance import *
+from .transforms import *
