743567fce496452e4cf3d179f42f790145bb7e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 08:05:18 +0000

    Add package potnia/0.4.1/potnia-0.4.1.tar.gz

---
+++ packages/potnia/potnia-0.4.1.tar.gz/potnia-0.4.1/potnia/__init__.py
@@ -0,0 +1,12 @@
+from .script import Script
+from .scripts.linear_a import linear_a, LinearA
+from .scripts.linear_b import linear_b, LinearB
+from .scripts.hittite import hittite, Hittite
+from .scripts.arabic import arabic, Arabic
+
+
+# Luwian is currently a work in progress
+# from .luwian import luwian 
+
+# Akkadian is currently a work in progress
+# from .akkadian import akkadian
