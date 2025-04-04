eec55cc838bd1322abc6853a4ea4e43da18676e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:38 +0000

    Add package django-oscar-reports/2.4.0/django_oscar_reports-2.4.0.tar.gz

---
+++ packages/django-oscar-reports/django_oscar_reports-2.4.0.tar.gz/django_oscar_reports-2.4.0/src/oscarreports/tasks.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from uuid import UUID
+
+from celery import shared_task
+
+
+@shared_task(ignore_result=True)
+def generate_report(report_uuid: str | UUID, report_format: str) -> None:
+    from . import models
+
+    report = models.Report.objects.get(uuid=report_uuid)
+    report.generate(report_format)
