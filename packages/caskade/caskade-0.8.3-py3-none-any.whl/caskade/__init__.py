a3720f7756d6089fa9e712da3efa07a19b21ad1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:06:24 +0000

    Add package caskade/0.8.3/caskade-0.8.3-py3-none-any.whl

---
+++ packages/caskade/caskade-0.8.3-py3-none-any.whl/caskade/__init__.py
@@ -0,0 +1,55 @@
+from ._version import version as VERSION  # noqa
+
+from .base import Node
+from .context import ActiveContext, ValidContext, OverrideParam
+from .decorators import forward
+from .module import Module
+from .param import Param, dynamic
+from .collection import NodeList, NodeTuple
+from .tests import test
+from .errors import (
+    CaskadeException,
+    GraphError,
+    LinkToAttributeError,
+    NodeConfigurationError,
+    ParamConfigurationError,
+    ParamTypeError,
+    ActiveStateError,
+    FillDynamicParamsError,
+    FillDynamicParamsTensorError,
+    FillDynamicParamsSequenceError,
+    FillDynamicParamsMappingError,
+)
+from .warnings import CaskadeWarning, InvalidValueWarning, SaveStateWarning
+
+
+__version__ = VERSION
+__author__ = "Connor Stone and Alexandre Adam"
+
+__all__ = (
+    "Node",
+    "Module",
+    "Param",
+    "dynamic",
+    "NodeList",
+    "NodeTuple",
+    "ActiveContext",
+    "ValidContext",
+    "OverrideParam",
+    "forward",
+    "test",
+    "CaskadeException",
+    "GraphError",
+    "LinkToAttributeError",
+    "NodeConfigurationError",
+    "ParamConfigurationError",
+    "ParamTypeError",
+    "ActiveStateError",
+    "FillDynamicParamsError",
+    "FillDynamicParamsTensorError",
+    "FillDynamicParamsSequenceError",
+    "FillDynamicParamsMappingError",
+    "CaskadeWarning",
+    "InvalidValueWarning",
+    "SaveStateWarning",
+)
