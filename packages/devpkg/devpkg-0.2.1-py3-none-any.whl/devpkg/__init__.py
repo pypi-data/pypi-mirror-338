23c12264463389ef925b999381e8812ea103e22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:36 +0000

    Add package devpkg/0.2.1/devpkg-0.2.1-py3-none-any.whl

---
+++ packages/devpkg/devpkg-0.2.1-py3-none-any.whl/devpkg/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .is_literal import is_literal
+from .singleton import Singleton
+from .traceback import log_traceback
+from .versions import PackageSource, VersionChecker, VersionInfo
