ff6ae33af57d0df1a977b289ad73a8b46448860f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:30 +0000

    Add package pylexfluent/0.1.62/pylexfluent-0.1.62-py3-none-any.whl

---
+++ packages/pylexfluent/pylexfluent-0.1.62-py3-none-any.whl/lxf/domain/extracted_data.py
@@ -0,0 +1,34 @@
+import datetime
+from pydantic import BaseModel, Field
+
+HIERARCHIE_DOCUMENT="document"
+HIERARCHIE_DOSSIER="dossier"
+
+class ExtractedMetadata (BaseModel):
+    origine:str=Field(default="LEXIA")
+    classe:str=Field(default="DIVERS") 
+    classe_confidence:float=Field(default=0.0)
+    documentId:str=Field(default="")
+    traite_le:str=Field(default=str(datetime.datetime.today()))
+
+class ChunkMetadata(BaseModel):
+    chunk:int=Field(default=1)
+    chunks:int=Field(default=1)
+    title:str=Field(default="")
+    description:str=Field(default="")
+    source:str=Field(default="")
+    hierarchie:str=Field(default=HIERARCHIE_DOCUMENT)
+
+
+class Chunk(BaseModel):
+    metadata:ChunkMetadata=Field(default=None)
+    page_content:str=Field(default="")    
+    summary:str=Field(default="")
+    keywords:list[str]=[]
+    explain:str=Field(default="")
+class ExtractedData(BaseModel) :
+    metadata:ExtractedMetadata=Field(default=None)
+    chunks:list[Chunk]=[]
+    keywords:list[str]=[]
+    summary:str=Field(default="")
+    explain:str=Field(default="")
