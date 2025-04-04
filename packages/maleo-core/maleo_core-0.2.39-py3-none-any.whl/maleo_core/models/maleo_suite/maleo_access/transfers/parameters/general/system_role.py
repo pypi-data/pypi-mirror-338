7f36b571066417f154af2212f2d7d2e1e01d9933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:07:00 +0000

    Add package maleo-core/0.2.39/maleo_core-0.2.39-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.2.39-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/general/system_role.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from enum import StrEnum
+from pydantic import Field
+from typing import Union
+from uuid import UUID
+from maleo_core.models.base.transfers.parameters.general import BaseGeneralParameters
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.system_role import MaleoAccessSystemRoleGeneralTransfers
+
+class MaleoAccessSystemRoleGeneralParameters:
+    class UniqueIdentifiers(StrEnum):
+        ID = "id"
+        UUID = "uuid"
+        KEY = "key"
+        NAME = "name"
+
+    class GetSingle(BaseGeneralParameters.GetSingle):
+        identifier:MaleoAccessSystemRoleGeneralParameters.UniqueIdentifiers = Field(..., description="Identifier")
+        value:Union[int, UUID, str] = Field(..., description="Value")
+
+    CreateOrUpdate = MaleoAccessSystemRoleGeneralTransfers.Base
+
+    class CheckUniqueExistence(BaseGeneralParameters.CheckUniqueExistence):
+        field:MaleoAccessSystemRoleGeneralParameters.UniqueIdentifiers = Field(..., description="Field to be checked")
+
+    UniqueFieldChecks = list[CheckUniqueExistence]
