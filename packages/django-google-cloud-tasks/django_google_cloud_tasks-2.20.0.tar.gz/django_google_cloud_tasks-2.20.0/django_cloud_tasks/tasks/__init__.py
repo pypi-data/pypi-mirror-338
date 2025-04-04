f02647a9201e6f2966cad46f24233e6b9fac2171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:05:59 +0000

    Add package django-google-cloud-tasks/2.20.0/django_google_cloud_tasks-2.20.0.tar.gz

---
+++ packages/django-google-cloud-tasks/django_google_cloud_tasks-2.20.0.tar.gz/django_google_cloud_tasks-2.20.0/django_cloud_tasks/tasks/__init__.py
@@ -0,0 +1,24 @@
+from django_cloud_tasks.tasks.periodic_task import PeriodicTask
+from django_cloud_tasks.tasks.publisher_task import PublisherTask, ModelPublisherTask
+from django_cloud_tasks.tasks.routine_task import (
+    PipelineDispatcherTask,
+    RoutineReverterTask,
+    RoutineExecutorTask,
+    RoutineTask,
+)
+from django_cloud_tasks.tasks.subscriber_task import SubscriberTask
+from django_cloud_tasks.tasks.task import Task, TaskMetadata, is_task_route
+
+__all__ = (
+    "Task",
+    "TaskMetadata",
+    "PeriodicTask",
+    "ModelPublisherTask",
+    "PublisherTask",
+    "SubscriberTask",
+    "RoutineTask",
+    "PipelineDispatcherTask",
+    "RoutineExecutorTask",
+    "RoutineReverterTask",
+    "is_task_route",
+)
