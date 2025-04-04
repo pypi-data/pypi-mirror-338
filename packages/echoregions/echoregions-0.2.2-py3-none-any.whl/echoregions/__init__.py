cea1ad143a48d6c9f44433d98b6d9a91f2bc5107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:04:50 +0000

    Add package echoregions/0.2.2/echoregions-0.2.2-py3-none-any.whl

---
+++ packages/echoregions/echoregions-0.2.2-py3-none-any.whl/echoregions/__init__.py
@@ -0,0 +1,21 @@
+"""
+These core functions are what users will call on
+at the beginning of their programming and will produce objects
+that contain the majority of the functionality within echoregions.
+"""
+
+from _echoregions_version import version as __version__  # noqa
+
+from .core import read_evl, read_evr, read_lines_csv, read_regions_csv
+from .regions2d.evr_writer import write_evr
+from .utils.api import convert_mask_2d_to_3d, convert_mask_3d_to_2d
+
+__all__ = [
+    "read_evl",
+    "read_lines_csv",
+    "read_evr",
+    "read_regions_csv",
+    "convert_mask_2d_to_3d",
+    "convert_mask_3d_to_2d",
+    "write_evr",
+]  # noqa
