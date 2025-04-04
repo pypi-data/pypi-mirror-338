7503fe972d437931a25f1d51589e103bda6d2599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:59 +0000

    Add package django-colorfield/0.13.0/django_colorfield-0.13.0.tar.gz

---
+++ packages/django-colorfield/django_colorfield-0.13.0.tar.gz/django_colorfield-0.13.0/tests/test_widgets.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# from colorfield.widgets import ColorWidget
+
+
+class ColorWidgetTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
