2d0d5b0b0b61c1220d0a80764a66df112e376c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 07:05:57 +0000

    Add package ftopsis-class-cin/0.1.1/ftopsis_class_cin-0.1.1-py3-none-any.whl

---
+++ packages/ftopsis-class-cin/ftopsis_class_cin-0.1.1-py3-none-any.whl/ftopsis_class_cin/__init__.py
@@ -0,0 +1,23 @@
+# ftopsis_class_cin/__init__.py
+
+"""
+Pacote FTOPSIS-CLASS: Implementação do método de decisão multi critério.
+
+Uso:
+    from ftopsis_class_cin import ftopsis_class_cin_decision_support, trapezoidal_ftopsis_class, triangular_ftopsis_class, FtopsisProcessor
+
+Versão:
+    0.1.1
+"""
+
+__version__ = "0.1.1"
+
+from .decision import FTOPSISProcessor as FtopsisProcessor, trapezoidal_ftopsis_class, triangular_ftopsis_class
+from .main import ftopsis_class_decision_support
+
+__all__ = [
+    "FtopsisProcessor",
+    "trapezoidal_ftopsis_class",
+    "triangular_ftopsis_class",
+    "ftopsis_class_decision_support"
+]
