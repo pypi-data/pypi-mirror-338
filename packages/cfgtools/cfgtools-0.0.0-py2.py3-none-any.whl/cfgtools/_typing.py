8928a68d8b0f2c06cb48039c9141967bb83c3198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:04:50 +0000

    Add package cfgtools/0.0.0/cfgtools-0.0.0-py2.py3-none-any.whl

---
+++ packages/cfgtools/cfgtools-0.0.0-py2.py3-none-any.whl/cfgtools/_typing.py
@@ -0,0 +1,14 @@
+"""
+Contains typing classes.
+
+NOTE: this module is private. All functions and objects are available in the main
+`$package` namespace - use that instead.
+
+"""
+
+import logging
+
+logging.warning(
+    "importing from '._typing' - this module is not intended for direct import, "
+    "therefore unexpected errors may occur"
+)
