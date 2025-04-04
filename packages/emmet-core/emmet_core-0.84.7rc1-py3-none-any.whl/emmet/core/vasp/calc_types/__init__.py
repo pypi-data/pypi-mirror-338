2c255b7e2105635a4c1bbcf228ebb8912311d793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:06:05 +0000

    Add package emmet-core/0.84.7rc1/emmet_core-0.84.7rc1-py3-none-any.whl

---
+++ packages/emmet-core/emmet_core-0.84.7rc1-py3-none-any.whl/emmet/core/vasp/calc_types/__init__.py
@@ -0,0 +1,4 @@
+"""Module defining VASP calculation types."""
+
+from emmet.core.vasp.calc_types.enums import CalcType, RunType, TaskType
+from emmet.core.vasp.calc_types.utils import calc_type, run_type, task_type
