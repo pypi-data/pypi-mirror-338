94882b2128ca5e49605e0a64a0ec7149d9fe5ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:05:58 +0000

    Add package ineedproxy/0.1.1/ineedproxy-0.1.1.tar.gz

---
+++ packages/ineedproxy/ineedproxy-0.1.1.tar.gz/ineedproxy-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="ineedproxy",
+    version="0.1.1",
+    description="Easy but powerful proxy management Python package. Used to fetch, manage, rotate and use proxies.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Paul Hartwich",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    license="MIT",
+    url="https://github.com/paul-hartwich/ineedproxy",
+)
