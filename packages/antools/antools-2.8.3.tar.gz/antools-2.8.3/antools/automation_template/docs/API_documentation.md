04c7c2407cf36658801a885baf2c32e7fc0bf4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:05:06 +0000

    Add package antools/2.8.3/antools-2.8.3.tar.gz

---
+++ packages/antools/antools-2.8.3.tar.gz/antools-2.8.3/antools/automation_template/docs/API_documentation.md
@@ -0,0 +1,3 @@
+# Documentation
+
+This is an example.
---
+++ packages/antools/antools-2.8.3.tar.gz/antools-2.8.3/antools/code_to_sort/multiprocessing/__init__.py
@@ -0,0 +1,4 @@
+from ._mp_process_class import MultiProcess
+from ._mp_handler import MultiProcessHandler
+from antools.logging._logger_class import _get_mp_logger
+
