5f17e64cbbb83ea54966c4db8081c6d06dd2a70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:39 +0000

    Add package df-compress/0.1.0/df_compress-0.1.0.tar.gz

---
+++ packages/df-compress/df_compress-0.1.0.tar.gz/df_compress-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='df-compress',
+    version='0.1.0',    
+    description="A python package to compress pandas DataFrames akin to Stata's `compress` command",
+    url='https://github.com/phchavesmaia/df-compress',
+    author='Pedro H. Chaves Maia',
+    author_email='pedro.maia@imdsbrasil.org',
+    license='MIT',
+    packages=['df_compress'],
+    install_requires=['pandas',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.13',
+    ],
+)
