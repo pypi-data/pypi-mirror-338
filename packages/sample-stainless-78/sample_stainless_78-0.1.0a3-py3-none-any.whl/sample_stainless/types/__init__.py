f741dc758d9529ade982c289754acb35d78a3069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:15 +0000

    Add package sample-stainless-78/0.1.0a3/sample_stainless_78-0.1.0a3-py3-none-any.whl

---
+++ packages/sample-stainless-78/sample_stainless_78-0.1.0a3-py3-none-any.whl/sample_stainless/types/__init__.py
@@ -0,0 +1,11 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .pet import Pet as Pet
+from .new_pet import NewPet as NewPet
+from .pet_add_params import PetAddParams as PetAddParams
+from .pet_list_params import PetListParams as PetListParams
+from .pet_list_response import PetListResponse as PetListResponse
+from .pet_add_all_params import PetAddAllParams as PetAddAllParams
+from .pet_get_pets_params import PetGetPetsParams as PetGetPetsParams
