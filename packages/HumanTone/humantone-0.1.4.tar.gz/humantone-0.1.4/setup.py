b77c696157be81ffb112308f88c40acd519f0c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:41 +0000

    Add package HumanTone/0.1.4/humantone-0.1.4.tar.gz

---
+++ packages/HumanTone/humantone-0.1.4.tar.gz/humantone-0.1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="HumanTone",
+    version="0.1.4",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+    "transformers",
+    "textblob",
+    "nltk",
+    "sentence-transformers",
+    "pyttsx3",
+    "torch",
+    "scipy",
+    "pandas"
+    ],
+    description="A unique sentiment analysis library with paraphrasing, contextual similarity, and speech output.",
+    author="Vansh Gautam",
+    author_email="vanshgautam2005@gmail.com",
+    github="Private",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
