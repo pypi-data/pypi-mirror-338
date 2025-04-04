83e1b4dc176bcba8aa98dc1b6c695ca5584e4e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:30 +0000

    Add package evilblade/1.1.7.91/evilblade-1.1.7.91.tar.gz

---
+++ packages/evilblade/evilblade-1.1.7.91.tar.gz/evilblade-1.1.7.91/README.md
@@ -0,0 +1,10 @@
+
+
+先使用该命令安装。
+
+``pip3 install  .``
+
+
+不行的话用使用该命令安装。
+sudo
+``pip3 install --user . --break-system-packages ``
---
+++ packages/evilblade/evilblade-1.1.7.91.tar.gz/evilblade-1.1.7.91/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='evilblade',
+    version='1.1.7.91',
+    description='7 N1nEmAn\'s evilblade!!',
+    packages=['evilblade'],
+    install_requires=[
+        'pwntools',
+        'LibcSearcher'
+    ],
+
+    long_description = """
+    —–Offense without defense,
+
+        unparalleled in the world.—–
+    Welcome to the universe of N1nEmAn.
+
+        To find detailed usage instructions for evilblade,
+
+            please visit cnblogs.com/9man.
+    """,
+    long_description_content_type="text/markdown",
+)
