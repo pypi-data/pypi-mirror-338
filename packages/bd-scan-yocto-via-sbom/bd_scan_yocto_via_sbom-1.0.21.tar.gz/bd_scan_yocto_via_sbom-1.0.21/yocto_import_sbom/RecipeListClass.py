88b55a65eefec1206f0e2e28f9b035b54351863d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:10 +0000

    Add package bd-scan-yocto-via-sbom/1.0.21/bd_scan_yocto_via_sbom-1.0.21.tar.gz

---
+++ packages/bd-scan-yocto-via-sbom/bd_scan_yocto_via_sbom-1.0.21.tar.gz/bd_scan_yocto_via_sbom-1.0.21/yocto_import_sbom/RecipeListClass.py
@@ -0,0 +1,221 @@
+import os.path
+import re
+import logging
+import shutil
+import tempfile
+
+from .RecipeClass import Recipe
+from .BBClass import BB
+
+
+class RecipeList:
+    def __init__(self):
+        self.recipes = []
+
+    def count(self):
+        return len(self.recipes)
+
+    def count_recipes_without_layer(self):
+        count_nolayer = 0
+        for recipe in self.recipes:
+            if not recipe.layer:
+                count_nolayer += 1
+        return count_nolayer
+
+    def check_recipe_exists(self, recipe_name):
+        for recipe in self.recipes:
+            if recipe.name == recipe_name:
+                return True
+        return False
+
+    def add_layer_to_recipe(self, rec, layer, ver):
+        epoch, version = Recipe.get_epoch_and_version(ver)
+        for recipe in self.recipes:
+            if recipe.name == rec:
+                if recipe.version != '':
+                    if recipe.version in version:
+                        recipe.add_layer(layer)
+                        recipe.epoch = epoch
+                else:
+                    recipe.add_layer(layer)
+                    recipe.epoch = epoch
+                    recipe.version = version
+                return
+
+    def add_rel_to_recipe(self, rec, rel):
+        for recipe in self.recipes:
+            if recipe.name == rec:
+                recipe.release = rel
+                return
+
+    def print_recipes(self):
+        for recipe in self.recipes:
+            recipe.print_recipe()
+
+    def get_layers(self):
+        layers = []
+        for recipe in self.recipes:
+            if recipe.layer not in layers:
+                layers.append(recipe.layer)
+        return layers
+
+    def check_recipes_in_oe(self, conf, oe):
+        recipes_in_oe = 0
+        exact_recipes_in_oe = 0
+        changed_layers = 0
+        exact_layers = 0
+        for recipe in self.recipes:
+            recipe.oe_recipe, recipe.oe_layer, exact_ver, exact_layer = oe.get_recipe(conf, recipe)
+            if recipe.oe_recipe != {}:
+                recipes_in_oe += 1
+                if not exact_layer:
+                    changed_layers += 1
+                else:
+                    exact_layers += 1
+            if exact_ver:
+                exact_recipes_in_oe += 1
+
+        # logging.info(f"- {recipes_in_oe} out of {self.count()} total recipes found in OE data ({exact_recipes_in_oe} "
+        #              f"exact version matches and {changed_layers} recipe layers modified)")
+        logging.info("")
+        logging.info(f"SUMMARY OE MATCH DATA:")
+        logging.info(f"- {self.count()} Total Recipes")
+        logging.info(f"- {recipes_in_oe} Recipes found in OE Data:")
+        logging.info(f"    - {exact_recipes_in_oe} with exact version match")
+        logging.info(f"    - {recipes_in_oe - exact_recipes_in_oe} with close version match (mapped to closest version)")
+        logging.info(f"    - {exact_layers} with the same layer as OE")
+        logging.info(f"    - {changed_layers} exist in different OE layer (mapped to original)")
+
+    def scan_pkg_download_files(self, conf, bom):
+        all_pkg_files = BB.get_pkg_files(conf)
+        all_download_files = BB.get_download_files(conf)
+        found_files = self.find_files(conf, all_pkg_files, all_download_files)
+        if len(found_files) > 0:
+            tdir = self.copy_files(found_files)
+            if tdir and bom.run_detect_sigscan(conf, tdir):
+                return len(found_files), True
+            else:
+                return len(found_files), False
+        return 0, False
+
+    def find_files(self, conf, all_pkg_files, all_download_files):
+        found_files = []
+        for recipe in self.recipes:
+            if not conf.scan_all_packages and recipe.matched_in_bom:
+                continue
+            found = False
+            recipe_esc = re.escape(recipe.name)
+            ver_esc = re.escape(recipe.version)
+            download_regex = re.compile(rf"^{recipe_esc}[_-]v?{ver_esc}[.-].*$")
+            pkg_regex = re.compile(rf"^(lib)?{recipe_esc}\d*[_-]v?{ver_esc}[+.-].*\.{conf.image_package_type}")
+
+            for path in all_download_files:
+                filename = os.path.basename(path)
+                download_res = download_regex.match(filename)
+                if download_res is not None:
+                    found_files.append(path)
+                    found = True
+                    logging.info(f"- Recipe:{recipe.name}/{recipe.version} - Located download file: {path}")
+
+            if found:
+                continue
+
+            for path in all_pkg_files:
+                filename = os.path.basename(path)
+                # pattern = f"{os.path.join(global_values.pkg_dir, global_values.machine)}/" \
+                #           f"{recipe}[-_]{ver}-*.{global_values.image_pkgtype}"
+                pkg_res = pkg_regex.match(filename)
+
+                if pkg_res is not None:
+                    found_files.append(path)
+                    logging.info(f"- Recipe:{recipe.name}/{recipe.version} - Located package file: {path}")
+                    found = True
+
+            if not found:
+                logging.info(f"- Recipe:{recipe.name}/{recipe.version} - No package file found")
+
+        return found_files
+
+    @staticmethod
+    def copy_files(files):
+        temppkgdir = tempfile.mkdtemp(prefix="bd_sig_pkgs")
+
+        # print(temppkgdir)
+        count = 0
+        for file in files:
+            try:
+                shutil.copy(file, temppkgdir)
+                count += 1
+            except Exception as e:
+                logging.warning(f"Unable to copy package file {file} to temporary scan folder - {e}")
+
+        logging.info(f"Copying recipe package files")
+        logging.info(f"- Copied {count} package files ...")
+        if count > 0:
+            return temppkgdir
+        else:
+            return ''
+
+    def check_recipes_in_bom(self, conf, bom):
+
+        in_bom = []
+        not_in_bom = []
+        matched_oe_not_in_bom = []
+        not_matched_oe_not_in_bom = []
+        not_matched_oe_in_bom = []
+        not_in_bom_recipename_in_oe = []
+        logging.info(f"Missing Recipes:")
+        for recipe in self.recipes:
+            fullid = recipe.full_id()
+
+            if recipe.matched_oe_exact:
+                fullid += " (EXACT VERSION)"
+            elif recipe.matched_oe:
+                fullid += f" (Closest version {recipe.oe_recipe['pv']}-{recipe.oe_recipe['pr']})"
+
+            if not recipe.check_in_bom(bom):
+                not_in_bom.append(fullid)
+                if recipe.matched_oe:
+                    matched_oe_not_in_bom.append(fullid)
+                    logging.info(f"- Recipe {fullid}: Matched in OE data but NOT found in BOM")
+                else:
+                    not_matched_oe_not_in_bom.append(fullid)
+                    logging.debug(f"- Recipe {fullid}: NOT matched in OE data and NOT found in BOM")
+                if recipe.recipename_in_oe:
+                    not_in_bom_recipename_in_oe.append(fullid)
+            else:
+                in_bom.append(fullid)
+                recipe.matched_in_bom = True
+                if not recipe.matched_oe:
+                    not_matched_oe_in_bom.append(fullid)
+                    # logging.debug(f"- Recipe {fullid}: Not matched in OE data but found in BOM")
+            #     logging.info(f"- Recipe {recipe.name}/{recipe.version}: Found in BOM")
+
+        logging.info("")
+        logging.info(" Summary of Components Mapped in Black Duck BOM:")
+        logging.info(f"- Total recipes in Yocto project - {self.count()}")
+        logging.info(f"- Total recipes matched in BOM - {bom.count_comps()}")
+        logging.info(f"    - Of which {len(not_matched_oe_in_bom)} not matched in OE data")
+        logging.info(f"- Recipes NOT in BOM - {len(not_in_bom)}")
+        logging.info(f"    - Of which {len(matched_oe_not_in_bom)} matched in OE data")
+        logging.info(f"    - Of which {len(not_matched_oe_not_in_bom)} not matched in OE data")
+        logging.info(f"    - Of which {len(not_in_bom_recipename_in_oe)} not matched but recipe exists in OE data")
+
+        if conf.recipe_report != '':
+            try:
+                with open(conf.recipe_report, "w") as repfile:
+                    repfile.write(f"RECIPES IN BOM ({len(in_bom)}):\n")
+                    repfile.write("\n".join(in_bom))
+                    repfile.write(f"\n\nRECIPES IN BOM - NOT MATCHED IN OE DATA ({len(not_matched_oe_in_bom)}):\n")
+                    repfile.write("\n".join(not_matched_oe_in_bom))
+                    repfile.write(f"\n\nRECIPES NOT IN BOM ({len(not_in_bom)}):\n")
+                    repfile.write("\n".join(not_in_bom))
+                    repfile.write(f"\n\nRECIPES NOT IN BOM - NOT MATCHED IN OE DATA ({len(not_matched_oe_not_in_bom)}):\n")
+                    repfile.write("\n".join(not_matched_oe_not_in_bom))
+                    repfile.write(f"\n\nRECIPES NOT IN BOM - MATCHED IN OE DATA ({len(matched_oe_not_in_bom)}):\n")
+                    repfile.write("\n".join(matched_oe_not_in_bom))
+                    repfile.write(f"\n\nRECIPES NOT IN BOM - RECIPE EXISTS IN OE DATA BUT NO VERSION MATCH ({len(not_in_bom_recipename_in_oe)}):\n")
+                    repfile.write("\n".join(not_in_bom_recipename_in_oe))
+                logging.info(f"Output full recipe report to '{conf.recipe_report}'")
+            except IOError as error:
+                logging.error(f"Unable to write recipe report file {conf.recipe_report} - {error}")
---
+++ packages/bd-scan-yocto-via-sbom/bd_scan_yocto_via_sbom-1.0.21.tar.gz/bd_scan_yocto_via_sbom-1.0.21/yocto_import_sbom/main.py
@@ -0,0 +1,119 @@
+from .RecipeListClass import RecipeList
+from .SBOMClass import SBOM
+from .BOMClass import BOM
+from .OEClass import OE
+from .BBClass import BB
+from .ConfigClass import Config
+import logging
+import sys
+
+import tempfile
+# import os
+
+empty_dir = tempfile.TemporaryDirectory()
+
+
+def main():
+    conf = Config()
+
+    logging.info("")
+    logging.info("--- PHASE 1 - PROCESS PROJECT --------------------------------------------")
+    bom = BOM(conf)
+
+    if conf.output_file == '':
+        extra_opt = '--detect.tools=DETECTOR'
+        if conf.unmap:
+            extra_opt += ' --detect.project.codelocation.unmap=true'
+        if not bom.run_detect_sigscan(conf, empty_dir.name,
+                                      extra_opt=extra_opt):
+            logging.error("Unable to run Detect to initialise project")
+            sys.exit(2)
+
+    reclist = RecipeList()
+    bb = BB()
+    if not bb.process(conf, reclist):
+        sys.exit(2)
+
+    logging.info("")
+    logging.info("--- PHASE 2 - GET OE DATA ------------------------------------------------")
+    if not conf.skip_oe_data:
+        oe_class = OE(conf)
+        reclist.check_recipes_in_oe(conf, oe_class)
+        logging.info("Done processing OE data")
+    else:
+        logging.info("Skipping connection to OE APIs to verify origin layers and revisions "
+                     "(remove --skip_oe_data to enable)")
+
+    logging.info("")
+    logging.info("--- PHASE 3 - WRITE SBOM -------------------------------------------------")
+    sbom = SBOM(conf.bd_project, conf.bd_version)
+    sbom.process_recipes(reclist.recipes)
+    if not sbom.output(conf.output_file):
+        logging.error("Unable to create SBOM file")
+        sys.exit(2)
+
+    if conf.output_file:
+        # Create SBOM and terminate
+        logging.info(f"Specified SBOM output file {sbom.file} created - nothing more to do")
+        logging.info("")
+        logging.info("Done")
+        sys.exit(0)
+
+    logging.info("Done creating SBOM file")
+    logging.info("")
+    logging.info("--- PHASE 4 - UPLOAD SBOM ------------------------------------------------")
+    # bom = BOM(conf)
+
+    if bom.upload_sbom(conf, bom, sbom):
+        logging.info(f"Uploaded SBOM file '{sbom.file}' to create project "
+                     f"'{conf.bd_project}' version '{conf.bd_version}'")
+    else:
+        sys.exit(2)
+
+    logging.info("")
+    logging.info("--- PHASE 5 - CHECKING OE RECIPES IN BOM ---------------------------------")
+    bom.get_proj()
+    if not bom.wait_for_bom_completion():
+        logging.error("Error waiting for project scan completion")
+        sys.exit(2)
+    bom.get_data()
+    reclist.check_recipes_in_bom(conf, bom)
+
+    logging.info("")
+    logging.info("--- PHASE 6 - SIGNATURE SCAN PACKAGES ------------------------------------")
+    if not conf.skip_sig_scan:
+        if conf.package_dir and conf.download_dir:
+            num, ret = reclist.scan_pkg_download_files(conf, bom)
+            if num > 0:
+                if ret:
+                    logging.info("Done")
+                else:
+                    logging.error(f"Unable to run Signature scan on package and download files")
+                    sys.exit(2)
+            else:
+                logging.info("No files to scan - skipping")
+        else:
+            logging.info("Skipped (package_dir or download_dir not identified)")
+    else:
+        logging.info("Skipped (--skip_sig_scan specified)")
+
+    logging.info("")
+    logging.info("--- PHASE 7 - APPLY CVE PATCHES ------------------------------------------")
+    if conf.cve_check_file:
+        bom.get_proj()
+        if not bom.wait_for_bom_completion():
+            logging.error("Error waiting for project scan completion")
+            sys.exit(2)
+
+        bom.get_data()
+        bom.process_cve_file(conf.cve_check_file, reclist)
+        bom.process_patched_cves()
+    else:
+        logging.info("Skipping CVE processing as no cve_check output file supplied")
+
+    logging.info("")
+    logging.info("DONE")
+
+
+if __name__ == '__main__':
+    main()
