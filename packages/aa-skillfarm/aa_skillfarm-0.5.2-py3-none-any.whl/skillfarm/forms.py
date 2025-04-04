b372e989fd6d82360d26c5f2d9df7101db778cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 03:18:18 +0000

    Add package aa-skillfarm/0.5.2/aa_skillfarm-0.5.2-py3-none-any.whl

---
+++ packages/aa-skillfarm/aa_skillfarm-0.5.2-py3-none-any.whl/skillfarm/forms.py
@@ -0,0 +1,38 @@
+"""Forms for app."""
+
+from django import forms
+from eveuniverse.models import EveType
+
+
+class ConfirmForm(forms.Form):
+    """Form Confirms."""
+
+    character_id = forms.CharField(
+        widget=forms.HiddenInput(),
+    )
+
+
+class SkillSetForm(forms.Form):
+    """Form SkillSet."""
+
+    character_id = forms.CharField(
+        widget=forms.HiddenInput(),
+    )
+
+    selected_skills = forms.CharField(
+        required=False,
+        widget=forms.HiddenInput(),
+    )
+
+    skills = forms.ModelMultipleChoiceField(
+        queryset=EveType.objects.filter(eve_group__eve_category__id=16)
+        .select_related("eve_group", "eve_group__eve_category")
+        .order_by("name"),
+        required=False,
+        widget=forms.SelectMultiple(
+            attrs={
+                "class": "form-select",
+                "id": "skillSetSelect",
+            }
+        ),
+    )
