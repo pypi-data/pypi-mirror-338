3abc3fd8cd00f85ae307161a7c32fcc5ac5302a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:08:08 +0000

    Add package regressionmadesimple/1.2.0/regressionmadesimple-1.2.0.tar.gz

---
+++ packages/regressionmadesimple/regressionmadesimple-1.2.0.tar.gz/regressionmadesimple-1.2.0/regressionmadesimple/__init__.py
@@ -0,0 +1,14 @@
+from .linear import Linear
+from .utils_preworks import Preworks
+from .options import options, save_options, load_options, reset_options
+
+__version__ = "1.2.0"
+
+__all__ = [
+    "Linear",
+    "Preworks",
+    "options",
+    "save_options",
+    "load_options",
+    "reset_options"
+]
