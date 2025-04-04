9987c92478f4d9f756cd3a9176421ef1b7e5561d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 08:05:10 +0000

    Add package commit-for-free/1.0.0.post1/commit_for_free-1.0.0.post1-py3-none-any.whl

---
+++ packages/commit-for-free/commit_for_free-1.0.0.post1-py3-none-any.whl/c4f/__main__.py
@@ -0,0 +1,11 @@
+"""
+Entry point for running c4f as a package.
+
+This module allows the package to be executed directly using:
+python -m c4f
+"""
+
+from .cli import main
+
+if __name__ == "__main__":
+    main() 
