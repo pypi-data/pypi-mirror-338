e8e508088ad0d4f1c6234044e04f75a415f04510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:06:19 +0000

    Add package JustSimplePy/2025.0.0.1/justsimplepy-2025.0.0.1-py3-none-any.whl

---
+++ packages/JustSimplePy/justsimplepy-2025.0.0.1-py3-none-any.whl/justsimplepython/__init__.py
@@ -0,0 +1,29 @@
+# JustSimplePy 🚀
+#A simple and efficient Python library for common coding tasks.
+
+## Features
+#✔ **Billing System** - Create detailed bills with tax calculations
+#✔ **Character Finder & Extractor** - Search for text or extract specific characters
+#✔ **Grid Formatter** - Convert user input into a structured 3x3 grid
+#✔ **SQL Data Inserter** - Send data directly to an SQL database
+#✔ **Easy Boot System** - Startup verification and user authentication
+
+## Installation
+#Install via **pip**:
+#```bash
+#pip install JustSimplePy
+
+# Preloads :
+import launch
+import SimpleMath
+import basicsetup
+# launch :
+
+# SimpleMath :
+
+
+# Boot setup :
+from JustSimplePython.basicsetup import on_boot
+
+# On boot :
+on_boot()
