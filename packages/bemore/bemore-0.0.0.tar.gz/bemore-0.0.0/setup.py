a59e71546ddc437b86f47c8256c1c1db7c821472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:50 +0000

    Add package bemore/0.0.0/bemore-0.0.0.tar.gz

---
+++ packages/bemore/bemore-0.0.0.tar.gz/bemore-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="bemore",
+    version="0.0",
+    description="Node based editor backend",
+    author="Thomas Satterly",
+    author_email="nunya@biz.ness",
+    packages=["bemore"],
+    install_requires=[],
+    scripts=[],
+)
