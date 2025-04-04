d0ebf580e2092a50ce676eef22b6932f32307084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:06:35 +0000

    Add package shareithub/1.1.2/shareithub-1.1.2-py3-none-any.whl

---
+++ packages/shareithub/shareithub-1.1.2-py3-none-any.whl/shareithub/__init__.py
@@ -0,0 +1,12 @@
+ascii_art = r"""
+╔═╗╦ ╦╔═╗╦═╗╔═╗  ╦╔╦╗  ╦ ╦╦ ╦╔╗ 
+╚═╗╠═╣╠═╣╠╦╝║╣   ║ ║   ╠═╣║ ║╠╩╗
+╚═╝╩ ╩╩ ╩╩╚═╚═╝  ╩ ╩   ╩ ╩╚═╝╚═╝
+"""
+intro = """
+Official Channel : SHARE IT HUB
+Telegram & Youtube, don't forget to subscribe, like & share !!!
+"""
+
+def subscribe():
+    return ascii_art + "\n\n" + intro
