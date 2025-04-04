b6d692e9bd3a0a4acc3486b6be645cf200ac18f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:05:56 +0000

    Add package lino/25.4.1/lino-25.4.1-py3-none-any.whl

---
+++ packages/lino/lino-25.4.1-py3-none-any.whl/lino/modlib/export_excel/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2014-2020 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+"""This plugin installs a button to export any table to excel xls format.
+
+To use it, simply add the following line to your
+:meth:`lino.core.site.Site.get_installed_plugins`::
+
+    yield 'lino.modlib.export_excel'
+
+"""
+
+from lino import ad, _
+
+
+class Plugin(ad.Plugin):
+    "See :doc:`/dev/plugins`."
+
+    verbose_name = _("Export to Excel xls format")
