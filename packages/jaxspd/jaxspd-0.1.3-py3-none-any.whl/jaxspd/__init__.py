11b3a0976ada0bf2e4a5c4d4ca2ee907bb2b12b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 05:05:54 +0000

    Add package jaxspd/0.1.3/jaxspd-0.1.3-py3-none-any.whl

---
+++ packages/jaxspd/jaxspd-0.1.3-py3-none-any.whl/jaxspd/__init__.py
@@ -0,0 +1,47 @@
+"""
+JAXSPD - JAX Speed Optimizations
+"""
+
+__version__ = "0.1.3"
+
+from .jit import jit
+from .gpu import enable_gpu_optimizations
+from .utils import optimize_for_gpu, batch_apply, device_put, pmap
+from .optimizations import (
+    auto_jit,
+    memory_efficient,
+    optimize_for_shape,
+    fused_operations,
+)
+from .profiling import (
+    Profiler,
+    profile,
+    debug_mode,
+    memory_tracker,
+    check_numerical_stability,
+)
+
+__all__ = [
+    # Core functionality
+    "jit",
+    "enable_gpu_optimizations",
+    "optimize_for_gpu",
+    
+    # Utility functions
+    "batch_apply",
+    "device_put",
+    "pmap",
+    
+    # Advanced optimizations
+    "auto_jit",
+    "memory_efficient",
+    "optimize_for_shape",
+    "fused_operations",
+    
+    # Profiling and debugging
+    "Profiler",
+    "profile",
+    "debug_mode",
+    "memory_tracker",
+    "check_numerical_stability",
+] 
