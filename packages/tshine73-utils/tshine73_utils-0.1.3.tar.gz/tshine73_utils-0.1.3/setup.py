92be173e217b599b73ca7a047f8e955eb26713d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 15:06:15 +0000

    Add package tshine73-utils/0.1.3/tshine73_utils-0.1.3.tar.gz

---
+++ packages/tshine73-utils/tshine73_utils-0.1.3.tar.gz/tshine73_utils-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+print(requirements)
+setup(
+    name="tshine73-utils",
+    version="0.1.3",
+    author="tshine73",
+    author_email="fan.steven.chiang@gmail.com",
+    description="tshine73 utils",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+    include_package_data=True,
+    install_requires=[
+        "psycopg2-binary==2.9.9", 'boto3==1.36.3', 'requests==2.32.3'
+    ]
+)
