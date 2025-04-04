5e027dc1e31dbf5680678edf9a025e2c2cfffa68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:32 +0000

    Add package django-username-email/2.5.7/django_username_email-2.5.7-py3-none-any.whl

---
+++ packages/django-username-email/django_username_email-2.5.7-py3-none-any.whl/cuser/admin.py
@@ -0,0 +1,51 @@
+import django
+from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group as StockGroup
+from django.utils.translation import gettext_lazy as _
+
+from cuser.forms import AdminUserCreationForm, UserChangeForm
+from cuser.models import CUser, Group
+from cuser.settings import CUSER_SETTINGS
+
+
+@admin.register(CUser)
+class UserAdmin(BaseUserAdmin):
+    add_form_template = 'admin/cuser/cuser/add_form.html'
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    form = UserChangeForm
+    add_form = AdminUserCreationForm
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            add_fieldsets = (
+                (None, {
+                    'classes': ('wide',),
+                    'fields': ['email', 'password1', 'password2'],
+                }),
+            )
+
+            if django.VERSION >= (5, 1):
+                add_fieldsets[0][1]['fields'].insert(1, 'usable_password')
+
+            return add_fieldsets
+
+        return super().get_fieldsets(request, obj)
+
+
+if CUSER_SETTINGS['register_proxy_auth_group_model']:
+    admin.site.unregister(StockGroup)
+
+    @admin.register(Group)
+    class GroupAdmin(BaseGroupAdmin):
+        pass
