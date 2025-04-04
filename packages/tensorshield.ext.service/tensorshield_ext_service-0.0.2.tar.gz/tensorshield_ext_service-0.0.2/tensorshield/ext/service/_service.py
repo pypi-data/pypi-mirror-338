b278f48811251a85911b2b2c9fe389a2d547e069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:07:03 +0000

    Add package tensorshield.ext.service/0.0.2/tensorshield_ext_service-0.0.2.tar.gz

---
+++ packages/tensorshield.ext.service/tensorshield_ext_service-0.0.2.tar.gz/tensorshield_ext_service-0.0.2/tensorshield/ext/service/_service.py
@@ -0,0 +1,32 @@
+import contextlib
+from typing import Self
+
+import fastapi
+from tensorshield.ext.subtensor import AsyncSubtensor
+
+
+class Service(fastapi.FastAPI):
+    chain_endpoint: str | None = None
+    title: str
+    subtensor: AsyncSubtensor | None = None
+
+    def __init__(
+        self,
+        chain_endpoint: str | None = None
+    ):
+        super().__init__(
+            lifespan=self.default_lifespan,
+            docs_url=None,
+            redoc_url='/docs',
+            title=self.title
+        )
+        self.chain_endpoint = chain_endpoint
+
+    @contextlib.asynccontextmanager
+    async def default_lifespan(self, _: Self):
+        if self.chain_endpoint:
+            self.subtensor = AsyncSubtensor(chain_endpoint=self.chain_endpoint)
+            await self.subtensor.connect()
+        yield
+        if self.subtensor is not None:
+            await self.subtensor.close()
