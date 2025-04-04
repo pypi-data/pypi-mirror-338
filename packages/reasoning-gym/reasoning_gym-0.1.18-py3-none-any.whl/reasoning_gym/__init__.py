c6233d65680abc76a11469a9935040b6519a6dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 08:05:00 +0000

    Add package reasoning-gym/0.1.18/reasoning_gym-0.1.18-py3-none-any.whl

---
+++ packages/reasoning-gym/reasoning_gym-0.1.18-py3-none-any.whl/reasoning_gym/__init__.py
@@ -0,0 +1,24 @@
+"""
+Reasoning Gym - A library of procedural dataset generators for training reasoning models
+"""
+
+from . import algebra, algorithmic, arc, arithmetic, code, cognition, data, games, geometry, graphs, induction, logic
+from .factory import create_dataset, register_dataset
+
+__version__ = "0.1.18"
+__all__ = [
+    "arc",
+    "algebra",
+    "algorithmic",
+    "arithmetic",
+    "code",
+    "cognition",
+    "data",
+    "games",
+    "geometry",
+    "graphs",
+    "logic",
+    "induction",
+    "create_dataset",
+    "register_dataset",
+]
---
+++ packages/reasoning-gym/reasoning_gym-0.1.18-py3-none-any.whl/reasoning_gym/logic/__init__.py
@@ -0,0 +1,35 @@
+"""
+Logic tasks for training reasoning capabilities.
+"""
+
+from .aiw import AliceInWonderlandConfig, AliceInWonderlandCurriculum, AliceInWonderlandDataset
+from .circuit_logic import CircuitLogicConfig, CircuitLogicCurriculum, CircuitLogicDataset
+from .knights_knaves import KnightsKnavesConfig, KnightsKnavesCurriculum, KnightsKnavesDataset
+from .propositional_logic import PropositionalLogicConfig, PropositionalLogicCurriculum, PropositionalLogicDataset
+from .self_reference import SelfReferenceConfig, SelfReferenceCurriculum, SelfReferenceDataset
+from .syllogisms import SyllogismConfig, SyllogismDataset
+from .zebra_puzzles import ZebraConfig, ZebraCurriculum, ZebraDataset
+
+__all__ = [
+    "AliceInWonderlandConfig",
+    "AliceInWonderlandCurriculum",
+    "AliceInWonderlandDataset",
+    "PropositionalLogicConfig",
+    "PropositionalLogicDataset",
+    "PropositionalLogicCurriculum",
+    "SyllogismConfig",
+    "SyllogismDataset",
+    "syllogism_dataset",
+    "ZebraConfig",
+    "ZebraCurriculum",
+    "ZebraDataset",
+    "SelfReferenceCurriculum",
+    "SelfReferenceConfig",
+    "SelfReferenceDataset",
+    "CircuitLogicConfig",
+    "CircuitLogicDataset",
+    "CircuitLogicCurriculum",
+    "KnightsKnavesConfig",
+    "KnightsKnavesDataset",
+    "KnightsKnavesCurriculum",
+]
