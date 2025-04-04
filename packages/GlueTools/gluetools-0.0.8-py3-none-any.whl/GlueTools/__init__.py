327e5aae75be0ad64db3277ee8a1aa8e921e53e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:37 +0000

    Add package GlueTools/0.0.8/gluetools-0.0.8-py3-none-any.whl

---
+++ packages/GlueTools/gluetools-0.0.8-py3-none-any.whl/GlueTools/__init__.py
@@ -0,0 +1,7 @@
+from .myfunctions import connectDB
+from .myfunctions import close_connect
+from .myfunctions import querySQL
+from .myfunctions import send_mail
+from .myfunctions import close_connect_SFTP
+from .myfunctions import ReadFile_SFTP
+from .myfunctions import MoveFile
