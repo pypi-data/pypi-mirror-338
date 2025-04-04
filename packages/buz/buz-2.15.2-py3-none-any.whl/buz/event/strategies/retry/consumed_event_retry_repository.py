9f715eadd116e1934e3257bce32329297f4dc3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 16:06:44 +0000

    Add package buz/2.15.2/buz-2.15.2-py3-none-any.whl

---
+++ packages/buz/buz-2.15.2-py3-none-any.whl/buz/event/strategies/retry/consumed_event_retry_repository.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from buz.event import Event
+
+from buz.event.meta_subscriber import MetaSubscriber
+from buz.event.strategies.retry.consumed_event_retry import ConsumedEventRetry
+
+
+class ConsumedEventRetryRepository(ABC):
+    @abstractmethod
+    def save(self, consumed_event_retry: ConsumedEventRetry) -> None:
+        pass
+
+    @abstractmethod
+    def find_one_by_event_and_subscriber(
+        self, event: Event, subscribers: Sequence[MetaSubscriber]
+    ) -> ConsumedEventRetry:
+        pass
