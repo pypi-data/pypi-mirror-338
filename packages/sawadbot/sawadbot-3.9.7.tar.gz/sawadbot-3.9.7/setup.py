51e62b7441a1790213c1b19e559b942430e9520d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:06:09 +0000

    Add package sawadbot/3.9.7/sawadbot-3.9.7.tar.gz

---
+++ packages/sawadbot/sawadbot-3.9.7.tar.gz/sawadbot-3.9.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sawadbot",
+    version="3.9.7",
+    author="B_Q_5",
+    description="Tele : @B_Q_5",
+    packages=find_packages(),
+    install_requires=[
+        'pyTelegramBotAPI',
+    ],
+)
