a654ef44bf3f0a18c305c7246c6c76f6aac63176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 04:06:30 +0000

    Add package django-polls-gs377209/0.0.5/django_polls_gs377209-0.0.5.tar.gz

---
+++ packages/django-polls-gs377209/django_polls_gs377209-0.0.5.tar.gz/django_polls_gs377209-0.0.5/django_polls_gs377209/apps.py
@@ -0,0 +1,16 @@
+"""_summary_
+"""
+
+
+from django.apps import AppConfig
+
+
+class PollsConfig(AppConfig):
+    """_summary_
+
+    Args:
+        AppConfig (_type_): _description_
+    """
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_polls_gs377209"
+    label = "polls"
---
+++ packages/django-polls-gs377209/django_polls_gs377209-0.0.5.tar.gz/django_polls_gs377209-0.0.5/tests/test_sample.py
@@ -0,0 +1,20 @@
+"""_summary_
+"""
+
+
+def func(x):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return x + 1
+
+
+def test_answer():
+    """_summary_
+    """
+    assert func(4) == 5
