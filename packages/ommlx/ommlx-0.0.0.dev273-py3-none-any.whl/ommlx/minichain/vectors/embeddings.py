bf42124ec8a7711734df8e00e1697d67b3a27404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 14:05:59 +0000

    Add package ommlx/0.0.0.dev273/ommlx-0.0.0.dev273-py3-none-any.whl

---
+++ packages/ommlx/ommlx-0.0.0.dev273-py3-none-any.whl/ommlx/minichain/vectors/embeddings.py
@@ -0,0 +1,54 @@
+import abc
+import typing as ta
+
+from omlish import dataclasses as dc
+from omlish import lang
+
+from ..content.content import Content
+from ..models import Model
+from ..models import ModelOption
+from ..models import ModelRequest
+from ..models import ModelResponse
+from .vectors import Vector
+
+
+##
+
+
+EmbeddingInput: ta.TypeAlias = Content
+EmbeddingNew: ta.TypeAlias = ta.Any
+EmbeddingOutput: ta.TypeAlias = Vector
+
+EmbeddingOptions: ta.TypeAlias = ModelOption
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class EmbeddingRequest(
+    ModelRequest[
+        EmbeddingInput,
+        EmbeddingOptions,
+        EmbeddingNew,
+    ],
+    lang.Final,
+):
+    pass
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class EmbeddingResponse(ModelResponse[EmbeddingOutput], lang.Final):
+    pass
+
+
+# @omlish-manifest ommlx.minichain.backends.manifests.BackendTypeManifest
+class EmbeddingModel(
+    Model[
+        EmbeddingRequest,
+        EmbeddingOptions,
+        EmbeddingNew,
+        EmbeddingResponse,
+    ],
+    lang.Abstract,
+):
+    @abc.abstractmethod
+    def invoke(self, request: EmbeddingRequest) -> EmbeddingResponse:
+        raise NotImplementedError
