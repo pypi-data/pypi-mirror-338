2e87326016ce25fe0004b12bc131ddf201bb3af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 06:04:51 +0000

    Add package databridge-client/0.3.0/databridge_client-0.3.0.tar.gz

---
+++ packages/databridge-client/databridge_client-0.3.0.tar.gz/databridge_client-0.3.0/databridge/__init__.py
@@ -0,0 +1,15 @@
+"""
+DataBridge Python SDK for document ingestion and querying.
+"""
+
+from .sync import DataBridge
+from .async_ import AsyncDataBridge
+from .models import Document
+
+__all__ = [
+    "DataBridge",
+    "AsyncDataBridge",
+    "Document",
+]
+
+__version__ = "0.3.0"
