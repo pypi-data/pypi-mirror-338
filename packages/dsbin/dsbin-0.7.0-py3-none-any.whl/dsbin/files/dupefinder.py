edc65b2b23deb5c6deae5b2b495229e7882b46af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 08:12:12 +0000

    Add package dsbin/0.7.0/dsbin-0.7.0-py3-none-any.whl

---
+++ packages/dsbin/dsbin-0.7.0-py3-none-any.whl/dsbin/files/dupefinder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""Find duplicate files in a directory.
+
+This script will find duplicate files in a directory and print them to the console.
+"""
+
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+
+from polykit import polykit_setup
+
+from dsbin.files import FileManager
+
+polykit_setup()
+
+
+def main() -> None:
+    """Find duplicate files in a directory."""
+    input_files = sys.argv[1:]
+    if len(input_files) == 0:
+        input_files = [str(f) for f in Path().iterdir() if f.is_file()]
+
+    # Convert all input files to Path objects if they aren't already
+    paths = [Path(f) for f in input_files]
+    FileManager().find_dupes_by_hash(paths)
+
+
+if __name__ == "__main__":
+    main()
