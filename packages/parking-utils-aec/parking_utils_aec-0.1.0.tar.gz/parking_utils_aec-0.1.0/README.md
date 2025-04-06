3176d7abbe97bbf5735ddd93f7da5a5bf001e4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:03 +0000

    Add package parking-utils-aec/0.1.0/parking_utils_aec-0.1.0.tar.gz

---
+++ packages/parking-utils-aec/parking_utils_aec-0.1.0.tar.gz/parking_utils_aec-0.1.0/README.md
@@ -0,0 +1,20 @@
+# Parking Utils Library
+
+A utility library for parking applications with AWS integration.
+
+## Installation
+```bash
+
+## python version required 3.9
+
+## extract the zip folder and open any code editor
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install -r requiremeents.txt
+
+pip install -i https://test.pypi.org/simple/ parking-utils-aec
+
+run the server
---
+++ packages/parking-utils-aec/parking_utils_aec-0.1.0.tar.gz/parking_utils_aec-0.1.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parking-utils-aec",
+    version="0.1.0",
+    author="Harish Challa",
+    author_email="x23417498@student.ncirl.ie",
+    description="A utility library for parking applications with AWS integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "Pillow==10.4.0",
+        "boto3==1.37.23",
+        "tenacity==9.0.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9,<3.14",
+)
