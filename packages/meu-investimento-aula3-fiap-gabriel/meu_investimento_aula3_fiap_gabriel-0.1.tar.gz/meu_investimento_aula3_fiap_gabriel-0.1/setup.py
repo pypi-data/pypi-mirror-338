60fbe830f546387463bf1a4a753741e407b6e829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 07:07:52 +0000

    Add package meu-investimento-aula3-fiap-gabriel/0.1/meu_investimento_aula3_fiap_gabriel-0.1.tar.gz

---
+++ packages/meu-investimento-aula3-fiap-gabriel/meu_investimento_aula3_fiap_gabriel-0.1.tar.gz/meu_investimento_aula3_fiap_gabriel-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_aula3_fiap_gabriel',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='Gabriel Siqueira de Lima',
+   author_email='gabrielsiqueira094@gmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
