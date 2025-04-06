39ced0029406ee85ebb51dc4f201620f1cfddf13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:28 +0000

    Add package epics-archiver-tools/1.0.3/epics_archiver_tools-1.0.3-py3-none-any.whl

---
+++ packages/epics-archiver-tools/epics_archiver_tools-1.0.3-py3-none-any.whl/archivertools/__init__.py
@@ -0,0 +1,25 @@
+"""
+ArchiverTools - A Python library for interacting with EPICS archiver data.
+
+This package provides tools for downloading, processing, and analyzing data from EPICS archiver servers.
+It supports data retrieval, missing value imputation, and data matching across multiple Process Variables (PVs).
+
+Example:
+    >>> from archivertools import ArchiverClient
+    >>> client = ArchiverClient(archiver_url="http://your-server:17665")
+    >>> data = client.download_data(pv_name="YOUR_PV", start=start_time, end=end_time)
+"""
+
+from .archiver_client import ArchiverClient
+from .archiving_policy import ArchivingPolicy
+from .pv import PV
+
+__all__ = [
+    'archiver_client',
+    'archiving_policy',
+    'pv',
+]
+
+__version__ = '1.0.3'
+__author__ = 'Andrea Pollastro'
+__author_email__ = 'apollastro@lbl.gov'
