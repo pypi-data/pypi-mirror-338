46642442d4bd1e0503659465a5bd4eca40c95423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 06:05:32 +0000

    Add package esmerald/3.7.1/esmerald-3.7.1-py3-none-any.whl

---
+++ packages/esmerald/esmerald-3.7.1-py3-none-any.whl/esmerald/routing/events.py
@@ -0,0 +1,5 @@
+from lilya._internal._events import (
+    AsyncLifespanContextManager as AsyncLifespanContextManager,
+    generate_lifespan_events as generate_lifespan_events,
+    handle_lifespan_events as handle_lifespan_events,
+)
