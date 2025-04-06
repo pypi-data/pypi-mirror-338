342930670adc74b5905f1a342ecdb6a3537c933f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:06:07 +0000

    Add package aquastilla-softener/0.1.3/aquastilla_softener-0.1.3.tar.gz

---
+++ packages/aquastilla-softener/aquastilla_softener-0.1.3.tar.gz/aquastilla_softener-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="aquastilla_softener",
+    description="Library to fetch data from Aquastilla softener from Viessmann API",
+    version="0.1.3",
+    license="GPL",
+    author="Tomasz Szymanowicz",
+    author_email="alakdae@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/alakdae/aquastilla-softener",
+    keywords="aquastilla softener aquastilla_softener",
+)
