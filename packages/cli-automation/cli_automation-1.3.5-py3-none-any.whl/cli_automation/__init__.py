e482f3c80bc0e04b92e7da37a3880420a615ce51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 08:11:54 +0000

    Add package cli-automation/1.3.5/cli_automation-1.3.5-py3-none-any.whl

---
+++ packages/cli-automation/cli_automation-1.3.5-py3-none-any.whl/cli_automation/__init__.py
@@ -0,0 +1,60 @@
+import json
+from pathlib import Path
+import logging
+import logging.handlers
+from cli_automation.config_srv import *
+
+DATA = {
+    "tunnel": False,
+    "app": "cla",
+}
+class ClaConfig():
+    def __init__(self):
+        self.data = DATA
+        self.config_path = Path(__file__).parent / "config.json"
+        self.config = CONFIG_PARAMS
+        self.version = "1.3.5 - XXI - By Ed Scrimaglia"
+
+    def load_config(self):
+        try:
+            self.data.update(self.config)
+            with open(self.config_path, "r") as read_file:
+                file_read = json.load(read_file)
+                file_read["version"] = self.version
+                return file_read
+        except FileNotFoundError:
+            with open(self.config_path, "w") as write_file:
+                json.dump(self.data, write_file, indent=3)
+                return self.data
+        except Exception:
+            print ("** Error creating the configuration file")
+            SystemExit(1)
+
+class Logger():
+    def __init__(self):
+        self.logger = logging.getLogger("ClaLogger")
+        self.logger.setLevel(logging.DEBUG)
+        self.log_file = DATA.get("log_file")
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler = logging.handlers.RotatingFileHandler(self.log_file, maxBytes=5*1024*1024, backupCount=5)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.DEBUG)
+        self.logger.addHandler(file_handler)
+
+    def get_logger(self):
+        return self.logger
+    
+class Html():
+    def __init__(self):
+        self.index = Path("navigate.html")
+        self.cla = Path("cla.html")
+
+    def create_html(self):
+        if not self.index.exists():
+            if self.cla.exists():
+                data = self.cla.read_text()
+                self.index.write_text(data)
+
+config_data = ClaConfig().load_config()
+logger = Logger().get_logger()
+html = Html().create_html()
