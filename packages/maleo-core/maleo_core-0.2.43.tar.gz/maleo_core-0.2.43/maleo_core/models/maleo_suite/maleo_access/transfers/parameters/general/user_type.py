2b4db6c22251017cecb8b497b97d00ee91a955da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:28 +0000

    Add package maleo-core/0.2.43/maleo_core-0.2.43.tar.gz

---
+++ packages/maleo-core/maleo_core-0.2.43.tar.gz/maleo_core-0.2.43/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/general/user_type.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from enum import StrEnum
+from pydantic import Field
+from typing import Union
+from uuid import UUID
+from maleo_core.models.base.transfers.parameters.general import BaseGeneralParameters
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.user_type import MaleoAccessUserTypeGeneralTransfers
+
+class MaleoAccessUserTypeGeneralParameters:
+    class UniqueIdentifiers(StrEnum):
+        ID = "id"
+        UUID = "uuid"
+        KEY = "key"
+        NAME = "name"
+
+    class GetSingle(BaseGeneralParameters.GetSingle):
+        identifier:MaleoAccessUserTypeGeneralParameters.UniqueIdentifiers = Field(..., description="Identifier")
+        value:Union[int, UUID, str] = Field(..., description="Value")
+
+    CreateOrUpdate = MaleoAccessUserTypeGeneralTransfers.Base
+
+    class CheckUniqueExistence(BaseGeneralParameters.CheckUniqueExistence):
+        field:MaleoAccessUserTypeGeneralParameters.UniqueIdentifiers = Field(..., description="Field to be checked")
+
+    UniqueFieldChecks = list[CheckUniqueExistence]
