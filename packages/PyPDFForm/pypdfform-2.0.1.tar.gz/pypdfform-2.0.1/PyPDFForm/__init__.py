4d57c3912062e5ce5f4bf9c2a20302e5c3c0f841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 08:12:40 +0000

    Add package PyPDFForm/2.0.1/pypdfform-2.0.1.tar.gz

---
+++ packages/PyPDFForm/pypdfform-2.0.1.tar.gz/pypdfform-2.0.1/PyPDFForm/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Contains any object users might need."""
+
+__version__ = "2.0.1"
+
+from .wrapper import FormWrapper, PdfWrapper
+
+__all__ = ["FormWrapper", "PdfWrapper"]
