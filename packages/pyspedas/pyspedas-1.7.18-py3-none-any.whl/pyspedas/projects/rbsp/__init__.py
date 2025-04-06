00814324c3499946789da2ce3f37d12c1cc86442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 07:07:54 +0000

    Add package pyspedas/1.7.18/pyspedas-1.7.18-py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-1.7.18-py3-none-any.whl/pyspedas/projects/rbsp/__init__.py
@@ -0,0 +1,12 @@
+from .datasets import datasets
+from .efw import efw
+from .emfisis import emfisis
+from .hope import hope
+from .mageis import mageis
+from .magephem import magephem
+from .rbspice import rbspice
+from .rept import rept
+from .rps import rps
+from .rbspice_lib.rbsp_rbspice_omni import rbsp_rbspice_omni
+from .rbspice_lib.rbsp_rbspice_pad import rbsp_rbspice_pad
+
