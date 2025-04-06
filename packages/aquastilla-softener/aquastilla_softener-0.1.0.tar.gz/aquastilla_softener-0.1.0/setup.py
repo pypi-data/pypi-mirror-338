8ac62992a2c46c55e65e765cac939c2f4aa6cd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:20:06 +0000

    Add package aquastilla-softener/0.1.0/aquastilla_softener-0.1.0.tar.gz

---
+++ packages/aquastilla-softener/aquastilla_softener-0.1.0.tar.gz/aquastilla_softener-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="aquastilla_softener",
+    version="0.1.0",
+    license="GPL",
+    author="Tomasz Szymanowicz",
+    author_email="alakdae@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/alakdae/aquastilla-softener",
+    keywords="aquastilla softener aquastilla_softener",
+)
