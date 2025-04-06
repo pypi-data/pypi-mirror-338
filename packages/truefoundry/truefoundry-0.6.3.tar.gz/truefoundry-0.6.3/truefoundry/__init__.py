7c095129481f24754c1e293695369b767066abc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:15 +0000

    Add package truefoundry/0.6.3/truefoundry-0.6.3.tar.gz

---
+++ packages/truefoundry/truefoundry-0.6.3.tar.gz/truefoundry-0.6.3/truefoundry/__init__.py
@@ -0,0 +1,31 @@
+from truefoundry_sdk import (
+    AssistantMessage,
+    ChatPromptManifest,
+    ModelConfiguration,
+    Parameters,
+    PromptVersion,
+    SystemMessage,
+    UserMessage,
+)
+
+from truefoundry._client import client
+from truefoundry.common.warnings import (
+    suppress_truefoundry_deprecation_warnings,
+    surface_truefoundry_deprecation_warnings,
+)
+from truefoundry.deploy.core import login, logout
+
+surface_truefoundry_deprecation_warnings()
+__all__ = [
+    "AssistantMessage",
+    "ChatPromptManifest",
+    "client",
+    "login",
+    "logout",
+    "ModelConfiguration",
+    "Parameters",
+    "PromptVersion",
+    "suppress_truefoundry_deprecation_warnings",
+    "SystemMessage",
+    "UserMessage",
+]
