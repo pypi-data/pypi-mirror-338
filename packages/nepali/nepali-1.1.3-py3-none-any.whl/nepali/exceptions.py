c1337db8823d3891794311bd374d927e19ed083a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:54 +0000

    Add package nepali/1.1.3/nepali-1.1.3-py3-none-any.whl

---
+++ packages/nepali/nepali-1.1.3-py3-none-any.whl/nepali/exceptions.py
@@ -0,0 +1,23 @@
+"""
+Exceptions for nepali
+"""
+
+
+class InvalidDateFormatException(Exception):
+    pass
+
+
+class InvalidDateTimeFormatException(Exception):
+    pass
+
+
+class InvalidNepaliDateTimeObjectException(Exception):
+    pass
+
+
+class FormatNotMatchException(Exception):
+    """
+    raised while parsing nepalidatetime format
+    """
+
+    pass
