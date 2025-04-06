0dc1bca9d15ae977ad22b268a2dbb62df97d8ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 06:05:49 +0000

    Add package dtiplayground/0.5.9b4/dtiplayground-0.5.9b4.tar.gz

---
+++ packages/dtiplayground/dtiplayground-0.5.9b4.tar.gz/dtiplayground-0.5.9b4/dtiplayground/config.py
@@ -0,0 +1,21 @@
+
+INFO = {
+  "dtiplayground": { 
+    "version" : "0.5.9b4"
+  },
+  "dmriplayground": {
+    "version" : "0.5.9b4"
+  },
+  "dmriprep": {
+    "version" : "0.5.9b4"
+  },
+  "dmriatlas": {
+    "version" : "0.2.0b1"
+  },
+  "dmriautotract" : {
+    "version" : "0.0.2b1"
+  },
+  "dmrifiberprofile" : {
+    "version" : "0.5.9"
+  }
+}
