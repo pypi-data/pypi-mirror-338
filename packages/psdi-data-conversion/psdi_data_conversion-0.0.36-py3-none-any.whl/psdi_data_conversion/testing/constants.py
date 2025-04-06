c0e7e89b7e8210746ac56a33fc202ab60c6a1890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:05:56 +0000

    Add package psdi-data-conversion/0.0.36/psdi_data_conversion-0.0.36-py3-none-any.whl

---
+++ packages/psdi-data-conversion/psdi_data_conversion-0.0.36-py3-none-any.whl/psdi_data_conversion/testing/constants.py
@@ -0,0 +1,12 @@
+"""
+# constants.py
+
+Constants related to unit testing
+"""
+
+import os
+
+TEST_DATA_LOC = os.path.abspath("./test_data")
+
+INPUT_TEST_DATA_LOC = TEST_DATA_LOC
+OUTPUT_TEST_DATA_LOC = os.path.join(TEST_DATA_LOC, "output")
