ae2e3236d4bc7447b95d39b59372d9c90c72e733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:58 +0000

    Add package atomorph/0.2.6/atomorph-0.2.6.tar.gz

---
+++ packages/atomorph/atomorph-0.2.6.tar.gz/atomorph-0.2.6/atomorph/cli.py
@@ -0,0 +1,9 @@
+'''
+Author: glab-cabage 2227541807@qq.com
+Date: 2025-04-04 20:59:12
+LastEditors: glab-cabage 2227541807@qq.com
+LastEditTime: 2025-04-04 21:03:07
+FilePath: /AI project/Atomorph/Atomorph/cli.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+ 
