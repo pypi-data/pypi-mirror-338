c030223ba6f9824a634afb6550aaad0e774a6239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:11:33 +0000

    Add package dgmr/1.4.2/dgmr-1.4.2-py3-none-any.whl

---
+++ packages/dgmr/dgmr-1.4.2-py3-none-any.whl/dgmr/__init__.py
@@ -0,0 +1,5 @@
+"""Submodules of dgmr."""
+from .common import ContextConditioningStack, LatentConditioningStack
+from .dgmr import DGMR
+from .discriminators import Discriminator, SpatialDiscriminator, TemporalDiscriminator
+from .generators import Generator, Sampler
