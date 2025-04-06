ee804bd9d834aeaee6e1bf125513ea44d3bc7c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 07:07:15 +0000

    Add package langchain-litellm/0.1.4/langchain_litellm-0.1.4-py3-none-any.whl

---
+++ packages/langchain-litellm/langchain_litellm-0.1.4-py3-none-any.whl/langchain_litellm/__init__.py
@@ -0,0 +1,15 @@
+from importlib import metadata
+
+from .chat_models import ChatLiteLLM
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
+del metadata  # optional, avoids polluting the results of dir(__package__)
+
+__all__ = [
+    "ChatLiteLLM",
+    "__version__",
+]
