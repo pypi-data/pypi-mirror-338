0758fceecf756c740f5d9f327e4f78e0407a7fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:06:25 +0000

    Add package cognyx-bom-sdk/0.2.5/cognyx_bom_sdk-0.2.5-py3-none-any.whl

---
+++ packages/cognyx-bom-sdk/cognyx_bom_sdk-0.2.5-py3-none-any.whl/cognyx_bom_sdk/__init__.py
@@ -0,0 +1,46 @@
+"""Cognyx BOM SDK package.
+
+This library allows interaction with the Cognyx BOM API when writing BOM automations in Cognyx.
+It provides a set of functions to manipulate BOM data in a Pyodide sandbox environment.
+"""
+
+__version__ = "0.1.0"
+
+from cognyx_bom_sdk.client import BomClient
+from cognyx_bom_sdk.models import (
+    Attribute,
+    AttributeUpdatePayload,
+    Bom,
+    BomInstance,
+    BomInstanceSystemAttributes,
+    BomInstanceSystemStatus,
+    BomInstanceUpdate,
+    BomReadinessStatus,
+    BomUpdates,
+    Diversity,
+    InstanceUpdatePayload,
+    Object,
+    ObjectType,
+    UpdatePayload,
+    VariabilityConfiguration,
+)
+
+# Export main classes and functions for easier imports
+__all__ = [
+    "Attribute",
+    "AttributeUpdatePayload",
+    "Bom",
+    "BomClient",
+    "BomInstance",
+    "BomInstanceSystemAttributes",
+    "BomInstanceSystemStatus",
+    "BomInstanceUpdate",
+    "BomReadinessStatus",
+    "BomUpdates",
+    "Diversity",
+    "InstanceUpdatePayload",
+    "Object",
+    "ObjectType",
+    "UpdatePayload",
+    "VariabilityConfiguration",
+]
