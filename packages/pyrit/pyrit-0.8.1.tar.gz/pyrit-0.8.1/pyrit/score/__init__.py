11340127e614a25d4a4834d89313f022efc0f794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:42 +0000

    Add package pyrit/0.8.1/pyrit-0.8.1.tar.gz

---
+++ packages/pyrit/pyrit-0.8.1.tar.gz/pyrit-0.8.1/pyrit/score/__init__.py
@@ -0,0 +1,42 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from pyrit.score.azure_content_filter_scorer import AzureContentFilterScorer
+from pyrit.score.float_scale_threshold_scorer import FloatScaleThresholdScorer
+from pyrit.score.gandalf_scorer import GandalfScorer
+from pyrit.score.human_in_the_loop_scorer import HumanInTheLoopScorer
+from pyrit.score.human_in_the_loop_gradio import HumanInTheLoopScorerGradio
+from pyrit.score.insecure_code_scorer import InsecureCodeScorer
+from pyrit.score.markdown_injection import MarkdownInjectionScorer
+from pyrit.score.prompt_shield_scorer import PromptShieldScorer
+from pyrit.score.scorer import Scorer
+from pyrit.score.self_ask_category_scorer import ContentClassifierPaths, SelfAskCategoryScorer
+from pyrit.score.self_ask_likert_scorer import LikertScalePaths, SelfAskLikertScorer
+from pyrit.score.self_ask_refusal_scorer import SelfAskRefusalScorer
+from pyrit.score.self_ask_scale_scorer import SelfAskScaleScorer
+from pyrit.score.self_ask_true_false_scorer import SelfAskTrueFalseScorer, TrueFalseQuestion, TrueFalseQuestionPaths
+from pyrit.score.substring_scorer import SubStringScorer
+from pyrit.score.true_false_inverter_scorer import TrueFalseInverterScorer
+
+__all__ = [
+    "AzureContentFilterScorer",
+    "ContentClassifierPaths",
+    "FloatScaleThresholdScorer",
+    "GandalfScorer",
+    "HumanInTheLoopScorer",
+    "HumanInTheLoopScorerGradio",
+    "InsecureCodeScorer",
+    "LikertScalePaths",
+    "MarkdownInjectionScorer",
+    "PromptShieldScorer",
+    "Scorer",
+    "SelfAskCategoryScorer",
+    "SelfAskLikertScorer",
+    "SelfAskRefusalScorer",
+    "SelfAskScaleScorer",
+    "SelfAskTrueFalseScorer",
+    "SubStringScorer",
+    "TrueFalseInverterScorer",
+    "TrueFalseQuestion",
+    "TrueFalseQuestionPaths",
+]
