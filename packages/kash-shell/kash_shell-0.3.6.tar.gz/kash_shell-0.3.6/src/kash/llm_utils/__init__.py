caab468ebf51bfc27a82d9926347033a5363861c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 06:05:19 +0000

    Add package kash-shell/0.3.6/kash_shell-0.3.6.tar.gz

---
+++ packages/kash-shell/kash_shell-0.3.6.tar.gz/kash_shell-0.3.6/src/kash/llm_utils/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa: F401
+
+from kash.llm_utils.llm_completion import (
+    LLMCompletionResult,
+    llm_completion,
+    llm_template_completion,
+)
+from kash.llm_utils.llm_messages import Message, MessageTemplate
+from kash.llm_utils.llm_names import LLMDefault, LLMName
+from kash.llm_utils.llms import LLM
