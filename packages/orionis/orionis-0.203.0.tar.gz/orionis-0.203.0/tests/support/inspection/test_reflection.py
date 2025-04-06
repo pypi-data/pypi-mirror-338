3bdd26c70279f3a4439383afdae3473b977e0c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:05:52 +0000

    Add package orionis/0.203.0/orionis-0.203.0.tar.gz

---
+++ packages/orionis/orionis-0.203.0.tar.gz/orionis-0.203.0/tests/support/inspection/test_reflection.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestReflection(unittest.TestCase):
+    """
+    Unit tests for basic example functionality.
+    """
+
+    def testReflection(self):
+        self.assertEqual(3, 3)
