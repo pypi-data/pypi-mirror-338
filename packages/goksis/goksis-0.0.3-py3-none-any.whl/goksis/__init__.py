e6e72cf7ca95d384e2f4c3574b62ccb8b06a5b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 07:07:10 +0000

    Add package goksis/0.0.3/goksis-0.0.3-py3-none-any.whl

---
+++ packages/goksis/goksis-0.0.3-py3-none-any.whl/goksis/__init__.py
@@ -0,0 +1,7 @@
+from .filterwheel import FilterWheel
+from .camera import Camera
+from .telescope import Telescope
+from .enclosure import Enclosure
+from .focuser import Focuser
+from .weather import Weather
+from .utils import nmap_v4, nmap_v6
