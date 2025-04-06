041937a243e250ce7b00da4d9602bc98f699019a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 07:06:01 +0000

    Add package subatomic-agents/0.1.1/subatomic_agents-0.1.1-py3-none-any.whl

---
+++ packages/subatomic-agents/subatomic_agents-0.1.1-py3-none-any.whl/sales_proposal/agents/invoke_sales_proposal_agent.py
@@ -0,0 +1,7 @@
+from src.sales_proposal.agents.sales_proposal_agent import create_sales_proposal_agent
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+def get_sales_proposal_agent():
+    return create_sales_proposal_agent()
