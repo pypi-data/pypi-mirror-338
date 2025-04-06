bba1976daff6e1ebae8da1ecced89853c766e407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 06:05:35 +0000

    Add package nyc-parser/0.1.1/nyc_parser-0.1.1.tar.gz

---
+++ packages/nyc-parser/nyc_parser-0.1.1.tar.gz/nyc_parser-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nyc-parser",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A parser for New York City addresses and BBL values",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ian Shiland",
+    author_email="ishiland@gmail.com",
+    url="https://github.com/ishiland/nyc-parser",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    extras_require={
+        "docs": ["sphinx>=4.0.0", "sphinx_rtd_theme>=1.0.0"],
+    },
+)
