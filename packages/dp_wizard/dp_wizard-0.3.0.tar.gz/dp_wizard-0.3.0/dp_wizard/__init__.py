5bce3f35edb576c31235725feb071da462bcaf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:05:39 +0000

    Add package dp_wizard/0.3.0/dp_wizard-0.3.0.tar.gz

---
+++ packages/dp_wizard/dp_wizard-0.3.0.tar.gz/dp_wizard-0.3.0/dp_wizard/__init__.py
@@ -0,0 +1,21 @@
+"""DP Wizard makes it easier to get started with Differential Privacy."""
+
+from pathlib import Path
+
+
+__version__ = (Path(__file__).parent / "VERSION").read_text().strip()
+
+
+def main():  # pragma: no cover
+    import shiny
+    from dp_wizard.utils.argparse_helpers import get_cli_info
+
+    # We only call this here so "--help" is handled,
+    # and to validate inputs before starting the server.
+    get_cli_info()
+
+    shiny.run_app(
+        app="dp_wizard.app",
+        launch_browser=True,
+        reload=True,
+    )
