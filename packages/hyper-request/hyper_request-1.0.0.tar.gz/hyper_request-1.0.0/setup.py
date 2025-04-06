4146464170b97dd1bb0c053c176315740a38e961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 08:13:06 +0000

    Add package hyper-request/1.0.0/hyper_request-1.0.0.tar.gz

---
+++ packages/hyper-request/hyper_request-1.0.0.tar.gz/hyper_request-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+# Setting up
+setup(
+    version = "1.0.0",
+    name="hyper_request", 
+    author="",
+    author_email="",
+    packages=find_packages(),
+)
