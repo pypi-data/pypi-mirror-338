e1d37f9d62e2cc854e31172890799e2b032a47df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 07:06:35 +0000

    Add package subtransss/0.1.0/subtransss-0.1.0.tar.gz

---
+++ packages/subtransss/subtransss-0.1.0.tar.gz/subtransss-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+setup(
+    name='subtransss',
+    description='A simple test package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Adel Mahmoud',
+    author_email='adelam578568@gmail.com'
+)
