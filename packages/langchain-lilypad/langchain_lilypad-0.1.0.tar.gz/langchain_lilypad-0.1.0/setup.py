b09deac6bdff4595d96d668967314de909a0a664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 08:13:08 +0000

    Add package langchain-lilypad/0.1.0/langchain_lilypad-0.1.0.tar.gz

---
+++ packages/langchain-lilypad/langchain_lilypad-0.1.0.tar.gz/langchain_lilypad-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain_lilypad',
+    version='0.1.0',
+    author='Altaga',
+    author_email='altaga@protonmail.com',
+    description='The  module defines a custom chat model that interacts with the Lilypad API to generate conversational responses and bind tools for enhanced functionality, with mechanisms for parsing tool calls and customizing message payloads.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
