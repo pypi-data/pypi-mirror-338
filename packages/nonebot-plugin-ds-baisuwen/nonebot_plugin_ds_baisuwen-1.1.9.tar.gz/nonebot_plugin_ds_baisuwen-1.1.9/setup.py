c312807adb84ff2cb5b404510723ded87522d895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 08:12:37 +0000

    Add package nonebot-plugin-ds-baisuwen/1.1.9/nonebot_plugin_ds_baisuwen-1.1.9.tar.gz

---
+++ packages/nonebot-plugin-ds-baisuwen/nonebot_plugin_ds_baisuwen-1.1.9.tar.gz/nonebot_plugin_ds_baisuwen-1.1.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nonebot-plugin-ds-baisuwen",
+    version="1.1.9",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "nonebot2>=2.0.0",
+        "httpx[http2]>=0.23.0",
+        "tenacity>=8.2.2",
+        "redis>=4.5.1",
+        "nonebot-adapter-onebot>=2.0.0"
+    ],
+    extras_require={
+        "onebot-v11": ["nonebot-adapter-onebot-v11>=2.0.0"]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
