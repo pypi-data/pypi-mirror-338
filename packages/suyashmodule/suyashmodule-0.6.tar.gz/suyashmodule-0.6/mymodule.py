ca5f4ee28b47b8ed4471c66305b2b095fc8b44b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:19:02 +0000

    Add package suyashmodule/0.6/suyashmodule-0.6.tar.gz

---
+++ packages/suyashmodule/suyashmodule-0.6.tar.gz/suyashmodule-0.6/mymodule.py
@@ -0,0 +1,25 @@
+import time
+
+def greet(name):
+    return f"Hello, {name}!"
+
+
+def find():
+    print("I will guess your father's name...")
+    name = input("Enter your name: ")
+    dob = input("Enter your date of birth(DD/MM/YYYY): ")
+    print("Searching....")
+    time.sleep(2)
+    print("Found!")
+    time.sleep(2)
+    print("Found it!")
+    time.sleep(2)  
+    print("Found it again!")
+    print("3!")
+    time.sleep(1)
+    print("2!")
+    time.sleep(1)
+    print("1!")
+    time.sleep(1)  
+    print("Your father is Suyash Guliani!")
+    
